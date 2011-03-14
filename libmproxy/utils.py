897bd5c2b889f71bfacb9d126efd73238c49932a
Aldo Cortesi <aldo@nullcube.com>
2011-03-14 13:47:51 +1300

    We no longer use pytz.

--- libmproxy/utils.py
+++ libmproxy/utils.py
@@ -12,7 +12,7 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import re, os, subprocess, datetime, textwrap, errno, sys, time, pytz
+import re, os, subprocess, datetime, textwrap, errno, sys, time
 
 
 def timestamp():
