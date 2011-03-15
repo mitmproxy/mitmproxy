e6ef0c3faf21971c144aaf93e6c05d98f1fe74be
Aldo Cortesi <aldo@nullcube.com>
2011-03-16 11:09:10 +1300

    Begin to flesh out and refactor docs.

---
+++ doc-src/certinstall/index.py
@@ -0,0 +1,10 @@
+from countershape import Page
+
+pages = [
+    Page("firefox.html", "Firefox"),
+    Page("chrome.html", "Chrome"),
+    Page("safari.html", "Safari"),
+    Page("ie.html", "Internet Explorer"),
+    Page("windows7.html", "Windows 7"),
+    Page("ios.html", "IOS"),
+]
