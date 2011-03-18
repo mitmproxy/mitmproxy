872b7881f2793bec8b2596e2a65bc4891387a1d5
Aldo Cortesi <aldo@nullcube.com>
2011-03-19 11:26:51 +1300

    Docs.

--- doc-src/certinstall/index.py
+++ doc-src/certinstall/index.py
@@ -2,8 +2,6 @@ from countershape import Page
 
 pages = [
     Page("firefox.html", "Firefox"),
-    Page("safari.html", "Safari"),
-    Page("ie.html", "Internet Explorer"),
     Page("osx.html", "OSX"),
     Page("windows7.html", "Windows 7"),
     Page("ios.html", "IOS"),
