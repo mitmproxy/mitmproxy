8cd140ef3340775e2bb502142bfd93fc402111db
Aldo Cortesi <aldo@nullcube.com>
2012-06-28 15:14:57 +1200

    Add dependency reference for netlib.

--- setup.py
+++ setup.py
@@ -93,4 +93,7 @@ setup(
             "Topic :: Software Development :: Testing"
         ],
         install_requires=["netlib", "urwid>=1.0", "pyasn1>0.1.2", "pyopenssl>=0.12", "PIL", "lxml"],
+        dependency_links=[
+            'https://github.com/cortesi/netlib/tarball/master#egg=netlib-0.1'
+        ],
 )
