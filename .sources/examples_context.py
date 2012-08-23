b78a3817cdddd4bc43a7d3159382283f25a22fa7
Aldo Cortesi <aldo@nullcube.com>
2012-08-23 12:37:02 +1200

    Adjust examples and docs.

--- .sources/examples_context.py
+++ .sources/examples_context.py
@@ -1,23 +1,22 @@
 import requests
 from libpathod import test
 
-class Test:
+def test_simple():
     """
-        Testing the requests module with 
+        Testing the requests module with
         a pathod context manager.
     """
-    def test_simple(self):
-        # Start pathod in a separate thread
-        with test.Daemon() as d:
-            # Get a URL for a pathod spec
-            url = d.p("200:b@100")
-            # ... and request it
-            r = requests.put(url)
+    # Start pathod in a separate thread
+    with test.Daemon() as d:
+        # Get a URL for a pathod spec
+        url = d.p("200:b@100")
+        # ... and request it
+        r = requests.put(url)
 
-            # Check the returned data
-            assert r.status_code == 200
-            assert len(r.content) == 100
+        # Check the returned data
+        assert r.status_code == 200
+        assert len(r.content) == 100
 
-            # Check pathod's internal log
-            log = d.last_log()["request"]
-            assert log["method"] == "PUT"
+        # Check pathod's internal log
+        log = d.last_log()["request"]
+        assert log["method"] == "PUT"
