208204d33a3d279267b5460c5cde8667a2e7a7df
Jason A. Novak <janovak@uchicago.edu>
2013-04-21 14:28:00 -0500

    Further cleanup of documentation

--- doc-src/scripting/index.py
+++ doc-src/scripting/index.py
@@ -3,5 +3,5 @@ from countershape import Page
 pages = [
     Page("inlinescripts.html", "Inline Scripts"),
     Page("libmproxy.html", "libmproxy"),
-    Page("addingviews.html","Adding new content Views")
+    Page("addingviews.html","Adding new content viewers")
 ]
