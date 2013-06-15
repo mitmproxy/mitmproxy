b4f6f09c83faf54c4c1493e18ba11d0257ca841b
Maximilian Hils <git@maximilianhils.com>
2013-06-16 00:10:53 +0200

    update test script

--- test/scripts/a.py
+++ test/scripts/a.py
@@ -1,12 +1,12 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--var')
+parser.add_argument('--var', type=int)
 
 var = 0
-def __init__(ctx, argv):
+def start(ctx, argv):
     global var
-    var = parser.parse_args(argv).var
+    var = parser.parse_args(argv[1:]).var
 
 def here(ctx):
     global var
