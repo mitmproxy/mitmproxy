c9ab1c60b5d43f0b4d645c751350b16e9e562b55
Maximilian Hils <git@maximilianhils.com>
2013-06-16 00:28:21 +0200

    always read files in binary mode

--- test/test_certutils.py
+++ test/test_certutils.py
@@ -53,22 +53,22 @@ class TestDummyCert:
             assert certutils.dummy_ca(cacert)
             p = os.path.join(d, "foo")
             certutils.dummy_cert(
-                file(p, "w"),
+                file(p, "wb"),
                 cacert,
                 "foo.com",
                 ["one.com", "two.com", "*.three.com"]
             )
-            assert file(p).read()
+            assert file(p,"rb").read()
 
 
 
 class TestSSLCert:
     def test_simple(self):
-        c = certutils.SSLCert.from_pem(file(tutils.test_data.path("data/text_cert"), "r").read())
+        c = certutils.SSLCert.from_pem(file(tutils.test_data.path("data/text_cert"), "rb").read())
         assert c.cn == "google.com"
         assert len(c.altnames) == 436
 
-        c = certutils.SSLCert.from_pem(file(tutils.test_data.path("data/text_cert_2"), "r").read())
+        c = certutils.SSLCert.from_pem(file(tutils.test_data.path("data/text_cert_2"), "rb").read())
         assert c.cn == "www.inode.co.nz"
         assert len(c.altnames) == 2
         assert c.digest("sha1")
@@ -82,11 +82,11 @@ class TestSSLCert:
         c.has_expired
 
     def test_err_broken_sans(self):
-        c = certutils.SSLCert.from_pem(file(tutils.test_data.path("data/text_cert_weird1"), "r").read())
+        c = certutils.SSLCert.from_pem(file(tutils.test_data.path("data/text_cert_weird1"), "rb").read())
         # This breaks unless we ignore a decoding error.
         c.altnames
 
     def test_der(self):
-        d = file(tutils.test_data.path("data/dercert")).read()
+        d = file(tutils.test_data.path("data/dercert"),"rb").read()
         s = certutils.SSLCert.from_der(d)
         assert s.cn
