db43f1ffccfb006c6856888b8f31c0745aacebb4
Aldo Cortesi <aldo@nullcube.com>
2013-06-16 13:59:01 +1200

    Version bump, doc extension, URLs to github.com/mitmproxy/*

---
+++ doc-src/dev/index.py
@@ -0,0 +1,6 @@
+from countershape import Page
+
+pages = [
+     Page("testing.html", "Testing"),
+#    Page("addingviews.html", "Writing Content Views"),
+]
--- doc-src/scripting/index.py
+++ doc-src/scripting/index.py
@@ -3,5 +3,4 @@ from countershape import Page
 pages = [
     Page("inlinescripts.html", "Inline Scripts"),
     Page("libmproxy.html", "libmproxy"),
-    Page("addingviews.html","Adding new content viewers")
 ]
