64ce3b358ffed2eb34d6836b57dd7849ce856a09
Aldo Cortesi <aldo@nullcube.com>
2013-07-23 10:28:35 +1200

    Make a start on mitmproxy web app.

--- libmproxy/app.py
+++ libmproxy/app.py
@@ -1,7 +1,14 @@
 import flask
 
 mapp = flask.Flask(__name__)
+mapp.debug = True
+
 
 @mapp.route("/")
-def hello():
-    return "mitmproxy"
+def index():
+    return flask.render_template("index.html", section="home")
+
+
+@mapp.route("/certs")
+def certs():
+    return flask.render_template("certs.html", section="certs")
