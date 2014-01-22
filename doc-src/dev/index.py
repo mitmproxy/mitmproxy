8f88fcedd601c0033b4469b66626a83011879baf
Aldo Cortesi <aldo@nullcube.com>
2014-01-22 13:33:02 +1300

    Move the doc tree out into its own repo.

--- doc-src/dev/index.py
+++
@@ -1,6 +0,0 @@
-from countershape import Page
-
-pages = [
-     Page("testing.html", "Testing"),
-#    Page("addingviews.html", "Writing Content Views"),
-]
--- doc-src/tutorials/index.py
+++
@@ -1,7 +0,0 @@
-from countershape import Page
-
-pages = [
-    Page("30second.html", "Client playback: a 30 second example"),
-    Page("gamecenter.html", "Setting highscores on Apple's GameCenter"),
-    Page("transparent-dhcp.html", "Transparently proxify virtual machines")
-]
