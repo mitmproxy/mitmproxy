f3369529abd601d7f10fcdb45ec280e8f6a7ad19
Aldo Cortesi <aldo@nullcube.com>
2014-01-28 13:24:11 +1300

    Adjust docs for web app certificate installation.

--- doc-src/certinstall/index.py
+++ doc-src/certinstall/index.py
@@ -1,6 +1,7 @@
 from countershape import Page
 
 pages = [
+    Page("webapp.html", "Using the Web App"),
     Page("firefox.html", "Firefox"),
     Page("osx.html", "OSX"),
     Page("windows7.html", "Windows 7"),
