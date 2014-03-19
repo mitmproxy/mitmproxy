6924eb75bf20776ef9834e854c73cfb74db732e1
Maximilian Hils <git@maximilianhils.com>
2014-03-19 16:05:19 +0100

    fix minor version string

--- libmproxy/version.py
+++ libmproxy/version.py
@@ -1,5 +1,5 @@
 IVERSION = (0, 11)
 VERSION = ".".join(str(i) for i in IVERSION)
-MINORVERSION = ".".join(str(i) for i in IVERSION[:1])
+MINORVERSION = ".".join(str(i) for i in IVERSION[:2])
 NAME = "mitmproxy"
 NAMEVERSION = NAME + " " + VERSION
