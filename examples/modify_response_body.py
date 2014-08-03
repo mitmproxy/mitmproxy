8f9395060f5afbb56d6e959b16578b9af372e647
Maximilian Hils <git@maximilianhils.com>
2014-08-03 02:39:41 +0200

    improve example scripts

--- examples/modify_response_body.py
+++ examples/modify_response_body.py
@@ -8,10 +8,9 @@ from libmproxy.protocol.http import decoded
 def start(ctx, argv):
     if len(argv) != 3:
         sys.exit('Usage: -s "modify-response-body.py old new"')
-    global old, new  # In larger scripts, a centralized options object (as returned by argparse) is encouraged
-    old, new = argv[1:]
+    ctx.old, ctx.new = argv[1], argv[2]
 
 
 def response(ctx, flow):
     with decoded(flow.response):  # automatically decode gzipped responses.
-        flow.response.content = flow.response.content.replace(old, new)
+        flow.response.content = flow.response.content.replace(ctx.old, ctx.new)
