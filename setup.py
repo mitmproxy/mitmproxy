60584387ff860befe38ada5ec9d35f3c529d0238
Maximilian Hils <git@maximilianhils.com>
2014-11-11 12:26:20 +0100

    be more explicit about requirements

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
             "nose>=1.3.0",
             "nose-cov>=1.6",
             "coveralls>=0.4.1",
-            "pathod>=0.%s" % version.MINORVERSION
+            "pathod>=%s, <%s" % (version.MINORVERSION, version.NEXT_MINORVERSION)
         ]
     }
 )
