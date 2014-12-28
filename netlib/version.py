3c919631d40cef69dacd166dabafc238a753edc8
Aldo Cortesi <aldo@nullcube.com>
2014-12-28 22:46:19 +1300

    Bump version

--- netlib/version.py
+++ netlib/version.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, print_function, division)
 
-IVERSION = (0, 11, 1)
+IVERSION = (0, 11, 2)
 VERSION = ".".join(str(i) for i in IVERSION)
 MINORVERSION = ".".join(str(i) for i in IVERSION[:2])
 NAME = "netlib"
