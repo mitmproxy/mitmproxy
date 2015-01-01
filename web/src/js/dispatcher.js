4506bafddc7e9f728205a86801a4403e07488575
Aldo Cortesi <aldo@nullcube.com>
2015-01-01 16:59:58 +1300

    Use Facebook's dispatcher

--- web/src/js/dispatcher.js
+++ web/src/js/dispatcher.js
@@ -1,30 +1,13 @@
+
+var flux = require("flux");
+
 const PayloadSources = {
     VIEW: "view",
     SERVER: "server"
 };
 
 
-function Dispatcher() {
-    this.callbacks = [];
-}
-Dispatcher.prototype.register = function (callback) {
-    this.callbacks.push(callback);
-};
-Dispatcher.prototype.unregister = function (callback) {
-    var index = this.callbacks.indexOf(callback);
-    if (index >= 0) {
-        this.callbacks.splice(index, 1);
-    }
-};
-Dispatcher.prototype.dispatch = function (payload) {
-    console.debug("dispatch", payload);
-    for (var i = 0; i < this.callbacks.length; i++) {
-        this.callbacks[i](payload);
-    }
-};
-
-
-AppDispatcher = new Dispatcher();
+AppDispatcher = new flux.Dispatcher();
 AppDispatcher.dispatchViewAction = function (action) {
     action.source = PayloadSources.VIEW;
     this.dispatch(action);
