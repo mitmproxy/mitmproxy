e18294437c4629f26dc65e16a9252ef61a109284
Maximilian Hils <git@maximilianhils.com>
2015-01-09 17:17:12 +0100

    document choice of timeout value, refs #443

--- libmproxy/controller.py
+++ libmproxy/controller.py
@@ -108,6 +108,10 @@ class Master(object):
         self.should_exit.clear()
         self.server.start_slave(Slave, Channel(self.masterq, self.should_exit))
         while not self.should_exit.is_set():
+
+            # Don't choose a very small timeout in Python 2:
+            # https://github.com/mitmproxy/mitmproxy/issues/443
+            # TODO: Lower the timeout value if we move to Python 3.
             self.tick(self.masterq, 0.1)
         self.shutdown()
 
