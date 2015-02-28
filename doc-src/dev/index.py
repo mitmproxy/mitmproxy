eec4c539f2f6b8124ab92d6087afb228874a479e
Maximilian Hils <git@maximilianhils.com>
2015-02-28 03:35:28 +0100

    SSLKEYLOGFILE docs

--- doc-src/dev/index.py
+++ doc-src/dev/index.py
@@ -3,5 +3,6 @@ from countershape import Page
 pages = [
      Page("testing.html", "Testing"),
      Page("architecture.html", "Architecture"),
+     Page("sslkeylogfile.html", "TLS Master Secrets"),
 #    Page("addingviews.html", "Writing Content Views"),
 ]
