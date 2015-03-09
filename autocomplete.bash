db9db73629c4d7a58a3273f4f55e1cbc8e4daca4
Marcelo Glezer <marcelo.glezer@gmail.com>
2015-03-08 23:03:47 -0300

    now completing files and static options

--- autocomplete.bash
+++ autocomplete.bash
@@ -16,10 +16,41 @@ _mitmproxy()
  	opts+="--setheader --nonanonymous --singleuser --htpasswd --cert --cert-forward --ciphers-client --ciphers-server " 
     opts+="--client-certs --no-upstream-cert --ssl-port --ssl-version-client --ssl-version-server -i --intercept " 
 
+    case "${prev}" in
+		--conf | -r | --read-flows | -w | --wfile | -a | --afile | -c | --client-replay | -S | --server-replay | --replace-from-file )
+	    	COMPREPLY=( $(compgen -f -- ${cur}) )
+            return 0
+            ;;
+         
+        --cadir | --client-certs)
+	    	COMPREPLY=( $(compgen -d -- ${cur}) )
+            return 0
+			;;
+		-s | --script)
+	    	COMPREPLY=( $(compgen -f *.py -- ${cur}) )
+            return 0
+            ;;
+        --palette)
+	    	COMPREPLY=( $(compgen -W "dark light solarized_dark solarized_light" -- ${cur}) )
+            return 0
+            ;;
+        --http-form-in | --http-form-out)
+	    	COMPREPLY=( $(compgen -W "relative absolute" -- ${cur}) )
+            return 0
+            ;;
+        --ssl-version-client | --ssl-version-server)
+	    	COMPREPLY=( $(compgen -W "all secure SSLv2 SSLv3 TLSv1 TLSv1_1 TLSv1_2" -- ${cur}) )
+            return 0
+            ;;
+        *)
+        ;;
+    esac
+
     if [[ ${cur} == -* ]] ; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
         return 0
     fi
 }
-complete -F _mitmproxy mitmproxy
+
 complete -F _mitmproxy ./mitmproxy
+complete -F _mitmproxy mitmproxy
