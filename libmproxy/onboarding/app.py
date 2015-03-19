97c6d7ed25e026ae8d5511550df9718f829a7f1e
Maximilian Hils <git@maximilianhils.com>
2015-03-19 16:29:05 +0100

    onboarding app: serve certs inline to unbreak firefox

--- libmproxy/onboarding/app.py
+++ libmproxy/onboarding/app.py
@@ -45,7 +45,7 @@ class PEM(tornado.web.RequestHandler):
     def get(self):
         p = os.path.join(self.request.master.server.config.cadir, self.filename)
         self.set_header("Content-Type", "application/x-x509-ca-cert")
-        self.set_header("Content-Disposition", "attachment; filename={}".format(self.filename))
+        self.set_header("Content-Disposition", "inline; filename={}".format(self.filename))
 
         with open(p, "rb") as f:
             self.write(f.read())
@@ -59,7 +59,7 @@ class P12(tornado.web.RequestHandler):
     def get(self):
         p = os.path.join(self.request.master.server.config.cadir, self.filename)
         self.set_header("Content-Type", "application/x-pkcs12")
-        self.set_header("Content-Disposition", "attachment; filename={}".format(self.filename))
+        self.set_header("Content-Disposition", "inline; filename={}".format(self.filename))
 
         with open(p, "rb") as f:
             self.write(f.read())
