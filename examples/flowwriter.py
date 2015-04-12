ba149d90adc9d041b255bdefc7cb2002cbcf4216
Maximilian Hils <git@maximilianhils.com>
2015-04-12 03:50:14 +0200

    spaces, not tabs

--- examples/flowwriter.py
+++ examples/flowwriter.py
@@ -1,17 +1,20 @@
-from libmproxy.flow import FlowWriter
 import random
 import sys
+
+from libmproxy.flow import FlowWriter
+
 
 def start(context, argv):
     if len(argv) != 2:
         raise ValueError('Usage: -s "flowriter.py filename"')
 
     if argv[1] == "-":
-    	f = sys.stdout
+        f = sys.stdout
     else:
-    	f = open(argv[1], "wb")
+        f = open(argv[1], "wb")
     context.flow_writer = FlowWriter(f)
 
+
 def response(context, flow):
-	if random.choice([True, False]):
-		context.flow_writer.add(flow)
+    if random.choice([True, False]):
+        context.flow_writer.add(flow)
