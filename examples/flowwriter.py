c0a318566ae0fb5a264bfaa130f3ff3a09ea5bf5
Maximilian Hils <git@maximilianhils.com>
2015-04-12 03:47:58 +0200

    add flowwriter example

---
+++ examples/flowwriter.py
@@ -0,0 +1,17 @@
+from libmproxy.flow import FlowWriter
+import random
+import sys
+
+def start(context, argv):
+    if len(argv) != 2:
+        raise ValueError('Usage: -s "flowriter.py filename"')
+
+    if argv[1] == "-":
+    	f = sys.stdout
+    else:
+    	f = open(argv[1], "wb")
+    context.flow_writer = FlowWriter(f)
+
+def response(context, flow):
+	if random.choice([True, False]):
+		context.flow_writer.add(flow)
