2d9b9be1f4fb67d6989b57b68858896d8512293e
Aldo Cortesi <aldo@nullcube.com>
2015-06-05 11:50:29 +1200

    Revert "tcp: clear_log to clear socket logs"
    start_log also clears the log, which is good enough.
    
    This reverts commit 4ca62e0d9bd09aa286cde9bafceff7204304d00c.

--- test/test_tcp.py
+++ test/test_tcp.py
@@ -553,8 +553,6 @@ class TestFileLike:
         assert s.get_log() == ""
         s.read(1)
         assert s.get_log() == "o"
-        s.clear_log()
-        assert s.get_log() == ""
         s.stop_log()
         tutils.raises(ValueError, s.get_log)
 
