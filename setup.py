7e4020213cbc8e63fe1eff1aad9af0b63540e844
Aldo Cortesi <aldo@nullcube.com>
2015-08-16 12:31:51 +1200

    Remove release tools

--- setup.py
+++ setup.py
@@ -38,8 +38,6 @@ dev_deps = {
     "nose>=1.3.0",
     "nose-cov>=1.6",
     "coveralls>=0.4.1",
-    "click>=4.1",
-    "twine>=1.5.0",
     "pathod>=%s, <%s" % (version.MINORVERSION, version.NEXT_MINORVERSION),
     "countershape"
 }
