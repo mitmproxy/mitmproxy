92c763f469fdf721f3d981346f8a40e33b06de23
Maximilian Hils <git@maximilianhils.com>
2015-09-10 12:32:38 +0200

    fix mitmproxy/mitmproxy#759

--- netlib/version_check.py
+++ netlib/version_check.py
@@ -3,10 +3,11 @@ Having installed a wrong version of pyOpenSSL or netlib is unfortunately a
 very common source of error. Check before every start that both versions
 are somewhat okay.
 """
-from __future__ import division, absolute_import, print_function, unicode_literals
+from __future__ import division, absolute_import, print_function
 import sys
 import inspect
 import os.path
+
 import OpenSSL
 from . import version
 
@@ -28,19 +29,29 @@ def check_mitmproxy_version(mitmproxy_version, fp=sys.stderr):
 
 
 def check_pyopenssl_version(min_version=PYOPENSSL_MIN_VERSION, fp=sys.stderr):
-    v = tuple(int(x) for x in OpenSSL.__version__.split(".")[:2])
+    min_version_str = ".".join(str(x) for x in min_version)
+    try:
+        v = tuple(int(x) for x in OpenSSL.__version__.split(".")[:2])
+    except ValueError:
+        print(
+            "Cannot parse pyOpenSSL version: {}"
+            "mitmproxy requires pyOpenSSL {} or greater.".format(
+                OpenSSL.__version__, min_version_str
+            ),
+            file=fp
+        )
+        return
     if v < min_version:
         print(
-            "You are using an outdated version of pyOpenSSL:"
-            " mitmproxy requires pyOpenSSL %s or greater." %
-            str(min_version),
+            "You are using an outdated version of pyOpenSSL: "
+            "mitmproxy requires pyOpenSSL {} or greater.".format(min_version_str),
             file=fp
         )
         # Some users apparently have multiple versions of pyOpenSSL installed.
         # Report which one we got.
         pyopenssl_path = os.path.dirname(inspect.getfile(OpenSSL))
         print(
-            "Your pyOpenSSL %s installation is located at %s" % (
+            "Your pyOpenSSL {} installation is located at {}".format(
                 OpenSSL.__version__, pyopenssl_path
             ),
             file=fp
--- test/test_version_check.py
+++ test/test_version_check.py
@@ -24,5 +24,15 @@ def test_check_pyopenssl_version(sexit):
     assert not sexit.called
 
     version_check.check_pyopenssl_version((9999,), fp=fp)
-    assert fp.getvalue()
+    assert "outdated" in fp.getvalue()
     assert sexit.called
+
+
+@mock.patch("sys.exit")
+@mock.patch("OpenSSL.__version__")
+def test_unparseable_pyopenssl_version(version, sexit):
+    version.split.return_value = ["foo", "bar"]
+    fp = cStringIO.StringIO()
+    version_check.check_pyopenssl_version(fp=fp)
+    assert "Cannot parse" in fp.getvalue()
+    assert not sexit.called
