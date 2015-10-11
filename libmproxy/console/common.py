d7f9bd3c655d17c56ba6313a2dd6b43037beadb1
Marcelo Glezer <marcelo.glezer@gmail.com>
2015-10-11 14:02:22 -0300

    used http1.assemble_request/response instead of custom concats

--- libmproxy/console/common.py
+++ libmproxy/console/common.py
@@ -252,11 +252,7 @@ def copy_flow_format_data(part, scope, flow):
                 return None, "Request content is missing"
             with decoded(flow.request):
                 if part == "h":
-                    headerString = ""
-                    for k,v in flow.request.headers.fields:
-                        headerString +=  k + ":" + v + "\n"
-                    data += headerString
-                    data += flow.request.content
+                    data += netlib.http.http1.assemble_request(flow.request)
                 elif part == "c":
                     data += flow.request.content
                 else:
@@ -269,11 +265,7 @@ def copy_flow_format_data(part, scope, flow):
                 return None, "Response content is missing"
             with decoded(flow.response):
                 if part == "h":
-                    headerString = ""
-                    for k,v in flow.response.headers.fields:
-                        headerString +=  k + ":" + v + "\n"
-                    data += headerString
-                    data += flow.response.content
+                    data += netlib.http.http1.assemble_response(flow.response)
                 elif part == "c":
                     data += flow.response.content
                 else:
