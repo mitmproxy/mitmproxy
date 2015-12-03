b362e9a0131574636902f60554b293c0a98b83dd
Maximilian Hils <git@maximilianhils.com>
2015-12-03 14:43:56 +0100

    test flow conversion

---
+++ test/test_flow_format_compat.py
@@ -0,0 +1,17 @@
+from libmproxy.flow import FlowReader, FlowReadError
+import tutils
+
+
+def test_load():
+    with open(tutils.test_data.path("data/dumpfile-013"), "rb") as f:
+        flow_reader = FlowReader(f)
+        flows = list(flow_reader.stream())
+        assert len(flows) == 1
+        assert flows[0].request.url == "https://example.com/"
+
+
+def test_cannot_convert():
+    with open(tutils.test_data.path("data/dumpfile-012"), "rb") as f:
+        flow_reader = FlowReader(f)
+        with tutils.raises(FlowReadError):
+            list(flow_reader.stream())
