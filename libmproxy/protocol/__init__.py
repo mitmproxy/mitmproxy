ce0a500885619444b7fd386419c5c556d5196e18
David Weinstein <dweinst@insitusec.com>
2016-01-23 21:29:14 -0500

    Add ignore based on TLS ClientHello SNI
    - also add some documentation about ignoring based on SNI

--- libmproxy/protocol/__init__.py
+++ libmproxy/protocol/__init__.py
@@ -28,12 +28,12 @@ as late as possible; this makes server replay without any outgoing connections p
 from __future__ import (absolute_import, print_function, division)
 from .base import Layer, ServerConnectionMixin, Kill
 from .http import Http1Layer, UpstreamConnectLayer, Http2Layer
-from .tls import TlsLayer, is_tls_record_magic
+from .tls import TlsLayer, is_tls_record_magic, TlsClientHello
 from .rawtcp import RawTCPLayer
 
 __all__ = [
     "Layer", "ServerConnectionMixin", "Kill",
     "Http1Layer", "UpstreamConnectLayer", "Http2Layer",
-    "TlsLayer", "is_tls_record_magic",
+    "TlsLayer", "is_tls_record_magic", "TlsClientHello"
     "RawTCPLayer"
 ]
