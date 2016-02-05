4bad98cfceb576ea285ed72580448256b47fb5c2
Maximilian Hils <git@maximilianhils.com>
2016-02-05 23:39:48 +0100

    use setup.y environment markers

--- setup.py
+++ setup.py
@@ -14,20 +14,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-deps = {
-    "pyasn1>=0.1.9, <0.2",
-    "pyOpenSSL>=0.15.1, <0.16",
-    "cryptography>=1.2.2, <1.3",
-    "passlib>=1.6.5, <1.7",
-    "hpack>=2.1.0, <3.0",
-    "hyperframe>=3.2.0, <4.0",
-    "six>=1.10.0, <1.11",
-    "certifi>=2015.11.20.1",  # no semver here - this should always be on the last release!
-    "backports.ssl_match_hostname>=3.5.0.1, <3.6",
-}
-if sys.version_info < (3, 0):
-    deps.add("ipaddress>=1.0.15, <1.1")
-
 setup(
     name="netlib",
     version=version.VERSION,
@@ -57,18 +43,30 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=list(deps),
+    install_requires=[
+        "pyasn1>=0.1.9, <0.2",
+        "pyOpenSSL>=0.15.1, <0.16",
+        "cryptography>=1.2.2, <1.3",
+        "passlib>=1.6.5, <1.7",
+        "hpack>=2.1.0, <3.0",
+        "hyperframe>=3.2.0, <4.0",
+        "six>=1.10.0, <1.11",
+        "certifi>=2015.11.20.1",  # no semver here - this should always be on the last release!
+        "backports.ssl_match_hostname>=3.5.0.1, <3.6",
+    ],
     extras_require={
+        # Do not use a range operator here: https://bitbucket.org/pypa/setuptools/issues/380
+        # Ubuntu Trusty and other still ship with setuptools < 17.1
+        ':python_version == "2.7"': [
+            "ipaddress>=1.0.15, <1.1",
+        ],
         'dev': [
-            "mock>=1.0.1",
-            "pytest>=2.8.0",
-            "pytest-xdist>=1.13.1",
-            "pytest-cov>=2.1.0",
-            "pytest-timeout>=1.0.0",
-            "coveralls>=0.4.1",
-            "autopep8>=1.0.3",
-            "autoflake>=0.6.6",
-            "wheel>=0.24.0",
+            "mock>=1.3.0, <1.4",
+            "pytest>=2.8.7, <2.9",
+            "pytest-xdist>=1.14, <1.15",
+            "pytest-cov>=2.2.1, <2.3",
+            "pytest-timeout>=1.0.0, <1.1",
+            "coveralls>=1.1, <1.2"
         ]
     },
 )
