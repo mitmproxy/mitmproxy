14186d98d624492bb36e1d966a634994d0358bac
Maximilian Hils <git@maximilianhils.com>
2016-02-08 15:10:20 +0100

    bump version

--- libpathod/version.py
+++ libpathod/version.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, print_function, division)
 
-IVERSION = (0, 15)
+IVERSION = (0, 16)
 VERSION = ".".join(str(i) for i in IVERSION)
 MINORVERSION = ".".join(str(i) for i in IVERSION[:2])
 NAME = "pathod"
