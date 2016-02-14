aafa69a73829a7ec291a2d6fa0c4522caf287d17
Maximilian Hils <git@maximilianhils.com>
2016-02-14 17:25:30 +0100

    bump version

--- netlib/version.py
+++ netlib/version.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, print_function, division)
 
-IVERSION = (0, 16)
+IVERSION = (0, 17)
 VERSION = ".".join(str(i) for i in IVERSION)
 MINORVERSION = ".".join(str(i) for i in IVERSION[:2])
 NAME = "netlib"
