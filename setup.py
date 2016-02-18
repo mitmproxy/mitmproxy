d33d3663ecb166461d9cb5a78a29b44ee7a8fbb7
Maximilian Hils <git@maximilianhils.com>
2016-02-18 13:03:40 +0100

    combine projects

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ import sys
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-sys.path.append(os.path.join(here, "..", "netlib"))
 from mitmproxy import version
 
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -30,6 +29,7 @@ setup(
         "Environment :: Console :: Curses",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
@@ -41,37 +41,52 @@ setup(
         "Topic :: Internet :: Proxy Servers",
         "Topic :: Software Development :: Testing"
     ],
-    packages=find_packages(),
+    packages=find_packages(include=[
+        "mitmproxy", "mitmproxy.*",
+        "pathod", "pathod.*",
+        "netlib", "netlib.*"
+    ]),
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'mitmproxy = mitmproxy.main:mitmproxy',
-            'mitmdump = mitmproxy.main:mitmdump',
-            'mitmweb = mitmproxy.main:mitmweb'
+            "mitmproxy = mitmproxy.main:mitmproxy",
+            "mitmdump = mitmproxy.main:mitmdump",
+            "mitmweb = mitmproxy.main:mitmweb",
+            "pathod = pathod.pathod_cmdline:go_pathod",
+            "pathoc = pathod.pathoc_cmdline:go_pathoc"
         ]
     },
     # https://packaging.python.org/en/latest/requirements/#install-requires
     # It is not considered best practice to use install_requires to pin dependencies to specific versions.
     install_requires=[
-        "netlib=={}".format(version.VERSION),
-        "h2>=2.1.2, <3.0",
-        "tornado>=4.3, <4.4",
-        "configargparse>=0.10, <0.11",
-        "pyperclip>=1.5.22, <1.6",
+        "backports.ssl_match_hostname>=3.5.0.1, <3.6",
         "blinker>=1.4, <1.5",
-        "pyparsing>=2.1,<2.2",
-        "html2text==2016.1.8",
-        "construct>=2.5.2, <2.6",
-        "six>=1.10, <1.11",
-        "Pillow>=3.1, <3.2",
-        "watchdog>=0.8.3, <0.9",
         "click>=6.2, <7.0",
+        "certifi>=2015.11.20.1",  # no semver here - this should always be on the last release!
+        "configargparse>=0.10, <0.11",
+        "construct>=2.5.2, <2.6",
+        "cryptography>=1.2.2, <1.3",
+        "Flask>=0.10.1, <0.11",
+        "h2>=2.1.2, <3.0",
+        "hpack>=2.1.0, <3.0",
+        "html2text==2016.1.8",
+        "hyperframe>=3.2.0, <4.0",
+        "Pillow>=3.1, <3.2",
+        "passlib>=1.6.5, <1.7",
+        "pyasn1>=0.1.9, <0.2",
+        "pyOpenSSL>=0.15.1, <0.16",
+        "pyparsing>=2.1,<2.2",
+        "pyperclip>=1.5.22, <1.6",
+        "requests>=2.9.1, <2.10",
+        "six>=1.10, <1.11",
+        "tornado>=4.3, <4.4",
         "urwid>=1.3.1, <1.4",
+        "watchdog>=0.8.3, <0.9",
     ],
     extras_require={
         ':sys_platform == "win32"': [
-            "pydivert>=0.0.7, <0.1",
             "lxml==3.4.4",  # there are no Windows wheels for newer versions, so we pin this.
+            "pydivert>=0.0.7, <0.1",
         ],
         ':sys_platform != "win32"': [
             "lxml>=3.5.0, <3.6",
@@ -80,28 +95,28 @@ setup(
         # Ubuntu Trusty and other still ship with setuptools < 17.1
         ':python_version == "2.7"': [
             "enum34>=1.0.4, <2",
+            "ipaddress>=1.0.15, <1.1",
         ],
         'dev': [
+            "coveralls>=1.1, <1.2",
             "mock>=1.3.0, <1.4",
             "pytest>=2.8.7, <2.9",
-            "pytest-xdist>=1.14, <1.15",
             "pytest-cov>=2.2.1, <2.3",
             "pytest-timeout>=1.0.0, <1.1",
-            "coveralls>=1.1, <1.2",
-            "pathod=={}".format(version.VERSION),
+            "pytest-xdist>=1.14, <1.15",
             "sphinx>=1.3.5, <1.4",
             "sphinx-autobuild>=0.5.2, <0.7",
-            "sphinxcontrib-documentedlist>=0.3.0, <0.4"
+            "sphinxcontrib-documentedlist>=0.3.0, <0.4",
         ],
         'contentviews': [
-            "pyamf>=0.8.0, <0.9",
+            "cssutils>=1.0.1, <1.1",
             "protobuf>=2.6.1, <2.7",
-            "cssutils>=1.0.1, <1.1"
+            "pyamf>=0.8.0, <0.9",
         ],
         'examples': [
-            "pytz==2015.7.0",
-            "harparser>=0.2, <0.3",
             "beautifulsoup4>=4.4.1, <4.5",
+            "harparser>=0.2, <0.3",
+            "pytz==2015.7.0",
         ]
     }
 )
