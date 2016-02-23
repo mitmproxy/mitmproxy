9ccdee7915e2d1c29e963796f01422629460d783
Parth Ganatra <ganatraparth1995@gmail.com>
2016-02-24 00:17:48 +0530

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -4,13 +4,7 @@ VENV=./venv
 
 python -m virtualenv $VENV --always-copy
 . $VENV/bin/activate
-pip_version=$(pip -V | cut -d' ' -f 2 | cut -d'.' -f 1)
-if [ "$pip_version" -lt 6 ];
-then
-	echo "Error. Outdated pip"
-	echo "To upgrade pip run: \`pip install -U pip\`r"
-	exit 1
-fi
+pip install -U pip
 pip install -r requirements.txt
 
 echo ""
