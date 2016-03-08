6eb584f766616b57821d88fb0c1c545afe748a6d
Matthew Shao <me@matshao.com>
2016-03-08 19:42:38 +0800

    Fix #1005

--- mitmproxy/console/__init__.py
+++ mitmproxy/console/__init__.py
@@ -58,6 +58,8 @@ class ConsoleState(flow.State):
         super(ConsoleState, self).update_flow(f)
         if self.focus is None:
             self.set_focus(0)
+        elif self.follow_focus:
+            self.set_focus(self.flow_count())
         return f
 
     def set_limit(self, limit):
