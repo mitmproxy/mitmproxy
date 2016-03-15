0a478e4e39395c3eb3dea9933b6e41f6ac3da038
cle1000 <cle1000.cb@gmail.com>
2016-03-15 15:36:10 +0100

    Update modify_response_body.py
    fix typo

--- examples/modify_response_body.py
+++ examples/modify_response_body.py
@@ -5,7 +5,7 @@ from mitmproxy.models import decoded
 
 def start(context, argv):
     if len(argv) != 3:
-        raise ValueError('Usage: -s "modify-response-body.py old new"')
+        raise ValueError('Usage: -s "modify_response_body.py old new"')
     # You may want to use Python's argparse for more sophisticated argument
     # parsing.
     context.old, context.new = argv[1], argv[2]
