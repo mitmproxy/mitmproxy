a267934d99e2e5d561a71ee3bc3944ddb6f6ce53
Maximilian Hils <git@maximilianhils.com>
2016-04-02 22:00:14 +0200

    minor simplifications

--- mitmproxy/protocol/http.py
+++ mitmproxy/protocol/http.py
@@ -184,7 +184,7 @@ class HttpLayer(Layer):
                 flow.request = request
                 # set upstream auth
                 if self.mode == "upstream" and self.config.upstream_auth is not None:
-                    flow.request.set_auth(self.config.upstream_auth)
+                    self.data.headers["Proxy-Authorization"] = self.config.upstream_auth
                 self.process_request_hook(flow)
 
                 if not flow.response:
