60bbc250d45fbec544831fccee273f721a90435d
Maximilian Hils <git@maximilianhils.com>
2016-04-29 20:17:18 -0700

    mitmdump: fix shutdown after reading flow file

--- mitmproxy/dump.py
+++ mitmproxy/dump.py
@@ -346,6 +346,5 @@ class DumpMaster(flow.FlowMaster):
 
     def run(self):  # pragma: no cover
         if self.o.rfile and not self.o.keepserving:
-            self.shutdown()
             return
         super(DumpMaster, self).run()
