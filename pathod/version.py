acab4fea5a8f2858922a2a92ba4d7b6cf63df88a
Maximilian Hils <git@maximilianhils.com>
2016-05-11 13:02:19 -0600

    fix version.NAME

--- pathod/version.py
+++ pathod/version.py
@@ -1,3 +1,6 @@
 from __future__ import (absolute_import, print_function, division)
 
-from netlib.version import *
+from netlib.version import VERSION, IVERSION
+
+NAME = "pathod"
+NAMEVERSION = NAME + " " + VERSION
