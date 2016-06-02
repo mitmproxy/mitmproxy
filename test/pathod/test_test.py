c9dd574f48cb05ff762bccdeef2dc22c758de9ec
Aldo Cortesi <aldo@nullcube.com>
2016-06-02 19:40:39 +1200

    Silence requests warnings
    Once we convert the rest of the suite to pathoc, this can go.

--- test/pathod/test_test.py
+++ test/pathod/test_test.py
@@ -4,6 +4,9 @@ from pathod import test
 import tutils
 logging.disable(logging.CRITICAL)
 
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
+
 
 class TestDaemonManual:
 
