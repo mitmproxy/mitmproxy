009a4d2adde7fe8a6c00e18b610a61856bf3914f
Maximilian Hils <git@maximilianhils.com>
2016-06-06 23:38:21 -0700

    s/coveralls/codecov/

--- setup.py
+++ setup.py
@@ -97,7 +97,6 @@ setup(
             "ipaddress>=1.0.15, <1.1",
         ],
         'dev': [
-            "coveralls>=1.1, <1.2",
             "mock>=2.0, <2.1",
             "flake8>=2.5.4, <3",
             "pytest>=2.8.7, <2.10",
