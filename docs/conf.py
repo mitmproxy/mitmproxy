9b7de5c98f4faf55e273a04c227d45d35a85b4cc
Aldo Cortesi <aldo@nullcube.com>
2016-06-07 13:54:40 +1200

    docs: adjust theme override inclusion
    https://github.com/rtfd/readthedocs.org/issues/2116

--- docs/conf.py
+++ docs/conf.py
@@ -218,8 +218,5 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'mitmproxydoc'
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
-    ],
-}
+def setup(app):
+    app.add_stylesheet('theme_overrides.css')
