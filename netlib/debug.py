e9f8eb1f6bf1a30bba304769507c1eb6ef64c3cf
Aldo Cortesi <aldo@nullcube.com>
2016-06-11 23:13:41 +1200

    Satisfy lint

--- netlib/debug.py
+++ netlib/debug.py
@@ -81,7 +81,11 @@ def dump_stacks(signal, frame, file=sys.stdout):
     id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
     code = []
     for threadId, stack in sys._current_frames().items():
-        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""), threadId))
+        code.append(
+            "\n# Thread: %s(%d)" % (
+                id2name.get(threadId, ""), threadId
+            )
+        )
         for filename, lineno, name, line in traceback.extract_stack(stack):
             code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
             if line:
