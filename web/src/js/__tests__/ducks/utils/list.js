37c2b47c26dea9b143b2720761cdce21dafd3884
Jason <jason.daurus@gmail.com>
2016-06-27 18:36:21 +0800

    [web] add tests for utils

---
+++ web/src/js/__tests__/ducks/utils/list.js
@@ -0,0 +1,63 @@
+jest.unmock('lodash')
+jest.unmock('../../../ducks/utils/list')
+
+import reduce, * as list from '../../../ducks/utils/list'
+
+describe('list reduce', () => {
+
+    it('should add item', () => {
+        const state = createState([
+            { id: 1 },
+            { id: 2 }
+        ])
+        const result = createState([
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ])
+        expect(reduce(state, list.add({ id: 3 }))).toEqual(result)
+    })
+
+    it('should update item', () => {
+        const state = createState([
+            { id: 1, val: 1 },
+            { id: 2, val: 2 }
+        ])
+        const result = createState([
+            { id: 1, val: 1 },
+            { id: 2, val: 3 }
+        ])
+        expect(reduce(state, list.update(2, { id: 2, val: 3 }))).toEqual(result)
+    })
+
+    it('should remove item', () => {
+        const state = createState([
+            { id: 1 },
+            { id: 2 }
+        ])
+        const result = createState([
+            { id: 1 }
+        ])
+        result.byId[2] = result.indexOf[2] = null
+        expect(reduce(state, list.remove(2))).toEqual(result)
+    })
+
+    it('should replace all items', () => {
+        const state = createState([
+            { id: 1 },
+            { id: 2 }
+        ])
+        const result = createState([
+            { id: 1 }
+        ])
+        expect(reduce(state, list.receive([{ id: 1 }]))).toEqual(result)
+    })
+})
+
+function createState(items) {
+    return {
+        data: items,
+        byId: _.fromPairs(items.map((item, index) => [item.id, item])),
+        indexOf: _.fromPairs(items.map((item, index) => [item.id, index]))
+    }
+}
