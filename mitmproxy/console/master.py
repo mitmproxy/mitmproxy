bb1400777bf0ecbb9b3b937c4767cdd8c79c9264
Maximilian Hils <git@maximilianhils.com>
2016-07-21 18:30:09 -0700

    fix #1390

--- mitmproxy/console/master.py
+++ mitmproxy/console/master.py
@@ -86,10 +86,10 @@ class ConsoleState(flow.State):
 
     def set_focus(self, idx):
         if self.view:
-            if idx >= len(self.view):
-                idx = len(self.view) - 1
-            elif idx < 0:
+            if idx is None or idx < 0:
                 idx = 0
+            elif idx >= len(self.view):
+                idx = len(self.view) - 1
             self.focus = idx
         else:
             self.focus = None
