b7c85c39aca60d5acc47865c44dce86d6a9ac84b
Maximilian Hils <git@maximilianhils.com>
2016-07-25 19:43:48 -0700

    update dependencies

--- release/setup.py
+++ release/setup.py
@@ -6,7 +6,7 @@ setup(
     py_modules=["rtool"],
     install_requires=[
         "click>=6.2, <7.0",
-        "twine>=1.6.5, <1.7",
+        "twine>=1.6.5, <1.8",
         "virtualenv>=14.0.5, <15.1",
         "wheel>=0.29.0, <0.30",
         "six>=1.10.0, <1.11",
