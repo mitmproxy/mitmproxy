49ce50d5d043a1d85e6fd34b9305cdb36c1154d4
Maximilian Hils <git@maximilianhils.com>
2016-08-02 19:08:20 -0700

    Revert "Add missing ssl_insecure option"
    This reverts commit 1d33d76bfd489e51c63f1e5075c83c2be81fd56a.

--- mitmproxy/proxy/config.py
+++ mitmproxy/proxy/config.py
@@ -78,7 +78,6 @@ class ProxyConfig:
         self.check_tcp = None
         self.certstore = None
         self.clientcerts = None
-        self.ssl_insecure = False
         self.openssl_verification_mode_server = None
         self.configure(options, set(options.keys()))
         options.changed.connect(self.configure)
