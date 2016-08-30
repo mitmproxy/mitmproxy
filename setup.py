09d60b2fa30bd50245d51019ded79319d4a60a1b
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2016-08-30 20:36:18 +0200

    bump brotli dependency

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
         "tornado>=4.3, <4.5",
         "urwid>=1.3.1, <1.4",
         "watchdog>=0.8.3, <0.9",
-        "brotlipy>=0.3.0, <0.5",
+        "brotlipy>=0.5.1, <0.6",
     ],
     extras_require={
         ':sys_platform == "win32"': [
