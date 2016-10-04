f4201c87e42eb44f64b27468d20e939e2e823b62
Shane Bradfield <genavion@gmail.com>
2016-10-04 14:53:34 -0400

    Delete modify_form.py

--- examples/modify_form.py
+++
@@ -1,8 +0,0 @@
-def request(flow):
-    if flow.request.urlencoded_form:
-        flow.request.urlencoded_form["mitmproxy"] = "rocks"
-    else:
-        # This sets the proper content type and overrides the body.
-        flow.request.urlencoded_form = [
-            ("foo", "bar")
-        ]
