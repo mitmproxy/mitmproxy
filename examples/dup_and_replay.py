96268db00a5fa8856076d0c59974dae9b538388b
Shane Bradfield <genavion@gmail.com>
2016-10-04 14:52:43 -0400

    Delete dup_and_replay.py

--- examples/dup_and_replay.py
+++
@@ -1,7 +0,0 @@
-from mitmproxy import master
-
-
-def request(flow):
-    f = master.duplicate_flow(flow)
-    f.request.path = "/changed"
-    master.replay_request(f, block=True, run_scripthooks=False)
