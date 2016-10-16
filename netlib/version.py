5db860a334fbcb6230d73de8ca66492f73a6ce80
Aldo Cortesi <aldo@nullcube.com>
2016-10-16 23:52:47 +1300

    Bump to 0.18.1 to fix pypi obstreperousness
    Also include contriburors update.

--- netlib/version.py
+++ netlib/version.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, print_function, division)
 
-IVERSION = (0, 18)
+IVERSION = (0, 18, 1)
 VERSION = ".".join(str(i) for i in IVERSION)
 PATHOD = "pathod " + VERSION
 MITMPROXY = "mitmproxy " + VERSION
