18365fd790612877e798decf322c1151c9e34ab4
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-10-23 16:12:18 +0530

    Updated test suite to work with functools.lru_cache

--- test/mitmproxy/test_utils_lrucache.py
+++ test/mitmproxy/test_utils_lrucache.py
@@ -1,34 +1,33 @@
-from mitmproxy.utils import lrucache
-
+from functools import lru_cache
 
 def test_LRUCache():
-    cache = lrucache.LRUCache(2)
 
     class Foo:
         ran = False
 
+        @lru_cache(maxsize=2)
         def gen(self, x):
             self.ran = True
             return x
     f = Foo()
 
     assert not f.ran
-    assert cache.get(f.gen, 1) == 1
+    assert f.gen(1) == 1
     assert f.ran
     f.ran = False
-    assert cache.get(f.gen, 1) == 1
+    assert f.gen(1) == 1
     assert not f.ran
 
     f.ran = False
-    assert cache.get(f.gen, 1) == 1
+    assert f.gen(1) == 1
     assert not f.ran
-    assert cache.get(f.gen, 2) == 2
-    assert cache.get(f.gen, 3) == 3
+    assert f.gen(2) == 2
+    assert f.gen(3) == 3
     assert f.ran
 
     f.ran = False
-    assert cache.get(f.gen, 1) == 1
+    assert f.gen(1) == 1
     assert f.ran
-
-    assert len(cache.cacheList) == 2
-    assert len(cache.cache) == 2
+    #print(f.gen.cache_info())
+    #assert len(cache.cacheList) == 2
+    #assert len(cache.cache) == 2
