fd263f13ba0e3b5d86a61274037bb6372cc64bce
Maximilian Hils <git@maximilianhils.com>
2016-11-20 15:28:17 +0100

    minor changes to sync server

--- mitmproxy/proxy/protocol2/server.py
+++ mitmproxy/proxy/protocol2/server.py
@@ -11,29 +11,26 @@ from typing import MutableMapping
 from mitmproxy.proxy.protocol2 import events
 from mitmproxy.proxy.protocol2.context import Connection
 from mitmproxy.proxy.protocol2.context import Context, Client
+from mitmproxy.proxy.protocol2.events import Event
+from mitmproxy.proxy.protocol2.layer import Layer
 from mitmproxy.proxy.protocol2.reverse_proxy import ReverseProxy
 
 
-class TCPServer:
-    def __init__(self, address):
+class ConnectionHandler:
+    def __init__(self, sel: selectors.BaseSelector, sock: socket.socket, addr: tuple) -> None:
         self.connections = {}  # type: MutableMapping[Connection, socket.socket]
+        self.sel = sel
+        self.sock = sock
+        self.addr = addr
 
-        self.sel = selectors.DefaultSelector()
-        sock = socket.socket()
-        sock.bind(address)
-        sock.listen(100)
-        sock.setblocking(False)
-        self.sel.register(sock, selectors.EVENT_READ, self.accept)
-
-    def accept(self, server_sock, mask):
-        sock, addr = server_sock.accept()  # Should be ready
         sock.setblocking(False)
 
         client = Client(addr)
         context = Context(client)
+
         layer = ReverseProxy(context, ("example.com", 80))
 
-        self.receive_event(layer, events.Start())
+        self.server_event(layer, events.OpenConnection(client))
 
         callback = functools.partial(self.read, layer, client)
 
@@ -45,17 +42,17 @@ class TCPServer:
 
         self.connections[client] = sock
 
-    def read(self, layer, conn, sock, mask):
-        data = sock.recv(4096)  # Should be ready
+    def read(self, layer: Layer, conn: Connection, sock: socket.socket, mask: int):
+        data = sock.recv(4096)
         if data:
-            self.receive_event(layer, events.ReceiveData(conn, data))
+            self.server_event(layer, events.ReceiveData(conn, data))
         else:
             # TODO: Needs proper teardown.
             self.sel.unregister(sock)
             sock.close()
-            self.receive_event(layer, events.CloseConnection(conn))
+            self.server_event(layer, events.CloseConnection(conn))
 
-    def receive_event(self, layer, event):
+    def server_event(self, layer: Layer, event: Event):
         print(">>", event)
         layer_events = layer.handle_event(event)
         for event in layer_events:
@@ -79,14 +76,41 @@ class TCPServer:
             else:
                 raise NotImplementedError("Unexpected event: {}".format(event))
 
+
+class TCPServer:
+    def __init__(self, address):
+        self.connections = set()
+        self.server_sock = socket.socket()
+        self.server_sock.bind(address)
+        self.server_sock.listen(100)
+        self.server_sock.setblocking(False)
+
+        self.sel = selectors.DefaultSelector()
+        self.sel.register(self.server_sock, selectors.EVENT_READ, self.accept)
+
+    def accept(self, server_sock, mask):
+        sock, addr = server_sock.accept()  # Should be ready
+        connection = ConnectionHandler(self.sel, sock, addr)
+        self.connections.add(connection)
+
     def run(self):
         while True:
-            events = self.sel.select()
-            for key, mask in events:
-                callback = key.data
-                callback(key.fileobj, mask)
+            self.tick(None)
+
+    def tick(self, timeout):
+        for key, mask in self.sel.select(timeout):
+            callback = key.data
+            callback(key.fileobj, mask)
 
 
 if __name__ == '__main__':
     s = TCPServer(('', 8080))
     s.run()
+
+"""
+1) full async
+uh?
+2) notifier
+
+3) thread per connection
+"""
