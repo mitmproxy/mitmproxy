346d1b320a3d47c632e34dc8d59e88701ac91e65
Maximilian Hils <git@maximilianhils.com>
2016-11-22 04:28:09 +0100

    implement basic tls layer

--- mitmproxy/proxy/protocol2/tcp.py
+++ mitmproxy/proxy/protocol2/tcp.py
@@ -1,55 +1,12 @@
 import functools
+from warnings import warn
 
 from mitmproxy.proxy.protocol2 import events
 from mitmproxy.proxy.protocol2.context import ClientServerContext
 from mitmproxy.proxy.protocol2.events import TEventGenerator
 from mitmproxy.proxy.protocol2.layer import Layer
-
-"""
-Utility decorators that help build state machines
-"""
-
-
-def defer(event_type):
-    """
-    Queue up the events matching the specified event type and emit them immediately
-    after the state has changed.
-    """
-
-    def decorator(f):
-        deferred = []
-
-        @functools.wraps(f)
-        def wrapper(self, event: events.Event):
-            if isinstance(event, event_type):
-                deferred.append(event)
-            else:
-                yield from f(self, event)
-                if self.state != f:
-                    for event in deferred:
-                        yield from self.state(event)
-                    deferred.clear()
-
-        return wrapper
-
-    return decorator
-
-
-def exit_on_close(f):
-    """
-    Stop all further interaction once a single close event has been observed.
-    """
-    closed = False
-
-    @functools.wraps(f)
-    def wrapper(self, event: events.Event):
-        nonlocal closed
-        if isinstance(event, events.CloseConnection):
-            closed = True
-        if not closed:
-            yield from f(self, event)
-
-    return wrapper
+from mitmproxy.proxy.protocol2.utils import defer, only
+from mitmproxy.proxy.protocol2.utils import exit_on_close
 
 
 class TCPLayer(Layer):
@@ -62,35 +19,27 @@ class TCPLayer(Layer):
     def handle_event(self, event: events.Event) -> TEventGenerator:
         yield from self.state(event)
 
-    def start(self, event: events.Event) -> TEventGenerator:
-        if isinstance(event, events.OpenConnection):
-            if not self.context.server.connected:
-                yield events.OpenConnection(self.context.server)
-                self.state = self.wait_for_open
-            else:
-                self.state = self.relay_messages
+    @only(events.Start)
+    def start(self, _) -> TEventGenerator:
+        if not self.context.server.connected:
+            yield events.OpenConnection(self.context.server)
+            self.state = self.wait_for_open
         else:
-            raise TypeError("Unexpected event: {}".format(event))
+            self.state = self.relay_messages
 
     @defer(events.ReceiveData)
     @exit_on_close
-    def wait_for_open(self, event: events.Event) -> TEventGenerator:
-        if isinstance(event, events.OpenConnection):
-            # connection is now open
-            self.state = self.relay_messages
-        else:
-            raise TypeError("Unexpected event: {}".format(event))
-            # noinspection PyUnreachableCode
-            yield
+    @only(events.OpenConnection)
+    def wait_for_open(self, _) -> TEventGenerator:
+        self.state = self.relay_messages
+        yield from []
 
+    @only(events.ReceiveData, events.CloseConnection)
     def relay_messages(self, event: events.Event) -> TEventGenerator:
-        if isinstance(event, events.ReceiveData):
-            if event.connection == self.context.client:
-                dst = self.context.server
-            else:
-                dst = self.context.client
-            yield events.SendData(dst, event.data)
-        if isinstance(event, events.CloseConnection):
-            pass  # TODO: close other connection here.
-        else:
-            raise TypeError("Unexpected event: {}".format(event))
+        if isinstance(event, events.ReceiveClientData):
+            yield events.SendData(self.context.server, event.data)
+        elif isinstance(event, events.ReceiveServerData):
+            yield events.SendData(self.context.client, event.data)
+        elif isinstance(event, events.CloseConnection):
+            warn("unimplemented: tcp.relay_message:close")
+            # TODO: close other connection here.
