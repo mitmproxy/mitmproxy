123ef043dc7214c9818985aeea565a82c2680c0e
Maximilian Hils <git@maximilianhils.com>
2016-12-10 11:36:32 +0100

    add new xml/html pretty-printer :tada:

---
+++ test/mitmproxy/contentviews/test_xml_html.py
@@ -0,0 +1,29 @@
+import pytest
+
+from mitmproxy.contentviews import xml_html
+from mitmproxy.test import tutils
+from . import full_eval
+
+data = tutils.test_data.push("mitmproxy/contentviews/test_xml_html_data/")
+
+
+def test_simple():
+    v = full_eval(xml_html.ViewXmlHtml())
+    assert v(b"foo") == ('XML', [[('text', 'foo')]])
+    assert v(b"<html></html>") == ('HTML', [[('text', '<html></html>')]])
+
+
+@pytest.mark.parametrize("filename", [
+    "simple.html",
+    "cdata.xml",
+    "comment.xml",
+    "inline.html",
+])
+def test_format_xml(filename):
+    path = data.path(filename)
+    with open(path) as f:
+        input = f.read()
+    with open(path.replace(".", "-formatted.")) as f:
+        expected = f.read()
+    tokens = xml_html.tokenize(input)
+    assert xml_html.format_xml(tokens) == expected
