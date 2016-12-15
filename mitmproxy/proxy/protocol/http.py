61e552553cd2ae03b115756551e73a32ede48663
Aldo Cortesi <aldo@nullcube.com>
2016-12-16 10:54:36 +1300

    Catch occasional protocol errors in regular connect
    Fixes #1843 and #1847

--- mitmproxy/proxy/protocol/http.py
+++ mitmproxy/proxy/protocol/http.py
@@ -182,6 +182,17 @@ class HttpLayer(base.Layer):
 
         try:
             self.set_server((f.request.host, f.request.port))
+
+            if f.response:
+                resp = f.response
+            else:
+                resp = http.make_connect_response(f.request.data.http_version)
+
+            self.send_response(resp)
+
+            if is_ok(resp.status_code):
+                layer = self.ctx.next_layer(self)
+                layer()
         except (
             exceptions.ProtocolException, exceptions.NetlibException
         ) as e:
@@ -191,17 +202,6 @@ class HttpLayer(base.Layer):
             f.error = flow.Error(str(e))
             self.channel.ask("error", f)
             return False
-
-        if f.response:
-            resp = f.response
-        else:
-            resp = http.make_connect_response(f.request.data.http_version)
-
-        self.send_response(resp)
-
-        if is_ok(resp.status_code):
-            layer = self.ctx.next_layer(self)
-            layer()
 
         return False
 
