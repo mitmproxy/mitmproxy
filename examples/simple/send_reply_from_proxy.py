d5553cd4b766ad95e34a8f5929851d90b2ff329e
bb8 <tencrance@gmail.com>
2016-12-17 15:19:47 +0800

    Update send_reply_from_proxy.py
    Update the code to match the 0.18.2 verision 
    old:
    ```python
    from mitmproxy import http
    ```
    
    new:
    ```python
    from mitmproxy.models import http
    ```

--- examples/simple/send_reply_from_proxy.py
+++ examples/simple/send_reply_from_proxy.py
@@ -2,7 +2,8 @@
 This example shows how to send a reply from the proxy immediately
 without sending any data to the remote server.
 """
-from mitmproxy import http
+# from mitmproxy import http  # This line doesn't work on mitmdump/mitmproxy v0.18.2
+from mitmproxy.models import http ## This line work on v0.18.2
 
 
 def request(flow):
