0921d2d68457327df33d64995abf4cb7d084c6ee
Nishanth Hegde <hegde.nishanth@gmail.com>
2017-01-22 19:46:41 +0530

    Prevents some errors printing on stdout

--- mitmproxy/addons/termlog.py
+++ mitmproxy/addons/termlog.py
@@ -5,7 +5,7 @@ from mitmproxy import log
 
 
 class TermLog:
-    def __init__(self, outfile=sys.stdout):
+    def __init__(self, outfile=None):
         self.options = None
         self.outfile = outfile
 
@@ -13,6 +13,11 @@ class TermLog:
         self.options = options
 
     def log(self, e):
+	if self.outfile == None:
+		if log.log_tier(e.level) == log.log_tier("error"):
+			self.outfile = sys.stderr
+		else:
+			self.outfile = sys.stdout		
         if self.options.verbosity >= log.log_tier(e.level):
             click.secho(
                 e.msg,
