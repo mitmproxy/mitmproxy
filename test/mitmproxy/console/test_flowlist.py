df8a5aa9bea73472313a5c6785a5957ea6bbdcb6
lymanZerga11 <lymanZerga11@users.noreply.github.com>
2017-02-12 23:23:23 +0800

    Update test_flowlist.py

--- test/mitmproxy/console/test_flowlist.py
+++ test/mitmproxy/console/test_flowlist.py
@@ -1,19 +1,19 @@
 import mitmproxy.tools.console.flowlist as flowlist
-from mitmproxy.test import tutils
-from mitmproxy.test import tflow
 from mitmproxy.tools import console
 from mitmproxy import proxy
 from mitmproxy import options
-from mitmproxy.tools.console import common
 from .. import mastertest
 import pytest
 from unittest import mock
 
+
 class ScriptError(Exception):
     pass
 
+
 def mock_add_log(message):
     raise ScriptError(message)
+
 
 class TestFlowlist(mastertest.MasterTest):
     def mkmaster(self, **opts):
@@ -21,13 +21,11 @@ class TestFlowlist(mastertest.MasterTest):
             opts["verbosity"] = 1
         o = options.Options(**opts)
         return console.master.ConsoleMaster(o, proxy.DummyServer())
-    
+
     @mock.patch('mitmproxy.tools.console.signals.status_message.send', side_effect = mock_add_log)
-    def test_new_request(self,test_func):
+    def test_new_request(self, test_func):
         m = self.mkmaster()
         x = flowlist.FlowListBox(m)
         with pytest.raises(ScriptError) as e:
             x.new_request("nonexistent url", "GET")
         assert "Invalid URL" in str(e)
-        
-        
