7ce269caae67fbba1eb803f177c04a78bd061181
Matthew Shao <me@matshao.com>
2017-03-22 08:15:59 +0800

    [web] Update Footer.jsx (#2184)

--- web/src/js/components/Footer.jsx
+++ web/src/js/components/Footer.jsx
@@ -48,9 +48,11 @@ function Footer({ settings }) {
                 <span className="label label-success">stream: {formatSize(stream_large_bodies)}</span>
             )}
             <div className="pull-right">
-            <span className="label label-primary" title="HTTP Proxy Server Address">
-                { server ? ((listen_host||"*") + ":" + listen_port) : "" }
-            </span>
+                {server && (
+                    <span className="label label-primary" title="HTTP Proxy Server Address">
+                        {listen_host||"*"}:{listen_port}
+                    </span>
+                )}
             <span className="label label-info" title="Mitmproxy Version">
             v{version}
             </span>
