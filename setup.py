6af72160bf98b58682b8f9fc5aabf51928d2b1d3
Maximilian Hils <git@maximilianhils.com>
2017-04-20 15:29:42 +0200

    [requires.io] dependency update on master branch (#2252)

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
         "kaitaistruct>=0.7, <0.8",
         "passlib>=1.6.5, <1.8",
         "pyasn1>=0.1.9, <0.3",
-        "pyOpenSSL>=16.0, <17.0",
+        "pyOpenSSL>=16.0, <17.1",
         "pyparsing>=2.1.3, <2.3",
         "pyperclip>=1.5.22, <1.6",
         "requests>=2.9.1, <3",
