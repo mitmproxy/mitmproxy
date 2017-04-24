b3a11433389b47c913cfcd3c56a72d39ff6b93ef
Maximilian Hils <git@maximilianhils.com>
2017-04-24 14:10:47 +0200

    integrate readstdin into readfile addon

--- mitmproxy/addons/readfile.py
+++ mitmproxy/addons/readfile.py
@@ -1,4 +1,7 @@
 import os.path
+import typing
+
+import sys
 
 from mitmproxy import ctx
 from mitmproxy import io
@@ -9,30 +12,39 @@ class ReadFile:
     """
         An addon that handles reading from file on startup.
     """
-    def load_flows_file(self, path: str) -> int:
-        path = os.path.expanduser(path)
+    def load_flows(self, fo: typing.IO[bytes]) -> int:
         cnt = 0
+        freader = io.FlowReader(fo)
         try:
-            with open(path, "rb") as f:
-                freader = io.FlowReader(f)
-                for i in freader.stream():
-                    cnt += 1
-                    ctx.master.load_flow(i)
-                return cnt
-        except (IOError, exceptions.FlowReadException) as v:
+            for flow in freader.stream():
+                ctx.master.load_flow(flow)
+                cnt += 1
+        except (IOError, exceptions.FlowReadException) as e:
             if cnt:
-                ctx.log.warn(
-                    "Flow file corrupted - loaded %i flows." % cnt,
-                )
+                ctx.log.warn("Flow file corrupted - loaded %i flows." % cnt)
             else:
                 ctx.log.error("Flow file corrupted.")
-            raise exceptions.FlowReadException(v)
+            raise exceptions.FlowReadException(str(e)) from e
+        else:
+            return cnt
+
+    def load_flows_from_path(self, path: str) -> int:
+        if path == "-":
+            return self.load_flows(sys.stdin.buffer)
+        else:
+            path = os.path.expanduser(path)
+            try:
+                with open(path, "rb") as f:
+                    return self.load_flows(f)
+            except IOError as e:
+                ctx.log.error("Cannot load flows: {}".format(e))
+                raise exceptions.FlowReadException(str(e)) from e
 
     def running(self):
         if ctx.options.rfile:
             try:
-                self.load_flows_file(ctx.options.rfile)
-            except exceptions.FlowReadException as v:
-                raise exceptions.OptionsError(v)
+                self.load_flows_from_path(ctx.options.rfile)
+            except exceptions.FlowReadException as e:
+                raise exceptions.OptionsError(e) from e
             finally:
                 ctx.master.addons.trigger("processing_complete")
--- test/mitmproxy/addons/test_readfile.py
+++ test/mitmproxy/addons/test_readfile.py
@@ -1,62 +1,95 @@
-from mitmproxy.addons import readfile
-from mitmproxy.test import taddons
-from mitmproxy.test import tflow
-from mitmproxy import io
-from mitmproxy import exceptions
+import io
 from unittest import mock
 
 import pytest
 
+import mitmproxy.io
+from mitmproxy import exceptions
+from mitmproxy.addons import readfile
+from mitmproxy.test import taddons
+from mitmproxy.test import tflow
 
-def write_data(path, corrupt=False):
-    with open(path, "wb") as tf:
-        w = io.FlowWriter(tf)
-        for i in range(3):
-            f = tflow.tflow(resp=True)
-            w.add(f)
-        for i in range(3):
-            f = tflow.tflow(err=True)
-            w.add(f)
-        f = tflow.ttcpflow()
-        w.add(f)
-        f = tflow.ttcpflow(err=True)
-        w.add(f)
-        if corrupt:
-            tf.write(b"flibble")
+
+@pytest.fixture
+def data():
+    f = io.BytesIO()
+
+    w = mitmproxy.io.FlowWriter(f)
+    flows = [
+        tflow.tflow(resp=True),
+        tflow.tflow(err=True),
+        tflow.ttcpflow(),
+        tflow.ttcpflow(err=True)
+    ]
+    for flow in flows:
+        w.add(flow)
+
+    f.seek(0)
+    return f
+
+
+@pytest.fixture
+def corrupt_data():
+    f = data()
+    f.seek(0, io.SEEK_END)
+    f.write(b"qibble")
+    f.seek(0)
+    return f
 
 
 @mock.patch('mitmproxy.master.Master.load_flow')
-def test_configure(mck, tmpdir):
-
+def test_configure(mck, tmpdir, data, corrupt_data):
     rf = readfile.ReadFile()
     with taddons.context() as tctx:
-        tf = str(tmpdir.join("tfile"))
-        write_data(tf)
+        tf = tmpdir.join("tfile")
+
+        tf.write(data.getvalue())
         tctx.configure(rf, rfile=str(tf))
         assert not mck.called
         rf.running()
         assert mck.called
 
-        write_data(tf, corrupt=True)
+        tf.write(corrupt_data.getvalue())
         tctx.configure(rf, rfile=str(tf))
         with pytest.raises(exceptions.OptionsError):
             rf.running()
 
 
 @mock.patch('mitmproxy.master.Master.load_flow')
-def test_corruption(mck, tmpdir):
+@mock.patch('sys.stdin')
+def test_configure_stdin(stdin, load_flow, data, corrupt_data):
+    rf = readfile.ReadFile()
+    with taddons.context() as tctx:
+        stdin.buffer = data
+        tctx.configure(rf, rfile="-")
+        assert not load_flow.called
+        rf.running()
+        assert load_flow.called
 
+        stdin.buffer = corrupt_data
+        tctx.configure(rf, rfile="-")
+        with pytest.raises(exceptions.OptionsError):
+            rf.running()
+
+
+@mock.patch('mitmproxy.master.Master.load_flow')
+def test_corrupt(mck, corrupt_data):
     rf = readfile.ReadFile()
     with taddons.context() as tctx:
         with pytest.raises(exceptions.FlowReadException):
-            rf.load_flows_file("nonexistent")
+            rf.load_flows(io.BytesIO(b"qibble"))
         assert not mck.called
         assert len(tctx.master.logs) == 1
 
-        tfc = str(tmpdir.join("tfile"))
-        write_data(tfc, corrupt=True)
-
         with pytest.raises(exceptions.FlowReadException):
-            rf.load_flows_file(tfc)
+            rf.load_flows(corrupt_data)
         assert mck.called
         assert len(tctx.master.logs) == 2
+
+
+def test_nonexisting_file():
+    rf = readfile.ReadFile()
+    with taddons.context() as tctx:
+        with pytest.raises(exceptions.FlowReadException):
+            rf.load_flows_from_path("nonexistent")
+        assert len(tctx.master.logs) == 1
