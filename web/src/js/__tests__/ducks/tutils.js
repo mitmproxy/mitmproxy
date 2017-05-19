5df0ddf358367dcd37c934798464ad91f3c7bdf8
Matthew Shao <me@matshao.com>
2017-05-19 09:49:10 +0800

    [web] Updates test to use the new _tflow.js.

--- web/src/js/__tests__/ducks/tutils.js
+++ web/src/js/__tests__/ducks/tutils.js
@@ -7,30 +7,3 @@ export function createStore(parts) {
         applyMiddleware(...[thunk])
     )
 }
-
-export function TFlow(intercepted=false, marked=false, modified=false) {
-    return {
-        intercepted ,
-        marked,
-        modified,
-        id: "foo",
-        request: {
-            scheme: 'http',
-            is_replay: true,
-            method: 'GET',
-            contentLength: 100
-        },
-        response: {
-            status_code: 200,
-            headers: [["Content-Type", 'text/html']],
-            timestamp_end: 200
-        },
-        error: {
-            msg: ''
-        },
-        server_conn: {
-            timestamp_start: 100
-        },
-        type: 'http'
-    }
-}
