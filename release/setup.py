8861da5af39efcd3a6dacd7b7df895c47468d0d4
Maximilian Hils <git@maximilianhils.com>
2017-05-26 14:11:31 +0200

    bump cryptography

--- release/setup.py
+++ release/setup.py
@@ -8,7 +8,7 @@ setup(
         "click>=6.2, <7.0",
         "twine>=1.6.5, <1.9",
         "pysftp==0.2.8",
-        "cryptography>=1.6, <1.7",
+        "cryptography>=1.6, <1.9",
     ],
     entry_points={
         "console_scripts": [
