465178323f335ef446760854d4f9a46731397dc1
Maximilian Hils <git@maximilianhils.com>
2017-06-02 13:26:21 +0200

    explain codemirror rendering shortcomins

--- web/src/js/__tests__/components/ContentView/CodeEditorSpec.js
+++ web/src/js/__tests__/components/ContentView/CodeEditorSpec.js
@@ -10,7 +10,11 @@ describe('CodeEditor Component', () => {
             <CodeEditor content={content} onChange={changeFn}/>
         ),
         tree = codeEditor.toJSON()
+    
     it('should render correctly', () => {
+        // This actually does not render properly, but getting a full CodeMirror rendering
+        // is cumbersome. This is hopefully good enough.
+        // see: https://github.com/mitmproxy/mitmproxy/pull/2365#discussion_r119766850
         expect(tree).toMatchSnapshot()
     })
 
