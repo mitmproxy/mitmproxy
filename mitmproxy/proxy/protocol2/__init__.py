58506e2338a4ef0a4c26f9bdd2ca0b54400fb9d9
Maximilian Hils <git@maximilianhils.com>
2017-06-14 21:12:36 +0200

    wording

--- mitmproxy/proxy/protocol2/__init__.py
+++ mitmproxy/proxy/protocol2/__init__.py
@@ -2,10 +2,10 @@
 Experimental sans-io implementation of mitmproxy's protocol stack.
 
 Most important primitives:
-    - layers: represent protocol layers, e.g. one for tcp, tls, and so on. Layers are stacked, so
+    - layers: represent protocol layers, e.g. one for tcp, tls, and so on. Layers are nested, so
       a typical configuration might be ReverseProxy/TLS/TCP.
     - server: the proxy server does all IO and communication with the mitmproxy master.
-      It creates the top layer for each incoming client connection.
+      It creates the top/outermost layer for each incoming client connection.
     - events: When IO actions occur at the proxy server, they are passed down to the top layer as events.
     - commands: In the other direction, layers can emit commands to higher layers or the proxy server.
       This is used to e.g. send data, request for new connections to be opened, or to use mitmproxy's
