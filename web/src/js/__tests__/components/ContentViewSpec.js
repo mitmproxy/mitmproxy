06a0cabb7a8a338632a1760b91f49bbf0d6b3ff0
Matthew Shao <me@matshao.com>
2017-06-25 21:12:30 +0800

    [web] Add tests for js/components/ContentView.jsx

---
+++ web/src/js/__tests__/components/ContentViewSpec.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import ContentView from '../../components/ContentView'
+import { TStore, TFlow } from '../ducks/tutils'
+import { Provider } from 'react-redux'
+import mockXMLHttpRequest from 'mock-xmlhttprequest'
+
+global.XMLHttpRequest = mockXMLHttpRequest
+
+describe('ContentView Component', () => {
+    let store = TStore()
+
+    it('should render correctly', () => {
+        let tflow = TFlow(),
+            provider = renderer.create(
+            <Provider store={store}>
+                <ContentView flow={tflow} message={tflow.request}/>
+            </Provider>),
+            tree = provider.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('should render correctly with empty content', () => {
+        let tflow = TFlow()
+        tflow.response.contentLength = 0
+        let provider = renderer.create(
+            <Provider store={store}>
+                <ContentView flow={tflow} message={tflow.response} readonly={true}/>
+            </Provider>),
+            tree = provider.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('should render correctly with missing content', () => {
+        let tflow = TFlow()
+        tflow.response.contentLength = null
+        let provider = renderer.create(
+            <Provider store={store}>
+                <ContentView flow={tflow} message={tflow.response} readonly={true}/>
+            </Provider>),
+            tree = provider.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('should render correctly with content too large', () => {
+        let tflow = TFlow()
+        tflow.response.contentLength = 1024 * 1024 * 100
+        let provider = renderer.create(
+            <Provider store={store}>
+                <ContentView
+                    flow={tflow}
+                    message={tflow.response}
+                    readonly={true}
+                    uploadContent={jest.fn()}
+                    onOpenFile={jest.fn()}
+                />
+            </Provider>),
+            tree = provider.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+})
