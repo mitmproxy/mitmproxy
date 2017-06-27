fef17c0706593a6a3f0f333e1d1e65bf5d1c9765
Maximilian Hils <git@maximilianhils.com>
2017-06-27 18:35:33 +0200

    add test playbook placeholders, complete tcp layer

--- mitmproxy/proxy/protocol2/test/test_tcp.py
+++ mitmproxy/proxy/protocol2/test/test_tcp.py
@@ -1,7 +1,7 @@
-from mitmproxy.proxy.protocol2 import tcp
-from mitmproxy.proxy.protocol2.test.tutils import playbook
+from . import tutils
 from .. import commands
 from .. import events
+from .. import tcp
 
 
 def test_open_connection(tctx):
@@ -10,24 +10,35 @@ def test_open_connection(tctx):
     because the server may send data first.
     """
     assert (
-        playbook(tcp.TCPLayer(tctx))
+        tutils.playbook(tcp.TCPLayer(tctx, True))
         << commands.OpenConnection(tctx.server)
     )
 
     tctx.server.connected = True
     assert (
-        playbook(tcp.TCPLayer(tctx))
+        tutils.playbook(tcp.TCPLayer(tctx, True))
         << None
     )
 
 
 def test_simple(tctx):
     """open connection, receive data, send it to peer"""
+    f = tutils.Placeholder()
+    playbook = tutils.playbook(tcp.TCPLayer(tctx))
+
     assert (
-        playbook(tcp.TCPLayer(tctx))
+        playbook
+        << commands.Hook("tcp_start", f)
+        >> events.HookReply(-1, None)
         << commands.OpenConnection(tctx.server)
-        >> events.OpenConnectionReply(-1, "ok")
-        >> events.ClientDataReceived(tctx.client, b"hello!")
+        >> events.OpenConnectionReply(-1, None)
+        >> events.DataReceived(tctx.client, b"hello!")
+        << commands.Hook("tcp_message", f)
+    )
+    assert f().messages[0].content == b"hello!"
+    assert (
+        playbook
+        >> events.HookReply(-1, None)
         << commands.SendData(tctx.server, b"hello!")
     )
 
@@ -37,10 +48,15 @@ def test_receive_data_before_server_connected(tctx):
     assert that data received before a server connection is established
     will still be forwarded.
     """
+    f = tutils.Placeholder()
     assert (
-        playbook(tcp.TCPLayer(tctx))
+        tutils.playbook(tcp.TCPLayer(tctx))
+        << commands.Hook("tcp_start", f)
+        >> events.HookReply(-1, None)
         << commands.OpenConnection(tctx.server)
-        >> events.ClientDataReceived(tctx.client, b"hello!")
-        >> events.OpenConnectionReply(-2, "ok")
+        >> events.DataReceived(tctx.client, b"hello!")
+        >> events.OpenConnectionReply(-2, None)
+        << commands.Hook("tcp_message", f)
+        >> events.HookReply(-1, None)
         << commands.SendData(tctx.server, b"hello!")
     )
