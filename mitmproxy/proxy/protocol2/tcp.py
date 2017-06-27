6eecf3d3cb0dd26e1f973bfcdb5ab85f87804362
Maximilian Hils <git@maximilianhils.com>
2017-06-27 18:35:33 +0200

    add test playbook placeholders, complete tcp layer

--- mitmproxy/proxy/protocol2/tcp.py
+++ mitmproxy/proxy/protocol2/tcp.py
@@ -1,6 +1,6 @@
 import typing
-from warnings import warn
 
+from mitmproxy import tcp, flow
 from mitmproxy.proxy.protocol2 import events, commands
 from mitmproxy.proxy.protocol2.context import ClientServerContext
 from mitmproxy.proxy.protocol2.layer import Layer
@@ -16,27 +16,54 @@ class TCPLayer(Layer):
     # this is like a mini state machine.
     state: typing.Callable[[events.Event], commands.TCommandGenerator]
 
-    def __init__(self, context: ClientServerContext):
-        super().__init__(context)
-        self.state = self.start
+    ignore: bool
+    flow: tcp.TCPFlow
 
-    def handle(self, event: events.Event) -> commands.TCommandGenerator:
-        yield from self.state(event)
+    def __init__(self, context: ClientServerContext, ignore: bool = False):
+        super().__init__(context)
+        self.ignore = ignore
+        self.flow = None
 
     @expect(events.Start)
     def start(self, _) -> commands.TCommandGenerator:
+        if not self.ignore:
+            self.flow = tcp.TCPFlow(self.context.client, self.context.server, True)
+            yield commands.Hook("tcp_start", self.flow)
+
         if not self.context.server.connected:
-            ok = yield commands.OpenConnection(self.context.server)
-        self.state = self.relay_messages
+            err = yield commands.OpenConnection(self.context.server)
+            if err:
+                if not self.ignore:
+                    self.flow.error = flow.Error(err)
+                    yield commands.Hook("tcp_error", self.flow)
+                yield commands.CloseConnection(self.context.client)
+        self._handle_event = self.relay_messages
+
+    _handle_event = start
 
     @expect(events.DataReceived, events.ConnectionClosed)
     def relay_messages(self, event: events.Event) -> commands.TCommandGenerator:
-        if isinstance(event, events.ClientDataReceived):
-            yield commands.SendData(self.context.server, event.data)
+        if isinstance(event, events.DataReceived):
+            from_client = event.connection == self.context.client
+            if from_client:
+                send_to = self.context.server
+            else:
+                send_to = self.context.client
 
-        elif isinstance(event, events.ServerDataReceived):
-            yield commands.SendData(self.context.client, event.data)
+            if not self.ignore:
+                tcp_message = tcp.TCPMessage(from_client, event.data)
+                self.flow.messages.append(tcp_message)
+                yield commands.Hook("tcp_message", self.flow)
+
+            yield commands.SendData(send_to, event.data)
 
         elif isinstance(event, events.ConnectionClosed):
-            warn("unimplemented: tcp.relay_message:close")
-            # TODO: close other connection here.
+            # close everything
+            yield commands.CloseConnection(self.context.client)
+            if not self.ignore:
+                yield commands.Hook("tcp_end", self.flow)
+            self._handle_event = self.done
+
+    @expect(events.DataReceived, events.ConnectionClosed)
+    def done(self, _):
+        pass
