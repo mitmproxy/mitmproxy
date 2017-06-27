23a2409a87640c048cb8c9ed297b03c0f9425a37
Matthew Shao <me@matshao.com>
2017-06-27 22:00:10 +0800

    [web] Add modal to js/ducks/ui/index.js

--- web/src/js/ducks/ui/index.js
+++ web/src/js/ducks/ui/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import flow from './flow'
 import header from './header'
+import modal from './modal'
 
 // TODO: Just move ducks/ui/* into ducks/?
 export default combineReducers({
     flow,
     header,
+    modal
 })
