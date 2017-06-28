a4608c6a93457fdd6d8fe16393476bbe23f3466d
Maximilian Hils <git@maximilianhils.com>
2017-06-28 14:08:56 +0200

    minor fixes

--- mitmproxy/proxy/protocol2/reverse_proxy.py
+++ mitmproxy/proxy/protocol2/reverse_proxy.py
@@ -3,6 +3,7 @@ from mitmproxy.proxy.protocol2.context import ClientServerContext, Context, Serv
 from mitmproxy.proxy.protocol2.events import Event
 from mitmproxy.proxy.protocol2.http import HTTPLayer
 from mitmproxy.proxy.protocol2.layer import Layer
+from mitmproxy.proxy.protocol2.tcp import TCPLayer
 
 
 class ReverseProxy(Layer):
@@ -11,8 +12,8 @@ class ReverseProxy(Layer):
         server = Server(server_addr)
         self.child_context = ClientServerContext(context.client, server)
         # self.child_layer = TLSLayer(self.child_context, True, True)
-        # self.child_layer = TCPLayer(self.child_context, True, True)
-        self.child_layer = HTTPLayer(self.child_context)
+        self.child_layer = TCPLayer(self.child_context, False)
+        # self.child_layer = HTTPLayer(self.child_context)
 
     def _handle_event(self, event: Event) -> TCommandGenerator:
         yield from self.child_layer.handle_event(event)
