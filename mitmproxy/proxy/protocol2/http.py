c896111f2e5fcacb16666a8eb5f9f207b532f353
Maximilian Hils <git@maximilianhils.com>
2017-07-12 14:54:36 +0200

    add websocket stub

--- mitmproxy/proxy/protocol2/http.py
+++ mitmproxy/proxy/protocol2/http.py
@@ -1,8 +1,10 @@
 import typing
 from warnings import warn
 
+import sys
+
 import h11
-from mitmproxy.proxy.protocol2 import events, commands
+from mitmproxy.proxy.protocol2 import events, commands, websocket
 from mitmproxy.proxy.protocol2.context import ClientServerContext
 from mitmproxy.proxy.protocol2.layer import Layer
 from mitmproxy.proxy.protocol2.utils import expect
@@ -28,7 +30,7 @@ class HTTPLayer(Layer):
         def log_event(orig):
             def next_event():
                 e = orig()
-                print(e)
+                print(e, file=sys.__stdout__)
                 return e
 
             return next_event
@@ -56,7 +58,7 @@ class HTTPLayer(Layer):
         if event is h11.NEED_DATA:
             return
         elif isinstance(event, h11.Request):
-            yield commands.Hook("requestheaders", event)
+            yield commands.Log("requestheaders", event)
 
             if self.client_conn.client_is_waiting_for_100_continue:
                 raise NotImplementedError()
@@ -76,7 +78,7 @@ class HTTPLayer(Layer):
                 self.flow_events[0].append(event)
             elif isinstance(event, h11.EndOfMessage):
                 self.flow_events[0].append(event)
-                yield commands.Hook("request", self.flow_events)
+                yield commands.Log("request", self.flow_events)
                 yield from self._send_request()
                 return
             else:
@@ -84,7 +86,12 @@ class HTTPLayer(Layer):
 
     def _send_request(self):
         if not self.context.server.connected:
-            yield commands.OpenConnection(self.context.server)
+            err = yield commands.OpenConnection(self.context.server)
+            if err:
+                yield commands.Log("error", err)
+                yield commands.CloseConnection(self.context.client)
+                self._handle_event = self.done
+                return
         for e in self.flow_events[0]:
             bytes_to_send = self.server_conn.send(e)
             yield commands.SendData(self.context.server, bytes_to_send)
@@ -95,11 +102,18 @@ class HTTPLayer(Layer):
         if event is h11.NEED_DATA:
             return
         elif isinstance(event, h11.Response):
-            yield commands.Hook("responseheaders", event)
+            yield commands.Log("responseheaders", event)
 
             self.flow_events[1].append(event)
             self.state = self.read_response_body
             yield from self.read_response_body()  # there may already be further events.
+        elif isinstance(event, h11.InformationalResponse):
+            if event.status_code == 101:
+                # FIXME: check if this is actually WebSocket
+                child_layer = websocket.WebsocketLayer(self.context, None)
+                yield from child_layer.handle_event(events.Start())
+                self._handle_event = child_layer.handle_event
+                return
         else:
             raise TypeError(f"Unexpected event: {event}")
 
@@ -112,7 +126,7 @@ class HTTPLayer(Layer):
                 self.flow_events[1].append(event)
             elif isinstance(event, h11.EndOfMessage):
                 self.flow_events[1].append(event)
-                yield commands.Hook("response", self.flow_events)
+                yield commands.Log("response", self.flow_events)
                 yield from self._send_response()
                 return
             else:
@@ -128,3 +142,7 @@ class HTTPLayer(Layer):
         self.flow_events = [[], []]
         self.client_conn.start_next_cycle()
         self.server_conn.start_next_cycle()
+
+    @expect(events.DataReceived, events.ConnectionClosed)
+    def done(self, _):
+        yield from ()
