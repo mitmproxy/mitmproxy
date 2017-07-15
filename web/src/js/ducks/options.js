80091c859a5ce5c209eba3eda351daf62c776593
Matthew Shao <me@matshao.com>
2017-07-15 22:54:33 +0800

    [web] Add simple validation and improve UI.

--- web/src/js/ducks/options.js
+++ web/src/js/ducks/options.js
@@ -4,6 +4,7 @@ export const RECEIVE        = 'OPTIONS_RECEIVE'
 export const UPDATE         = 'OPTIONS_UPDATE'
 export const REQUEST_UPDATE = 'REQUEST_UPDATE'
 export const UNKNOWN_CMD    = 'OPTIONS_UNKNOWN_CMD'
+export const ERROR          = 'OPTIONS_ERROR'
 
 const defaultState = {
 
@@ -21,12 +22,26 @@ export default function reducer(state = defaultState, action) {
                 ...action.data,
             }
 
+        case ERROR:
+            return {
+                ...state,
+                ...action.data,
+            }
+
         default:
             return state
     }
 }
 
 export function update(options) {
-    fetchApi.put('/options', options)
-    return { type: REQUEST_UPDATE }
+    let error = ''
+    fetchApi.put('/options', options).then(
+        (response) => {
+            response.text().then(errorMsg => {
+                error = errorMsg
+                console.log(error)
+            })
+        }
+    )
+    return {type: ERROR, error}
 }
