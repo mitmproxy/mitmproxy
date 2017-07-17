ec5061327f56c8154a0a5e2ab169a1f371ec62b7
Matthew Shao <me@matshao.com>
2017-07-17 15:28:06 +0800

    [web] Add reducer and actions for option editor.

--- web/src/js/ducks/ui/index.js
+++ web/src/js/ducks/ui/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import flow from './flow'
 import header from './header'
 import modal from './modal'
+import option from './option'
 
 // TODO: Just move ducks/ui/* into ducks/?
 export default combineReducers({
     flow,
     header,
-    modal
+    modal,
+    option,
 })
