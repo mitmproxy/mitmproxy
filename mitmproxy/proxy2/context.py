c22650c4e97eac7612e76e3334e452692b8cca79
Maximilian Hils <git@maximilianhils.com>
2017-08-07 03:09:57 +0200

    [sans-io] add new tls layer (wip)
    this implementation is considerably more complex that the previous one,
    but supports different client and server states.

--- mitmproxy/proxy2/context.py
+++ mitmproxy/proxy2/context.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Union
 
 from mitmproxy.options import Options
 
@@ -8,22 +8,28 @@ class Connection:
     Connections exposed to the layers only contain metadata, no socket objects.
     """
     address: tuple
-    connected: bool
+    connected: bool = False
+    tls: bool = False
+    alpn: Optional[bytes] = None
 
     def __repr__(self):
         return f"{type(self).__name__}({repr(self.__dict__)})"
 
 
 class Client(Connection):
+    sni: Optional[bytes] = None
+
     def __init__(self, address):
         self.address = address
         self.connected = True
 
 
 class Server(Connection):
+    sni: Union[bytes, bool] = True
+    """True: client SNI, False: no SNI, bytes: custom value"""
+
     def __init__(self, address):
         self.address = address
-        self.connected = False
 
 
 class Context:
