4e456e33101670cc034ea1904f54c3cffa32b861
Maximilian Hils <git@maximilianhils.com>
2017-08-16 01:52:09 +0200

    [sans-io] minor test improvements

--- test/mitmproxy/proxy2/conftest.py
+++ test/mitmproxy/proxy2/conftest.py
@@ -7,7 +7,7 @@ from mitmproxy.proxy2 import context
 @pytest.fixture
 def tctx():
     return context.Context(
-        context.Client("client"),
-        context.Server("server"),
+        context.Client(("client", 1234)),
+        context.Server(("server", 42)),
         options.Options()
     )
