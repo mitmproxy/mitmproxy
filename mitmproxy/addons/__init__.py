2baa2c4049ab6b2801b3836b28b3e9df815a0075
Matthew Shao <me@matshao.com>
2017-08-21 09:27:49 +0800

    [web] Move static viewer from default addon to WebMaster.

--- mitmproxy/addons/__init__.py
+++ mitmproxy/addons/__init__.py
@@ -14,7 +14,6 @@ from mitmproxy.addons import replace
 from mitmproxy.addons import script
 from mitmproxy.addons import serverplayback
 from mitmproxy.addons import setheaders
-from mitmproxy.addons import static_viewer
 from mitmproxy.addons import stickyauth
 from mitmproxy.addons import stickycookie
 from mitmproxy.addons import streambodies
@@ -40,7 +39,6 @@ def default_addons():
         script.ScriptLoader(),
         serverplayback.ServerPlayback(),
         setheaders.SetHeaders(),
-        static_viewer.StaticViewer(),
         stickyauth.StickyAuth(),
         stickycookie.StickyCookie(),
         streambodies.StreamBodies(),
