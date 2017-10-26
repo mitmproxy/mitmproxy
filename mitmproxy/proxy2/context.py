a1efe27106bc5c6b3db4ccde3151072377b6b508
Maximilian Hils <git@maximilianhils.com>
2017-10-26 18:32:03 +0200

    [sans-io] improve logging

--- mitmproxy/proxy2/context.py
+++ mitmproxy/proxy2/context.py
@@ -35,12 +35,6 @@ class Server(Connection):
 class Context:
     """
     Layers get a context object that has all contextual information they require.
-    For now, the only required property is the client connection, with ClientServerContext
-    adding the server connection.
-
-    TODO: We may just use a simple context class that has _all_ attributes we ever think of?
-    Alternatively, we could have a `.get(attr)`, that mimicks what the transparent attribute
-    lookup did in the previous implementation.
     """
 
     client: Client
