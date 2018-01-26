2bcbaaece2b37e13698ce02a0c38f9c331d80842
kira0204 <rshtmudgal@gmail.com>
2018-01-27 04:39:37 +0530

    Added test

--- test/mitmproxy/addons/test_share.py
+++ test/mitmproxy/addons/test_share.py
@@ -1,1 +1,29 @@
-# TODO: Write test
+from unittest import mock
+import http.client
+
+from mitmproxy.test import taddons
+from mitmproxy.test import tflow
+
+from mitmproxy.addons import share
+from mitmproxy.addons import view
+
+
+def test_share_command():
+    with mock.patch('mitmproxy.addons.share.http.client.HTTPConnection') as mock_http:
+        sh = share.Share()
+        with taddons.context() as tctx:
+            sh.share([tflow.tflow(resp=True)])
+            assert sh.res == "URL: share.mitmproxy.org/%s" % sh.u_id
+
+            mock_http.return_value.getresponse.side_effect = http.client.RemoteDisconnected
+            sh.share([tflow.tflow(resp=True)])
+            assert sh.res == "The server couldn\'t fulfill the request."
+
+            mock_http.return_value.request.side_effect = http.client.CannotSendRequest
+            sh.share([tflow.tflow(resp=True)])
+            assert sh.res == "We failed to reach a server."
+
+            v = view.View()
+            tctx.master.addons.add(v)
+            tctx.master.addons.add(sh)
+            tctx.master.commands.call_args("share.flows", ["@shown"])
