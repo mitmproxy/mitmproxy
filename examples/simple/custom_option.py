29ef663aa10a2cef5e6e1d310848d888d0d8e379
Maximilian Hils <git@maximilianhils.com>
2018-02-20 17:14:21 +0100

    add documentation for custom_option.py

--- examples/simple/custom_option.py
+++ examples/simple/custom_option.py
@@ -1,3 +1,13 @@
+"""
+This example shows how addons can register custom options
+that can be configured at startup or during execution
+from the options dialog within mitmproxy.
+
+Example:
+
+$ mitmproxy --set custom=true
+$ mitmproxy --set custom   # shorthand for boolean options
+"""
 from mitmproxy import ctx
 
 
