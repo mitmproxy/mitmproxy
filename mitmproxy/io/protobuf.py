ba144b15bc1a33f71200e54eb4a25894a11d6a53
madt1m <blackjuniper@protonmail.com>
2018-06-22 22:43:05 +0200

    Added initial protobuf definitions

---
+++ mitmproxy/io/protobuf.py
@@ -0,0 +1,11 @@
+
+
+"Just define dumps and loads. Files will be handled later. Or maybe...do it, and DONT use it."
+
+import typing
+
+def dumps(value) -> bytes:
+    pass
+
+def dump(value, file_handle: typing.BinaryIO) -> None:
+    pass
