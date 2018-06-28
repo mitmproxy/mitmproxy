925bba4c3c5c0346f2b6d78ae69bfbe73150d73e
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2018-06-28 10:18:28 +0200

    Python 3.7 is here

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Security",
         "Topic :: Internet",
