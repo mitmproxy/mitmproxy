76581708d80de33f1a6af65524ea0ad9e7ce1be4
Miroslav <ttahabatt@gmail.com>
2018-07-30 03:05:49 +0300

    Pipes bug was fixed. Some minor code reorganization. New style annotation. New test case.

--- mitmproxy/language/parser.py
+++ mitmproxy/language/parser.py
@@ -3,6 +3,7 @@ import typing
 import ply.lex as lex
 import ply.yacc as yacc
 
+import mitmproxy.command  # noqa
 from mitmproxy import exceptions
 from mitmproxy.language.lexer import CommandLanguageLexer
 
@@ -11,10 +12,10 @@ class CommandLanguageParser:
     # the list of possible tokens is always required
     tokens = CommandLanguageLexer.tokens
 
-    def __init__(self, command_manager) -> None:
+    def __init__(self,
+                 command_manager: "mitmproxy.command.CommandManager") -> None:
         self.return_value: typing.Any = None
         self._pipe_value: typing.Any = None
-        self._ready_for_pipe: bool = False
         self.command_manager = command_manager
 
     # Grammar rules
@@ -27,10 +28,10 @@ class CommandLanguageParser:
         """starting_expression : PLAIN_STR
                                | quoted_str
                                | array
-                               | command_call"""
+                               | command_call_no_parentheses
+                               | command_call_with_parentheses"""
         p[0] = p[1]
         self._pipe_value = p[0]
-        self._ready_for_pipe = True
 
     def p_pipes_chain(self, p):
         """pipes_chain : empty
@@ -38,23 +39,23 @@ class CommandLanguageParser:
            pipes_chain : pipes_chain pipe_expression"""
         p[0] = self._create_list(p)
 
-    def p_pipe_expression(self, p):
-        """pipe_expression : PIPE command_call"""
-        p[0] = p[2]
+    def p_pipe_expression_no_parentheses(self, p):
+        """pipe_expression : PIPE COMMAND argument_list
+           pipe_expression : PIPE COMMAND LPAREN argument_list RPAREN"""
+        if len(p) == 4:
+            new_args = [self._pipe_value, *p[3]]
+        else:
+            new_args = [self._pipe_value, *p[4]]
+        p[0] = self.command_manager.call_strings(p[2], new_args)
         self._pipe_value = p[0]
 
-    def p_command_call(self, p):
-        """command_call : command_call_no_parentheses
-                        | command_call_with_parentheses"""
-        p[0] = p[1]
-
-    def p_command_call_no_parentheses(self, p):
+    def p_call_command_no_parentheses(self, p):
         """command_call_no_parentheses : COMMAND argument_list"""
-        p[0] = self._command_call(p[1], p[2])
+        p[0] = self.command_manager.call_strings(p[1], p[2])
 
-    def p_command_call_with_parentheses(self, p):
+    def p_call_command_with_parentheses(self, p):
         """command_call_with_parentheses : COMMAND LPAREN argument_list RPAREN"""
-        p[0] = self._command_call(p[1], p[3])
+        p[0] = self.command_manager.call_strings(p[1], p[3])
 
     def p_argument_list(self, p):
         """argument_list : empty
@@ -87,14 +88,6 @@ class CommandLanguageParser:
         else:
             raise exceptions.CommandError(f"Syntax error at '{p.value}'")
 
-    # Supporting methods
-
-    def _command_call(self, command: str, args: list) -> typing.Any:
-        if self._ready_for_pipe:
-            new_first_argument = self._pipe_value
-            args = [new_first_argument, *args]
-        return self.command_manager.call_strings(command, args)
-
     @staticmethod
     def _create_list(p: yacc.YaccProduction) -> typing.List[typing.Any]:
         if len(p) == 2:
@@ -110,11 +103,13 @@ class CommandLanguageParser:
 
     def parse(self, lexer: lex.Lexer, **kwargs) -> typing.Any:
         self.parser.parse(lexer=lexer, **kwargs)
-        self._pipe_value, self._ready_for_pipe = None, False
+        self._pipe_value = None
         return self.return_value
 
 
-def create_parser(command_manager) -> CommandLanguageParser:
+def create_parser(
+        command_manager: "mitmproxy.command.CommandManager"
+) -> CommandLanguageParser:
     command_parser = CommandLanguageParser(command_manager)
     command_parser.build(debug=False, write_tables=False)
     return command_parser
