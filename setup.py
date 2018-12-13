5250414ceff6c7290e1db1d2e198abd09fc4abb9
requires.io <support@requires.io>
2018-12-13 17:07:39 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ setup(
             "pytest-xdist>=1.22,<2",
             "pytest>=4.0,<5",
             "requests>=2.9.1, <3",
-            "tox>=3.5,<3.6",
+            "tox>=3.6,<3.7",
             "rstcheck>=2.2, <4.0",
         ],
         'examples': [
