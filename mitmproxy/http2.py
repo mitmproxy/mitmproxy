cd068cd4b6b325b9f603fe139ed57a92ad6af700
Josué Tille <josue@tille.ch>
2019-06-09 15:00:59 +0200

    Add new attribute in http2 flow

--- mitmproxy/http2.py
+++ mitmproxy/http2.py
@@ -3,15 +3,17 @@ from mitmproxy.coretypes import serializable
 import time
 from typing import List
 
-import h2.events
+from h2 import events
 
 
 class HTTP2Frame(serializable.Serializable):
 
     def __init__(self, from_client, events, timestamp=None):
-        self.from_client = from_client
-        self.events: List[h2.events.Event] = events
-        self.timestamp = timestamp or time.time()
+        self.from_client: bool = from_client
+        self.events: List[events.Event] = events
+        self.frame_type: str = self._detect_frame_type(events[0])
+        self.stream_ID: int = events[0].stream_id if hasattr(events[0], "stream_id") else 0
+        self.timestamp: float = timestamp or time.time()
 
     @classmethod
     def from_state(cls, state):
@@ -24,11 +26,47 @@ class HTTP2Frame(serializable.Serializable):
         self.from_client, self.events, self.timestamp = state
 
     def __repr__(self):
-        return "<HTTP2Frame: {direction}, events: {events}>".format(
+        return "<HTTP2Frame: {direction}, type: {type}, events: {events}>".format(
             direction="->" if self.from_client else "<-",
+            type=self.frame_type,
             events=repr(self.events)
         )
 
+    def _detect_frame_type(self, event: events.Event) -> str:
+        if isinstance(event, events.RequestReceived):
+            return "HEADER"
+        elif isinstance(event, events.ResponseReceived):
+            return "HEADER"
+        elif isinstance(event, events.TrailersReceived):
+            raise "TRAILER RECEIVED"
+        elif isinstance(event, events.InformationalResponseReceived):
+            raise "INFORMATIONAL RESPONSE RECEIVED"
+        elif isinstance(event, events.DataReceived):
+            return "DATA"
+        elif isinstance(event, events.WindowUpdated):
+            return "WINDOWS UPDATE"
+        elif isinstance(event, events.RemoteSettingsChanged):
+            return "SETTINGS"
+        elif isinstance(event, events.PingReceived):
+            return "PING"
+        elif isinstance(event, events.PingAcknowledged):
+            return "PING"
+        elif isinstance(event, events.StreamReset):
+            return "STREAM RESET"
+        elif isinstance(event, events.PushedStreamReceived):
+            return "PUSH"
+        elif isinstance(event, events.SettingsAcknowledged):
+            return "SETTINGS"
+        elif isinstance(event, events.PriorityUpdated):
+            return "PRIORITY"
+        elif isinstance(event, events.ConnectionTerminated):
+            return "CONNECTION TERMINATED"
+        elif isinstance(event, events.AlternativeServiceAvailable):
+            return "ALTSVC"
+        elif isinstance(event, events.UnknownFrameReceived):
+            return "UNKNOWN"
+        else:
+            return "UNKNOWN"
 
 class HTTP2Flow(flow.Flow):
 
