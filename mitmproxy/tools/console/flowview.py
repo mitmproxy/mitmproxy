b9eae3c138e9e0b4c500f496f02e9ff20820c255
Josué Tille <josue@tille.ch>
2019-06-18 18:38:08 +0200

    Update view for frame

--- mitmproxy/tools/console/flowview.py
+++ mitmproxy/tools/console/flowview.py
@@ -289,69 +289,6 @@ class FlowDetailsHttp2(FlowDetails):
     def view_details(self):
         return flowdetailview.flowdetails(self.view, self.flow)
 
-    def content_view(self, viewmode, message):
-        if message.raw_content is None:
-            msg, body = "", [urwid.Text([("error", "[content missing]")])]
-            return msg, body
-        else:
-            full = self.master.commands.execute("view.http2.settings.getval @focus fullcontents false")
-            if full == "true":
-                limit = sys.maxsize
-            else:
-                limit = ctx.options.content_view_lines_cutoff
-
-            flow_modify_cache_invalidation = hash((
-                message.raw_content,
-                message.headers.fields,
-                getattr(message, "path", None),
-            ))
-            # we need to pass the message off-band because it's not hashable
-            self._get_content_view_message = message
-            return self._get_content_view(viewmode, limit, flow_modify_cache_invalidation)
-
-    @lru_cache(maxsize=200)
-    def _get_content_view(self, viewmode, max_lines, _):
-        message = self._get_content_view_message
-        self._get_content_view_message = None
-        description, lines, error = contentviews.get_message_content_view(
-            viewmode, message
-        )
-        if error:
-            self.master.log.debug(error)
-        # Give hint that you have to tab for the response.
-        if description == "No content" and isinstance(message, http.HTTPRequest):
-            description = "No request content (press tab to view response)"
-
-        # If the users has a wide terminal, he gets fewer lines; this should not be an issue.
-        chars_per_line = 80
-        max_chars = max_lines * chars_per_line
-        total_chars = 0
-        text_objects = []
-        for line in lines:
-            txt = []
-            for (style, text) in line:
-                if total_chars + len(text) > max_chars:
-                    text = text[:max_chars - total_chars]
-                txt.append((style, text))
-                total_chars += len(text)
-                if total_chars == max_chars:
-                    break
-
-            # round up to the next line.
-            total_chars = int(math.ceil(total_chars / chars_per_line) * chars_per_line)
-
-            text_objects.append(urwid.Text(txt))
-            if total_chars == max_chars:
-                text_objects.append(urwid.Text([
-                    ("highlight", "Stopped displaying data after %d lines. Press " % max_lines),
-                    ("key", "f"),
-                    ("highlight", " to load all data.")
-                ]))
-                break
-
-        return description, text_objects
-
-
     def _frame_base(self, frame):
         pass
     def _frame_header(self, frame):
@@ -359,21 +296,16 @@ class FlowDetailsHttp2(FlowDetails):
     def _frame_pushed(self, frame):
         pass
     def _frame_data(self, frame):
-        txt = [
-            urwid.Text(
-                        [
-                            ("highlight", "exemple !!!!"),
-                        ]
-            )
-            
-        ]
-        
-        
-        
-        
-        
+        base_frame_info = [
+                ("Type", frame.frame_type),
+                ("Stream-ID", str(frame.stream_id)),
+                ("Flow controlled length", str(frame.length)),
+                ("End stream", str(frame.end_stream))
+            ]
+        txt = common.format_keyvals(base_frame_info, indent=4)
+        txt.append(urwid.Text([("head", "Data")]))
+        txt.append(urwid.Text([("data", str(frame.data))]))
         return txt
-
 
     def _frame_windows_update(self, frame):
         pass
@@ -423,68 +355,7 @@ class FlowDetailsHttp2(FlowDetails):
             txt = self._frame_base(frame)
         else:
             raise exceptions.TypeError("Unknown frame type: %s" % frame)
-        
-        #if conn:
-            #hdrs = []
-            #for k, v in conn.headers.fields:
-                ## This will always force an ascii representation of headers. For example, if the server sends a
-                ##
-                ##     X-Authors: Made with ❤ in Hamburg
-                ##
-                ## header, mitmproxy will display the following:
-                ##
-                ##     X-Authors: Made with \xe2\x9d\xa4 in Hamburg.
-                ##
-                ## The alternative would be to just use the header's UTF-8 representation and maybe
-                ## do `str.replace("\t", "\\t")` to exempt tabs from urwid's special characters escaping [1].
-                ## That would in some terminals allow rendering UTF-8 characters, but the mapping
-                ## wouldn't be bijective, i.e. a user couldn't distinguish "\\t" and "\t".
-                ## Also, from a security perspective, a mitmproxy user couldn't be fooled by homoglyphs.
-                ##
-                ## 1) https://github.com/mitmproxy/mitmproxy/issues/1833
-                ##    https://github.com/urwid/urwid/blob/6608ee2c9932d264abd1171468d833b7a4082e13/urwid/display_common.py#L35-L36,
-
-                #k = strutils.bytes_to_escaped_str(k) + ":"
-                #v = strutils.bytes_to_escaped_str(v)
-                #hdrs.append((k, v))
-            #txt = common.format_keyvals(
-                #hdrs,
-                #key_format="header"
-            #)
-            #viewmode = self.master.commands.call("console.flowview.mode")
-            #msg, body = self.content_view(viewmode, conn)
-
-            #cols = [
-                #urwid.Text(
-                    #[
-                        #("heading", msg),
-                    #]
-                #),
-                #urwid.Text(
-                    #[
-                        #" ",
-                        #('heading', "["),
-                        #('heading_key', "m"),
-                        #('heading', (":%s]" % viewmode)),
-                    #],
-                    #align="right"
-                #)
-            #]
-            #title = urwid.AttrWrap(urwid.Columns(cols), "heading")
-
-            #txt.append(title)
-            #txt.extend(body)
-        #else:
-            #txt = [
-                #urwid.Text(""),
-                #urwid.Text(
-                    #[
-                        #("highlight", "No response. Press "),
-                        #("key", "e"),
-                        #("highlight", " and edit any aspect to add one."),
-                    #]
-                #)
-            #]
+ 
         return searchable.Searchable(txt)
 
 
