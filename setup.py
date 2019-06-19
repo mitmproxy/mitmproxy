897d7b6d80ccf40dc30260157a3b56e7363c4dd0
requires.io <support@requires.io>
2019-06-20 01:00:35 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
             "asynctest>=0.12.0",
             "flake8>=3.7,<3.8",
             "Flask>=1.0,<1.1",
-            "mypy>=0.701,<0.702",
+            "mypy>=0.710,<0.711",
             "parver>=0.1,<2.0",
             "pytest-asyncio>=0.8",
             "pytest-cov>=2.5.1,<3",
