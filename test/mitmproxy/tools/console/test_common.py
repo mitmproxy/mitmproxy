3d427008fc2b765e2ca6985fb86716b3309fea89
Josué Tille <josue@tille.ch>
2019-07-02 16:11:15 +0200

    Add some tests for frame formating in common

--- test/mitmproxy/tools/console/test_common.py
+++ test/mitmproxy/tools/console/test_common.py
@@ -11,6 +11,12 @@ def test_format_flow():
     assert common.format_item(f, True, extended=True)
 
 
+def test_format_http2_flow():
+    f = tflow.thttp2flow()
+    for m in f.messages:
+        assert common.format_http2_item(m, True)
+
+
 def test_format_keyvals():
     assert common.format_keyvals(
         [
@@ -30,5 +36,28 @@ def test_format_keyvals():
     assert common.format_keyvals(
         [
             ("aa", wrapped)
+        ]
+    )
+
+
+def test_format_keyvals_with_index():
+    assert common.format_keyvals(
+        [
+            ("0", "aa", "bb"),
+            ("2", "cc", "dd"),
+            ("4", "ee", None),
+        ]
+    )
+    wrapped = urwid.BoxAdapter(
+        urwid.ListBox(
+            urwid.SimpleFocusListWalker(
+                common.format_keyvals([("10", "foo", "bar")])
+            )
+        ), 1
+    )
+    assert wrapped.render((30, ))
+    assert common.format_keyvals(
+        [
+            ("25", "aa", wrapped)
         ]
     )
