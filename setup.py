5b08e83865cfe0e17b8970f1851580b02fde919f
requires.io <support@requires.io>
2019-11-14 18:04:28 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         "ruamel.yaml>=0.16,<0.17",
         "sortedcontainers>=2.1.0,<2.2",
         "tornado>=4.3,<7",
-        "urwid>=2.0.1,<2.1",
+        "urwid>=2.1,<2.2",
         "wsproto>=0.15,<0.16",
         "publicsuffix2>=2.20190812,<3",
         "zstandard>=0.11.0,<0.13.0",
