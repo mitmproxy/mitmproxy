056a9024ec53aecb2c9f4f1a0f1ec3d099d80518
Yoav Shai <gergesh@gmail.com>
2019-12-05 22:36:26 +0200

    Add message to ContentView metadata

--- mitmproxy/contentviews/__init__.py
+++ mitmproxy/contentviews/__init__.py
@@ -103,6 +103,7 @@ def get_message_content_view(viewname, message):
         metadata["query"] = message.query
     if isinstance(message, http.Message):
         metadata["headers"] = message.headers
+    metadata["message"] = message
 
     description, lines, error = get_content_view(
         viewmode, content, **metadata
