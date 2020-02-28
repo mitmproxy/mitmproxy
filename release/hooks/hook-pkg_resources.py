002836adeccada1373961c2e7369e614537ded73
Maximilian Hils <git@maximilianhils.com>
2020-02-28 12:42:41 +0100

    fix binary builds
    See https://github.com/pypa/setuptools/issues/1963 for details.

---
+++ release/hooks/hook-pkg_resources.py
@@ -0,0 +1,5 @@
+# temporary fix for https://github.com/pypa/setuptools/issues/1963
+# can be removed when we upgrade to PyInstaller 3.7.
+hiddenimports = collect_submodules('pkg_resources._vendor')
+hiddenimports.append('pkg_resources.py2_warn')
+excludedimports = ['__main__']
