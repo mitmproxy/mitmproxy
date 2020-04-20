22a6a7a9d7eaa1f20605e4b6adb00eda4977c1e5
requires.io <support@requires.io>
2020-04-20 23:53:04 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
             "Flask>=1.0,<1.2",
             "hypothesis>=5.10,<5.11",
             "parver>=0.1,<2.0",
-            "pytest-asyncio>=0.10.0,<0.11",
+            "pytest-asyncio>=0.11,<0.12",
             "pytest-cov>=2.7.1,<3",
             "pytest-timeout>=1.3.3,<2",
             "pytest-xdist>=1.29,<2",
