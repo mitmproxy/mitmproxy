5ce87b72537ae9b6dca73159549444bd4f28e186
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2020-05-03 10:28:43 +0300

    Assert viewmode not None

--- mitmproxy/contentviews/__init__.py
+++ mitmproxy/contentviews/__init__.py
@@ -121,6 +121,9 @@ def get_tcp_content_view(viewname: str, data: bytes):
     if not viewmode:
         viewmode = get("auto")
 
+    # https://github.com/mitmproxy/mitmproxy/pull/3970#issuecomment-623024447
+    assert viewmode
+
     description, lines, error = get_content_view(viewmode, data)
 
     return description, lines, error
