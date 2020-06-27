ebbba73281dceae4c486d62ab7b33dabd82cbfbb
sanlengjingvv <lankeren123543@gmail.com>
2020-06-27 15:41:14 +0800

    serialize trailers for ws broadcast

--- mitmproxy/tools/web/app.py
+++ mitmproxy/tools/web/app.py
@@ -93,6 +93,8 @@ def flow_to_json(flow: mitmproxy.flow.Flow) -> dict:
                 "timestamp_end": flow.response.timestamp_end,
                 "is_replay": flow.response.is_replay,
             }
+        if flow.trailers:
+            f["trailers"] = tuple(flow.trailers.items(True))
     f.get("server_conn", {}).pop("cert", None)
     f.get("client_conn", {}).pop("mitmcert", None)
 
