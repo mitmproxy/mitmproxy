0ee0cf566817fe842d3997445f6d891a5a1a90f6
Maximilian Hils <git@maximilianhils.com>
2020-07-04 00:19:43 +0200

    minor addon improvements, fix tests

--- mitmproxy/addons/mapremote.py
+++ mitmproxy/addons/mapremote.py
@@ -1,8 +1,7 @@
-import os
 import re
 import typing
 
-from mitmproxy import exceptions
+from mitmproxy import exceptions, http
 from mitmproxy import ctx
 from mitmproxy.addons.modifyheaders import parse_modify_spec, ModifySpec
 
@@ -26,40 +25,26 @@ class MapRemote:
             self.replacements = []
             for option in ctx.options.map_remote:
                 try:
-                    spec = parse_modify_spec(option)
-                    try:
-                        re.compile(spec.subject)
-                    except re.error:
-                        raise ValueError(f"Invalid regular expression: {spec.subject}")
+                    spec = parse_modify_spec(option, True)
                 except ValueError as e:
-                    raise exceptions.OptionsError(
-                        f"Cannot parse map_remote option {option}: {e}"
-                    ) from e
+                    raise exceptions.OptionsError(f"Cannot parse map_remote option {option}: {e}") from e
 
                 self.replacements.append(spec)
 
-    def request(self, flow):
-        if not flow.reply.has_message:
-            for spec in self.replacements:
-                if spec.matches(flow):
-                    self.replace(flow.request, spec.subject, spec.replacement)
+    def request(self, flow: http.HTTPFlow) -> None:
+        if flow.reply and flow.reply.has_message:
+            return
+        for spec in self.replacements:
+            if spec.matches(flow):
+                try:
+                    replacement = spec.read_replacement()
+                except IOError as e:
+                    ctx.log.warn(f"Could not read replacement file: {e}")
+                    continue
 
-    def replace(self, obj, search, repl):
-        """
-        Replaces all matches of the regex search in the url of the request with repl.
-
-        Returns:
-            The number of replacements made.
-        """
-        if repl.startswith(b"@"):
-            path = os.path.expanduser(repl[1:])
-            try:
-                with open(path, "rb") as f:
-                    repl = f.read()
-            except IOError:
-                ctx.log.warn("Could not read replacement file: %s" % repl)
-                return
-
-        replacements = 0
-        obj.url, replacements = re.subn(search, repl, obj.pretty_url.encode("utf8", "surrogateescape"), flags=re.DOTALL)
-        return replacements
+                url = flow.request.pretty_url.encode("utf8", "surrogateescape")
+                new_url = re.sub(spec.subject, replacement, url)
+                # this is a bit messy: setting .url also updates the host header,
+                # so we really only do that if the replacement affected the URL.
+                if url != new_url:
+                    flow.request.url = new_url
--- mitmproxy/addons/modifyheaders.py
+++ mitmproxy/addons/modifyheaders.py
@@ -1,26 +1,39 @@
-import os
+import re
 import typing
+from pathlib import Path
 
-from mitmproxy import exceptions
+from mitmproxy import exceptions, http
 from mitmproxy import flowfilter
+from mitmproxy.net.http import Headers
 from mitmproxy.utils import strutils
 from mitmproxy import ctx
 
 
 class ModifySpec(typing.NamedTuple):
-    """
-        match_str: a string specifying a flow filter pattern.
-        matches: the parsed match_str as a flowfilter.TFilter object
-        subject: a header name for ModifyHeaders and a regex pattern for ModifyBody
-        replacement: the replacement string
-    """
-    match_str: str
     matches: flowfilter.TFilter
     subject: bytes
-    replacement: bytes
+    replacement_str: str
+
+    def read_replacement(self) -> bytes:
+        """
+        Process the replacement str. This usually just involves converting it to bytes.
+        However, if it starts with `@`, we interpret the rest as a file path to read from.
+
+        Raises:
+            - IOError if the file cannot be read.
+        """
+        if self.replacement_str.startswith("@"):
+            return Path(self.replacement_str[1:]).expanduser().read_bytes()
+        else:
+            # We could cache this at some point, but unlikely to be a problem.
+            return strutils.escaped_str_to_bytes(self.replacement_str)
 
 
-def parse_modify_spec(option) -> ModifySpec:
+def _match_all(flow) -> bool:
+    return True
+
+
+def parse_modify_spec(option, subject_is_regex: bool) -> ModifySpec:
     """
         The form for the modify_* options is as follows:
 
@@ -48,24 +61,31 @@ def parse_modify_spec(option) -> ModifySpec:
     sep, rem = option[0], option[1:]
     parts = rem.split(sep, 2)
     if len(parts) == 2:
-        flow_filter_pattern = ".*"
+        flow_filter = _match_all
         subject, replacement = parts
     elif len(parts) == 3:
         flow_filter_pattern, subject, replacement = parts
+        flow_filter = flowfilter.parse(flow_filter_pattern)  # type: ignore
+        if not flow_filter:
+            raise ValueError(f"Invalid filter pattern: {flow_filter_pattern}")
     else:
         raise ValueError("Invalid number of parameters (2 or 3 are expected)")
 
-    flow_filter = flowfilter.parse(flow_filter_pattern)
-    if not flow_filter:
-        raise ValueError(f"Invalid filter pattern: {flow_filter_pattern}")
-
     subject = strutils.escaped_str_to_bytes(subject)
-    replacement = strutils.escaped_str_to_bytes(replacement)
+    if subject_is_regex:
+        try:
+            re.compile(subject)
+        except re.error as e:
+            raise ValueError(f"Invalid regular expression {subject!r} ({e})")
 
-    if replacement.startswith(b"@") and not os.path.isfile(os.path.expanduser(replacement[1:])):
-        raise ValueError(f"Invalid file path: {replacement[1:]}")
+    spec = ModifySpec(flow_filter, subject, replacement)
 
-    return ModifySpec(flow_filter_pattern, flow_filter, subject, replacement)
+    try:
+        spec.read_replacement()
+    except IOError as e:
+        raise ValueError(f"Invalid file path: {replacement[1:]} ({e})")
+
+    return spec
 
 
 class ModifyHeaders:
@@ -87,34 +107,10 @@ class ModifyHeaders:
         if "modify_headers" in updated:
             for option in ctx.options.modify_headers:
                 try:
-                    spec = parse_modify_spec(option)
+                    spec = parse_modify_spec(option, False)
                 except ValueError as e:
-                    raise exceptions.OptionsError(
-                        f"Cannot parse modify_headers option {option}: {e}"
-                    ) from e
+                    raise exceptions.OptionsError(f"Cannot parse modify_headers option {option}: {e}") from e
                 self.replacements.append(spec)
-
-    def run(self, flow, hdrs):
-        # unset all specified headers
-        for spec in self.replacements:
-            if spec.matches(flow):
-                hdrs.pop(spec.subject, None)
-
-        # set all specified headers if the replacement string is not empty
-        for spec in self.replacements:
-            if spec.replacement.startswith(b"@"):
-                path = os.path.expanduser(spec.replacement[1:])
-                try:
-                    with open(path, "rb") as file:
-                        replacement = file.read()
-                except IOError:
-                    ctx.log.warn(f"Could not read replacement file {path}")
-                    return
-            else:
-                replacement = spec.replacement
-
-            if spec.matches(flow) and replacement:
-                hdrs.add(spec.subject, replacement)
 
     def request(self, flow):
         if not flow.reply.has_message:
@@ -123,3 +119,21 @@ class ModifyHeaders:
     def response(self, flow):
         if not flow.reply.has_message:
             self.run(flow, flow.response.headers)
+
+    def run(self, flow: http.HTTPFlow, hdrs: Headers) -> None:
+        # unset all specified headers
+        for spec in self.replacements:
+            if spec.matches(flow):
+                hdrs.pop(spec.subject, None)
+
+        # set all specified headers if the replacement string is not empty
+        for spec in self.replacements:
+            if spec.matches(flow):
+                try:
+                    replacement = spec.read_replacement()
+                except IOError as e:
+                    ctx.log.warn(f"Could not read replacement file: {e}")
+                    continue
+                else:
+                    if replacement:
+                        hdrs.add(spec.subject, replacement)
