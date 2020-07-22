647eab557baaf071d9237e0561f4ff45d03766ac
Maximilian Hils <git@maximilianhils.com>
2020-07-22 18:28:40 +0200

    lint!

--- test/mitmproxy/io/test_compat.py
+++ test/mitmproxy/io/test_compat.py
@@ -8,7 +8,7 @@ from mitmproxy import exceptions
     ["dumpfile-011.bin", "https://example.com/"],
     ["dumpfile-018.bin", "https://www.example.com/"],
     ["dumpfile-019.bin", "https://webrv.rtb-seller.com/"],
-    ])
+])
 def test_load(tdata, dumpfile, url):
     with open(tdata.path("mitmproxy/data/" + dumpfile), "rb") as f:
         flow_reader = io.FlowReader(f)
