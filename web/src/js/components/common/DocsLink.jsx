9e4f7d47f741624c7e975881a354206ca6605d48
rugk <rugk+git@posteo.de>
2020-08-06 22:04:33 +0200

    Fix link to anticache docs in mitmweb (#4126)

--- web/src/js/components/common/DocsLink.jsx
+++ web/src/js/components/common/DocsLink.jsx
@@ -6,7 +6,7 @@ DocsLink.propTypes = {
 }
 
 export default function DocsLink({ children, resource }) {
-    let url = `http://docs.mitmproxy.org/en/stable/${resource}`
+    let url = `https://docs.mitmproxy.org/stable/${resource}`
     return (
         <a target="_blank" href={url}>
             {children || <i className="fa fa-question-circle"></i>}
