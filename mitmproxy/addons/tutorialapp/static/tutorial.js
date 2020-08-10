395185fd8d2efc415bfc73dbc3d8aafd3c07e1b2
Martin Plattner <git@mplattner.at>
2020-08-10 20:21:13 +0200

    tutorial addon: add asciinema annotations draft

--- mitmproxy/addons/tutorialapp/static/tutorial.js
+++ mitmproxy/addons/tutorialapp/static/tutorial.js
@@ -14,7 +14,49 @@ for(let i = 0; i < ajaxButtons.length; i++) {
     })
 }
 
+
+var timer;
+
+function playEvent(e) {
+    console.log("playing at " + this.currentTime);
+    var playStartPosition = this.currentTime;
+    var playStartDate = new Date();
+    var annotations = this.annotations;
+
+    timer = setInterval(function(){
+        var now = new Date();
+        updateAnnotations(playStartPosition + (now - playStartDate) / 1000, annotations);
+    }, 250);
+}
+
+function pauseEvent(e) {
+    console.log("paused");
+    if (timer){
+        clearInterval(timer);
+    }
+}
+
+function updateAnnotations(playedTimeSecs, annotations){
+    for(let i = 0; i < annotations.length; i++) {
+        if (annotations[i].getAttribute("data-from") < playedTimeSecs && annotations[i].getAttribute("data-to") > playedTimeSecs) {
+            annotations[i].style.display = "block";
+        }
+        else {
+            annotations[i].style.display = "none";
+        }
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("btn-reload-votes").click();
     $('[data-toggle="tooltip"]').tooltip();
+
+    var asciiPlayers = document.getElementsByTagName('asciinema-player');
+
+    for(let i = 0; i < asciiPlayers.length; i++) {
+        asciiPlayers[i].annotations = asciiPlayers[i].parentElement.getElementsByClassName("annotation");
+        asciiPlayers[i].addEventListener("play", playEvent);
+        asciiPlayers[i].addEventListener('pause', pauseEvent);
+    }
 });
