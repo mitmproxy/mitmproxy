cf0a1b0ce256d9088fa22b62a91eef51de58c386
Maximilian Hils <git@maximilianhils.com>
2020-08-27 16:49:44 +0200

    add py39 trove classifier

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Security",
         "Topic :: Internet :: WWW/HTTP",
