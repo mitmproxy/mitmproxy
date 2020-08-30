e1e15acd375dd4dd6d957a9504ad94c4ca3e88ff
gorogoroumaru <zokutyou2@gmail.com>
2020-08-30 13:10:11 +0000

    make columns configurable and customizable

--- mitmproxy/tools/web/webaddons.py
+++ mitmproxy/tools/web/webaddons.py
@@ -1,6 +1,8 @@
 import webbrowser
 
 from mitmproxy import ctx
+from typing import Sequence
+
 
 
 class WebAddon:
@@ -20,6 +22,10 @@ class WebAddon:
         loader.add_option(
             "web_host", str, "127.0.0.1",
             "Web UI host."
+        )
+        loader.add_option(
+            "web_columns", Sequence[str], ["tls", "icon", "path", "method", "status", "size", "time"],
+            "Columns to show in the flow list"
         )
 
     def running(self):
