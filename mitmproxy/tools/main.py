93ab0230a464c399d49ae8da3df88d936b0c606b
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2020-10-06 23:33:52 +0200

    fix typo

--- mitmproxy/tools/main.py
+++ mitmproxy/tools/main.py
@@ -11,11 +11,11 @@ if sys.version_info < (3, 6):
     # This must be before any mitmproxy imports, as they already break!
     # Keep all other imports below with the 'noqa' magic comment.
     print("#" * 76, file=sys.stderr)
-    print("# mitmproxy requires Python 3.6 or higher!                                 #", file=sys.stderr)
+    print("# mitmproxy requires Python 3.6 or higher!                                  #", file=sys.stderr)
     print("#" + " " * 74 + "#", file=sys.stderr)
-    print("# Please upgrade your Python intepreter or use our mitmproxy binaries from #", file=sys.stderr)
-    print("# https://mitmproxy.org. If your operating system does not include the     #", file=sys.stderr)
-    print("# required Python version, you can try using pyenv or similar tools.       #", file=sys.stderr)
+    print("# Please upgrade your Python interpreter or use our mitmproxy binaries from #", file=sys.stderr)
+    print("# https://mitmproxy.org. If your operating system does not include the      #", file=sys.stderr)
+    print("# required Python version, you can try using pyenv or similar tools.        #", file=sys.stderr)
     print("#" * 76, file=sys.stderr)
     sys.exit(1)
 else:
