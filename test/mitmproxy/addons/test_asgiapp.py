66322c57e11aabc68d629e2c85914ceecf6bea12
Jan Peter Stotz <jan-peter.stotz@sit.fraunhofer.de>
2020-11-06 11:32:19 +0100

    unit test for issue #4270 added

--- test/mitmproxy/addons/test_asgiapp.py
+++ test/mitmproxy/addons/test_asgiapp.py
@@ -1,4 +1,7 @@
+import json
+
 import flask
+from flask import request
 
 from .. import tservers
 from mitmproxy.addons import asgiapp
@@ -9,6 +12,14 @@ tapp = flask.Flask(__name__)
 @tapp.route("/")
 def hello():
     return "testapp"
+
+
+@tapp.route("/parameters")
+def request_check():
+    args = {}
+    for k in request.args.keys():
+        args[k] = request.args[k]
+    return json.dumps(args)
 
 
 @tapp.route("/error")
@@ -37,6 +48,12 @@ class TestApp(tservers.HTTPProxyTest):
         with p.connect():
             ret = p.request("get:'http://testapp/'")
         assert b"testapp" in ret.content
+
+    def test_parameters(self):
+        p = self.pathoc()
+        with p.connect():
+            ret = p.request("get:'http://testapp/parameters?param1=1&param2=2'")
+        assert b'{"param1": "1", "param2": "2"}' == ret.data.content
 
     def test_app_err(self):
         p = self.pathoc()
