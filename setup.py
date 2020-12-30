32d4852122e9614aac22ead7fa09b55e5164592a
Maximilian Hils <git@maximilianhils.com>
2020-12-30 22:56:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
         "urwid>=2.1.1,<2.2",
         "wsproto>=1.0,<1.1",
         "publicsuffix2>=2.20190812,<3",
-        "zstandard>=0.15,<0.16",
+        "zstandard>=0.11,<0.16",
     ],
     extras_require={
         ':sys_platform == "win32"': [
