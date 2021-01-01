0bbb5ca34547998b73842207a5312fbf35b9e2b8
Maximilian Hils <git@maximilianhils.com>
2021-01-01 16:51:37 +0100

    reverse proxy: do not assume TLS for non-standard ports

--- test/mitmproxy/net/test_server_spec.py
+++ test/mitmproxy/net/test_server_spec.py
@@ -3,16 +3,21 @@ import pytest
 from mitmproxy.net import server_spec
 
 
-def test_parse():
-    assert server_spec.parse("example.com") == ("https", ("example.com", 443))
-    assert server_spec.parse("example.com") == ("https", ("example.com", 443))
-    assert server_spec.parse("http://example.com") == ("http", ("example.com", 80))
-    assert server_spec.parse("http://127.0.0.1") == ("http", ("127.0.0.1", 80))
-    assert server_spec.parse("http://[::1]") == ("http", ("::1", 80))
-    assert server_spec.parse("http://[::1]/") == ("http", ("::1", 80))
-    assert server_spec.parse("https://[::1]/") == ("https", ("::1", 443))
-    assert server_spec.parse("http://[::1]:8080") == ("http", ("::1", 8080))
+@pytest.mark.parametrize("spec,out", [
+    ("example.com", ("https", ("example.com", 443))),
+    ("http://example.com", ("http", ("example.com", 80))),
+    ("smtp.example.com:25", ("http", ("smtp.example.com", 25))),
+    ("http://127.0.0.1", ("http", ("127.0.0.1", 80))),
+    ("http://[::1]", ("http", ("::1", 80))),
+    ("http://[::1]/", ("http", ("::1", 80))),
+    ("https://[::1]/", ("https", ("::1", 443))),
+    ("http://[::1]:8080", ("http", ("::1", 8080))),
+])
+def test_parse(spec, out):
+    assert server_spec.parse(spec) == out
 
+
+def test_parse_err():
     with pytest.raises(ValueError, match="Invalid server specification"):
         server_spec.parse(":")
 
