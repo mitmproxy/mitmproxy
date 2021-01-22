ea624ae6e32e198b2b91e91c4d97a7536cdec781
Maximilian Hils <git@maximilianhils.com>
2021-01-22 01:40:08 +0100

    clean doc folder before building, fixes #4405 (#4406)

--- docs/ci.sh
+++ docs/ci.sh
@@ -11,7 +11,7 @@ set -o pipefail
 # Only upload if we have defined credentials - we only have these defined for
 # trusted commits (i.e. not PRs).
 if [[ ! -z "${AWS_ACCESS_KEY_ID}" && $GITHUB_REF == "refs/heads/master" ]]; then
-    aws s3 sync --acl public-read ./public s3://docs.mitmproxy.org/master
+    aws s3 sync --delete --acl public-read ./public s3://docs.mitmproxy.org/master
     aws cloudfront create-invalidation --distribution-id E1TH3USJHFQZ5Q \
         --paths "/master/*"
 fi
