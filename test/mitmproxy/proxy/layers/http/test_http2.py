mitmproxy/mitmproxy

commit 70e08c880cadc5b87eb5187787a65c7857e0750c

don't reuse closed connections, refs #4451 (#4458)

mitmproxy/proxy/layers/http/__init__.py
@@ -628,6 +628,7 @@ class HttpLayer(layer.Layer):
                 h2_to_h1 = self.context.client.alpn == b"h2" and not conn_is_pending_or_h2
                 connection_suitable = (
                     event.connection_spec_matches(connection)
+                    and connection.connected
                     and not h2_to_h1
                 )
                 if connection_suitable:

test/mitmproxy/proxy/layers/http/test_http.py
@@ -969,3 +969,26 @@ def test_upgrade(tctx, proto):
             << Log("Sent HTTP 101 response, but no protocol is enabled to upgrade to.", "warn")
             << CloseConnection(tctx.client)
         )
+
+
+def test_dont_reuse_closed(tctx):
+    """Test that a closed connection is not reused."""
+    server = Placeholder(Server)
+    server2 = Placeholder(Server)
+    assert (
+            Playbook(http.HttpLayer(tctx, HTTPMode.regular), hooks=False)
+            >> DataReceived(tctx.client, b"GET http://example.com/ HTTP/1.1\r\nHost: example.com\r\n\r\n")
+            << OpenConnection(server)
+            >> reply(None)
+            << SendData(server, b"GET / HTTP/1.1\r\nHost: example.com\r\n\r\n")
+            >> DataReceived(server, b"HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n")
+            << SendData(tctx.client, b"HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n")
+            >> ConnectionClosed(server)
+            << CloseConnection(server)
+            >> DataReceived(tctx.client, b"GET http://example.com/two HTTP/1.1\r\nHost: example.com\r\n\r\n")
+            << OpenConnection(server2)
+            >> reply(None)
+            << SendData(server2, b"GET /two HTTP/1.1\r\nHost: example.com\r\n\r\n")
+            >> DataReceived(server2, b"HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n")
+            << SendData(tctx.client, b"HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n")
+    )

commit f11deaab933ebc2f415f5c7eefd3b8b70d36acc7

fix regression introduced in 70e08c880

mitmproxy/proxy/layers/http/__init__.py
@@ -628,16 +628,18 @@ class HttpLayer(layer.Layer):
                 h2_to_h1 = self.context.client.alpn == b"h2" and not conn_is_pending_or_h2
                 connection_suitable = (
                     event.connection_spec_matches(connection)
-                    and connection.connected
                     and not h2_to_h1
                 )
                 if connection_suitable:
                     if connection in self.waiting_for_establishment:
                         self.waiting_for_establishment[connection].append(event)
-                    else:
+                        return
+                    elif connection.connected:
                         stream = self.command_sources.pop(event)
                         yield from self.event_to_child(stream, GetHttpConnectionCompleted(event, (connection, None)))
-                    return
+                        return
+                    else:
+                        pass  # the connection is at least half-closed already, we want a new one.
 
         can_use_context_connection = (
             self.context.server not in self.connections and

test/mitmproxy/proxy/layers/http/test_http2.py
@@ -467,6 +467,29 @@ def test_max_concurrency(tctx):
     assert req2.stream_id == 3
 
 
+def test_stream_concurrent_get_connection(tctx):
+    """Test that an immediate second request for the same domain does not trigger a second connection attempt."""
+    playbook, cff = start_h2_client(tctx)
+    playbook.hooks = False
+
+    server = Placeholder(Server)
+    data = Placeholder(bytes)
+
+    assert (playbook
+            >> DataReceived(tctx.client, cff.build_headers_frame(example_request_headers, flags=["END_STREAM"], stream_id=1).serialize())
+            << (o := OpenConnection(server))
+            >> DataReceived(tctx.client, cff.build_headers_frame(example_request_headers, flags=["END_STREAM"], stream_id=3).serialize())
+            >> reply(None, to=o, side_effect=make_h2)
+            << SendData(server, data)
+            )
+    frames = decode_frames(data())
+    assert [type(x) for x in frames] == [
+        hyperframe.frame.SettingsFrame,
+        hyperframe.frame.HeadersFrame,
+        hyperframe.frame.HeadersFrame,
+    ]
+
+
 def test_kill_stream(tctx):
     """Test that we can kill individual streams."""
     playbook, cff = start_h2_client(tctx)