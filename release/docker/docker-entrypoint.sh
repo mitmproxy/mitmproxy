b35af38e41206047eaae98cd06776438f5b14ea7
alt-glitch <balyan.sid@gmail.com>
2021-02-24 11:58:13 +0530

    Docker: Moved from Alpine to Debian Buster

--- release/docker/docker-entrypoint.sh
+++ release/docker/docker-entrypoint.sh
@@ -2,7 +2,8 @@
 # WARNING: do not change the shebang - the Docker base image might not have what you want!
 
 set -o errexit
-set -o pipefail
+# set -o pipefail
+# Commented out since the option wasn't working on sh in Debian Buster
 set -o nounset
 # set -o xtrace
 
@@ -11,7 +12,7 @@ MITMPROXY_PATH="/home/mitmproxy/.mitmproxy"
 if [[ "$1" = "mitmdump" || "$1" = "mitmproxy" || "$1" = "mitmweb" ]]; then
   mkdir -p "$MITMPROXY_PATH"
   chown -R mitmproxy:mitmproxy "$MITMPROXY_PATH"
-  su-exec mitmproxy "$@"
+  su - mitmproxy -c "$@" # su-exec didn't have a package for Debian Buster, hence su
 else
   exec "$@"
 fi
