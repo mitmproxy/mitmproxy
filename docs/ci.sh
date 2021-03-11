49982c3f686fc35376a79c5ebbebcbd997a0ece3
Peter Dave Hello <hsu@peterdavehello.org>
2021-03-12 01:35:47 +0800

    Improve Shell Script syntax

--- docs/ci.sh
+++ docs/ci.sh
@@ -10,7 +10,7 @@ set -o pipefail
 
 # Only upload if we have defined credentials - we only have these defined for
 # trusted commits (i.e. not PRs).
-if [[ ! -z "${AWS_ACCESS_KEY_ID}" && $GITHUB_REF == "refs/heads/master" ]]; then
+if [[ -n "${AWS_ACCESS_KEY_ID}" && $GITHUB_REF == "refs/heads/master" ]]; then
     aws s3 sync --delete --acl public-read ./public s3://docs.mitmproxy.org/master
     aws cloudfront create-invalidation --distribution-id E1TH3USJHFQZ5Q \
         --paths "/master/*"
