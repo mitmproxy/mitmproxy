f326de6efe16b32ee24469b5aee786d8493ea5b2
HereC <ebeland@gmail.com>
2021-03-22 12:57:11 -0400

    Add allow-only, and more tests

--- mitmproxy/addons/blocklist.py
+++ mitmproxy/addons/blocklist.py
@@ -1,29 +1,14 @@
-import re
 import typing
 import mimetypes
 
 from mitmproxy import ctx, exceptions, flowfilter, http, version
-from mitmproxy.utils.spec import parse_spec
+from mitmproxy.net.http.status_codes import RESPONSES
+
 
 class BlockListSpec(typing.NamedTuple):
     matches: flowfilter.TFilter
-    regex: str
-    status_code: int
-
-def parse_block_list_spec(option: str) -> BlockListSpec:
-    filter, regex, status = parse_spec(option)
-
-    try:
-        re.compile(regex)
-    except re.error as e:
-        raise ValueError(f"Invalid regular expression {regex!r} ({e})")
-
-    try:
-        status_code = int(status)
-    except:
-        raise ValueError(f"Invalid HTTP status code {status!s}")
-
-    return BlockListSpec(filter, regex, status_code)
+    block_type: str
+    status: int
 
 
 class BlockList:
@@ -34,40 +19,67 @@ class BlockList:
         loader.add_option(
             "block_list", typing.Sequence[str], [],
             """
-            Block request and instead return a Response with a hard-coded HTTP statuses.
+            Block matching requests and return an empty response with the specified HTTP status.
+            Option is formatted as ":[flow-filter]:(block|allow-only):status", where the
+            separator can be any character. allow-only allows only matching traffic to be unaffected,
+            block will block only matching traffic.
+            HTTP status is the HTTP status code to return for blocked requests.
+            Note: Status code 444 is special-cased and will close the connection without sending a response.
             """
         )
 
     def configure(self, updated):
         if "block_list" in updated:
-            self.replacements = []
+            self.block_list_items = []
             for option in ctx.options.block_list:
                 try:
-                    spec = parse_block_list_spec(option)
+                    spec = self.__parse_blocklist_spec(option)
                 except ValueError as e:
                     raise exceptions.OptionsError(f"Cannot parse block_list option {option}: {e}") from e
 
-                self.replacements.append(spec)
+                self.block_list_items.append(spec)
 
     def request(self, flow: http.HTTPFlow) -> None:
         if flow.response or flow.error or (flow.reply and flow.reply.state == "taken"):
             return
 
-        url = flow.request.pretty_url
+        for spec in self.block_list_items:
+            matching = spec.matches(flow)
+            if (matching and spec.block_type == 'block') or (not matching and spec.block_type == 'allow-only'):
+                self.__block_it(flow, spec)
 
-        headers = {
-            "Server": version.MITMPROXY
-        }
+    def __block_it(self, flow, spec):
+        if spec.status == 444:
+            flow.kill()
+        else:
+            headers = {"Server": version.MITMPROXY}
+            mimetype = mimetypes.guess_type(flow.request.url)[0]
+            if mimetype:
+                headers["Content-Type"] = mimetype
+            flow.response = http.Response.make(
+                spec.status,
+                headers=headers
+            )
+        return
 
-        mimetype = mimetypes.guess_type(flow.request.url)[0]
-        if mimetype:
-            headers["Content-Type"] = mimetype
+    def __parse_blocklist_spec(self, option: str) -> typing.Tuple[flowfilter.TFilter, str, str]:
+        """
+        Parses strings in the following format, enforces number of segments:
 
-        for spec in self.replacements:
-            if spec.matches(flow) and re.search(spec.regex, url):
-                    flow.response = http.Response.make(
-                                    spec.status_code,
-                                    headers=headers
-                    )
-                    # only set flow.response once, for the first matching rule
-                    return
+            [/flow-filter]/(block|allow-only)/status
+
+        """
+        sep, rem = option[0], option[1:]
+        parts = rem.lower().split(sep, 2)
+        if len(parts) == 3:
+            flow_patt, block_type, status = parts
+            flow_filter = flowfilter.parse(flow_patt)
+            if not flow_filter:
+                raise ValueError(f"Invalid filter pattern: {flow_patt}")
+            if block_type not in ('block', 'allow-only'):
+                raise ValueError(f"Invalid block type. Must be `allow-only` or `block`")
+            if not RESPONSES.get(int(status)):
+                raise ValueError(f"Invalid HTTP status code: {status}")
+        else:
+            raise ValueError("Invalid number of parameters (3 are expected)")
+        return BlockListSpec(matches=flow_filter, block_type=block_type, status=int(status))
--- test/mitmproxy/addons/test_blocklist.py
+++ test/mitmproxy/addons/test_blocklist.py
@@ -1,32 +1,56 @@
 import pytest
 
+from mitmproxy.exceptions import OptionsError
 from mitmproxy.addons import blocklist
 from mitmproxy.test import taddons
 from mitmproxy.test import tflow
 
 
 class TestBlockList:
-    def test_good_three_part_configure(self):
-        bl = blocklist.BlockList()
-        with taddons.context(bl) as tctx:
-            tctx.configure(bl, block_list=["/one/two/200"])
 
-    def test_good_two_part_configure(self):
+    def test_good_configure(self):
         bl = blocklist.BlockList()
         with taddons.context(bl) as tctx:
-            tctx.configure(bl, block_list=["/one/200"])
+            tctx.configure(bl, block_list=[":mysite.com:allow-only:200"])
 
-    def test_invalid_regex(self):
+    def test_invalid_parameters_length(self):
         bl = blocklist.BlockList()
         with taddons.context(bl) as tctx:
-            with pytest.raises(Exception, match="Invalid regular expression"):
-                tctx.configure(bl, block_list=["/foo/+/200"])
+            with pytest.raises(OptionsError, match="Invalid number of parameters"):
+                tctx.configure(bl, block_list=["/~u index.html/300"])
+
+    def test_configure_bad_http_status_code(self):
+        bl = blocklist.BlockList()
+        with taddons.context(bl) as tctx:
+            with pytest.raises(OptionsError, match="Invalid HTTP status code"):
+                tctx.configure(bl, block_list=["/~u index.html/block/999"])
 
     def test_configure_invalid_status_code(self):
         bl = blocklist.BlockList()
         with taddons.context(bl) as tctx:
-            with pytest.raises(Exception, match="Invalid HTTP status code"):
-                tctx.configure(bl, block_list=["/foo/.*/NOT_A_STATUS_CODE"])
+            with pytest.raises(OptionsError, match="Cannot parse block_list option"):
+                tctx.configure(bl, block_list=[":mysite.com:block:NOT_A_STATUS_CODE"])
+
+    def test_configure_bad_allow_block_option(self):
+        bl = blocklist.BlockList()
+        with taddons.context(bl) as tctx:
+            with pytest.raises(OptionsError, match="Invalid block type"):
+                tctx.configure(bl, block_list=[":mysite.com:notright:400"])
+
+    def test_special_kill_status_closes_connection(self):
+        bl = blocklist.BlockList()
+        with taddons.context(bl) as tctx:
+            tctx.configure(
+                bl,
+                block_list=[
+                    ':~u jpg:block:444',
+                ]
+            )
+            f = tflow.tflow()
+            f.request.url = b"https://example.org/images/test.jpg"
+            bl.request(f)
+            assert (f.error.msg == f.error.KILLED_MESSAGE)
+            assert (f.response is None)
 
     def test_simple(self):
         bl = blocklist.BlockList()
@@ -34,7 +58,7 @@ class TestBlockList:
             tctx.configure(
                 bl,
                 block_list=[
-                    ":example.org:images:200",
+                    ':~u example.org:block:200',
                 ]
             )
             f = tflow.tflow()
@@ -42,19 +66,75 @@ class TestBlockList:
             bl.request(f)
             assert f.response.status_code == 200
 
+    def test_allowonly_allows_matches_through(self):
+        bl = blocklist.BlockList()
+        with taddons.context(bl) as tctx:
+            tctx.configure(
+                bl,
+                block_list=[
+                    ':jpg:allow-only:404',
+                ]
+            )
+            f = tflow.tflow(resp=True)
+            f.request.url = b"https://foo.org/images/test.jpg"
+            bl.request(f)
+            assert f.response.status_code == 200
+
+    def test_allowonly_blocks_non_match(self):
+        bl = blocklist.BlockList()
+        with taddons.context(bl) as tctx:
+            tctx.configure(
+                bl,
+                block_list=[
+                    ':~u .png:allow-only:404',
+                ]
+            )
+            f = tflow.tflow()
+            f.request.url = b"https://foo.org/images/test.jpg"
+            bl.request(f)
+            assert f.response.status_code == 404
+
+    def test_block_blocks_matching_flow(self):
+        bl = blocklist.BlockList()
+        with taddons.context(bl) as tctx:
+            tctx.configure(
+                bl,
+                block_list=[
+                    ':~u .jpg:block:404',
+                ]
+            )
+            f = tflow.tflow()
+            f.request.url = b"https://example.org/images/test.jpg"
+            bl.request(f)
+            assert f.response.status_code == 404
+
+    def block_ignores_non_match(self):
+        bl = blocklist.BlockList()
+        with taddons.context(bl) as tctx:
+            tctx.configure(
+                bl,
+                block_list=[
+                    ':~u .png:block:404',
+                ]
+            )
+            f = tflow.tflow(resp=True)
+            f.request.url = b"https://foo.org/images/test.jpg"
+            bl.request(f)
+            assert f.response.status_code == 200
+
     def test_has_guessed_content_type(self):
         bl = blocklist.BlockList()
         with taddons.context(bl) as tctx:
-            tctx.configure(bl, block_list=[":example.org:204"])
+            tctx.configure(bl, block_list=[":example.org:block:204"])
             f = tflow.tflow()
             f.request.url = b"https://example.org/images/test.jpg"
             bl.request(f)
             assert f.response.headers['Content-Type'] == 'image/jpeg'
 
-    def test_has_no_content(self):
+    def test_blocked_response_has_no_content(self):
         bl = blocklist.BlockList()
         with taddons.context(bl) as tctx:
-            tctx.configure(bl, block_list=[":example.org:204"])
+            tctx.configure(bl, block_list=[":example.org:block:204"])
             f = tflow.tflow()
             f.request.url = b"https://example.org/images/test.jpg"
             bl.request(f)
