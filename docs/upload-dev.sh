bbd8819d6272c596bde91dce5653557f1d10b133
Maximilian Hils <git@maximilianhils.com>
2021-05-10 11:02:30 +0200

    ci: try harder to fix docs deploy

--- docs/upload-dev.sh
+++ docs/upload-dev.sh
@@ -13,8 +13,5 @@ fi
 # This script is run during CI on the main branch and uploads docs to docs.mitmproxy.org/dev.
 
 aws configure set preview.cloudfront true
-aws --profile mitmproxy \
-    s3 sync --delete --acl public-read ./public s3://docs.mitmproxy.org/dev
-aws --profile mitmproxy \
-    cloudfront create-invalidation --distribution-id E1TH3USJHFQZ5Q \
-    --paths "/dev/*"
+aws s3 sync --delete --acl public-read ./public s3://docs.mitmproxy.org/dev
+aws cloudfront create-invalidation --distribution-id E1TH3USJHFQZ5Q --paths "/dev/*"
