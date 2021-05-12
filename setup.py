0509011c75b6d961598493443460cf0c02c3775a
requires.io <support@requires.io>
2021-05-12 05:10:16 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -70,9 +70,9 @@ setup(
         "blinker>=1.4, <1.5",
         "Brotli>=1.0,<1.1",
         "certifi>=2019.9.11",  # no semver here - this should always be on the last release!
-        "click>=7.0,<8",
+        "click>=8.0,<8.1",
         "cryptography>=3.3,<3.5",
-        "flask>=1.1.1,<1.2",
+        "flask>=2.0,<2.1",
         "h11>=0.11,<0.13",
         "h2>=4.0,<5",
         "hyperframe>=6.0,<7",
