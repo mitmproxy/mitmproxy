c34617a82ca7623a7757f85c10a0433d31035c7a
HereC <ebeland@gmail.com>
2021-05-24 17:53:29 -0400

    Add ssl to selenium proxy in Ruby example to make it work

--- clients/examples/simple.rb
+++ clients/examples/simple.rb
@@ -6,14 +6,17 @@ gemfile do
   gem 'webdrivers', require: true
 end
 
+
+
 include BrowserupProxy
 # start the proxy
 #pid = Kernel.spawn("python /Users/ebeland/apps/mitmproxynew/mitmproxy/tools/browserup_proxy.py")
 #Process.detach(pid)
 
-proxy = Selenium::WebDriver::Proxy.new(http: "localhost:8080")
+proxy = Selenium::WebDriver::Proxy.new(http: "localhost:8080", ssl: "localhost:8080")
 caps = Selenium::WebDriver::Remote::Capabilities.chrome(:proxy => proxy)
 driver = Selenium::WebDriver.for :chrome, desired_capabilities: caps
+
 
 driver.navigate.to "https://www.yahoo.com"
 criteria = MatchCriteria.new({ 'content' => 'vaccine'})
