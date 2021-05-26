2d264ce6f308abc6f1d41bc5abeac7e9e53156ee
Brad Dixon <brad.dixon@carvesystems.com>
2021-05-26 10:32:39 -0400

    Add a test to please our CI overlords. :)

--- test/mitmproxy/utils/test_emoji.py
+++ test/mitmproxy/utils/test_emoji.py
@@ -1,1 +1,6 @@
-# No tests required for constants table.
+from mitmproxy.utils import emoji
+from mitmproxy.tools.console.common import SYMBOL_MARK
+
+
+def test_emoji():
+    assert emoji.emoji[":default:"] == SYMBOL_MARK
