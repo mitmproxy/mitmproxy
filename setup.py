575fbeaa6326ddaeadae71ccbb2699d909f384fb
Maximilian Hils <git@maximilianhils.com>
2021-06-28 19:03:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements/#install-requires
     # It is not considered best practice to use install_requires to pin dependencies to specific versions.
     install_requires=[
-        "asgiref>=3.4,<3.5",
+        "asgiref>=3.2.10,<3.5",
         "blinker>=1.4, <1.5",
         "Brotli>=1.0,<1.1",
         "certifi>=2019.9.11",  # no semver here - this should always be on the last release!
