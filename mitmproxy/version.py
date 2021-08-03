88374e958b42091a13fee22ce61e54410cdfc0c6
Maximilian Hils <git@maximilianhils.com>
2021-08-03 17:33:06 +0200

    reopen main for development

--- mitmproxy/version.py
+++ mitmproxy/version.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import sys
 
-VERSION = "7.0.1"
+VERSION = "8.0.0.dev"
 MITMPROXY = "mitmproxy " + VERSION
 
 # Serialization format version. This is displayed nowhere, it just needs to be incremented by one
