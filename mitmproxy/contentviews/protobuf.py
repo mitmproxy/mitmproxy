a7f27259a7316e9080ebd0cd98868f80eabde9fe
Maximilian Hils <git@maximilianhils.com>
2021-08-12 10:21:06 +0200

    contentviews: ignore empty data

--- mitmproxy/contentviews/protobuf.py
+++ mitmproxy/contentviews/protobuf.py
@@ -80,4 +80,4 @@ class ViewProtobuf(base.View):
         return "Protobuf", base.format_text(decoded)
 
     def render_priority(self, data: bytes, *, content_type: Optional[str] = None, **metadata) -> float:
-        return float(content_type in self.__content_types)
+        return float(bool(data) and content_type in self.__content_types)
