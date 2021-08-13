fdfdf09bc98d39236b3035621937cad44f3aff3d
kirill.turutin <kiturutin@gmail.com>
2021-08-13 12:11:20 +0300

    Fixed falcon version inconsistency

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
         "wsproto>=1.0,<1.1",
         "publicsuffix2>=2.20190812,<3",
         "zstandard>=0.11,<0.16",
-        "falcon>=2.0.0",
+        "falcon~=2.0.0",
         "marshmallow>=3.0.0",
         "falcon-apispec>=0.4.0",
         "python-dateutil>=2.8.1",
