f5197cf016207565be37f934c36e8adafaf7bc91
Maximilian Hils <git@maximilianhils.com>
2021-08-31 11:06:49 +0200

    charset detection: ignore case when searching in HTML

--- mitmproxy/http.py
+++ mitmproxy/http.py
@@ -408,13 +408,13 @@ class Message(serializable.Serializable):
             if "json" in self.headers.get("content-type", ""):
                 enc = "utf8"
         if not enc:
-            meta_charset = re.search(rb"""<meta[^>]+charset=['"]?([^'">]+)""", content)
+            meta_charset = re.search(rb"""<meta[^>]+charset=['"]?([^'">]+)""", content, re.IGNORECASE)
             if meta_charset:
                 enc = meta_charset.group(1).decode("ascii", "ignore")
         if not enc:
             if "text/css" in self.headers.get("content-type", ""):
                 # @charset rule must be the very first thing.
-                css_charset = re.match(rb"""@charset "([^"]+)";""", content)
+                css_charset = re.match(rb"""@charset "([^"]+)";""", content, re.IGNORECASE)
                 if css_charset:
                     enc = css_charset.group(1).decode("ascii", "ignore")
         if not enc:
