c9280002646843692378bf2bb69e5f3f56092400
Maximilian Hils <git@maximilianhils.com>
2021-11-21 15:07:47 +0100

    lint!

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
         "pyperclip>=1.6.0,<1.9",
         "ruamel.yaml>=0.16,<0.17.17",
         # Kaitai parsers depend on setuptools, remove once https://github.com/kaitai-io/kaitai_struct_python_runtime/issues/62 is fixed
-        "setuptools",  
+        "setuptools",
         "sortedcontainers>=2.3,<2.5",
         "tornado>=6.1,<7",
         "urwid>=2.1.1,<2.2",
