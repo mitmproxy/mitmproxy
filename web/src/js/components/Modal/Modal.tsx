59dcd0dfd2b3d16b8f8d4dff60f16e9cc5a0e0f5
shubhangi013 <choudharyshubhangi13@gmail.com>
2021-12-04 17:43:23 +0530

    migrated the files to tsx

--- web/src/js/components/Modal/Modal.tsx
+++ web/src/js/components/Modal/Modal.tsx
@@ -5,7 +5,7 @@ import { useAppSelector } from "../../ducks";
 
 export default function PureModal() {
     const activeModal = useAppSelector(state => state.ui.modal.activeModal)
-    const ActiveModal = ModalList.find(m => m.name === activeModal )
+    const ActiveModal:(() => JSX.Element) | undefined = ModalList.find(m => m.name === activeModal )
 
     return(
         activeModal ? <ActiveModal/> : <div/>
--- web/src/js/components/Modal/ModalLayout.tsx
+++ web/src/js/components/Modal/ModalLayout.tsx
@@ -1,10 +1,14 @@
 import * as React from "react"
 
-export default function ModalLayout ({ children }) {
+type ModalLayoutProps = {
+    children: React.ReactNode,
+}
+
+export default function ModalLayout ({ children}: ModalLayoutProps ) {
     return (
         <div>
         <div className="modal-backdrop fade in"></div>
-        <div className="modal modal-visible" id="optionsModal" tabIndex="-1" role="dialog" aria-labelledby="options">
+        <div className="modal modal-visible" id="optionsModal" tabIndex={-1} role="dialog" aria-labelledby="options">
         <div className="modal-dialog modal-lg" role="document">
             <div className="modal-content">
                 {children}
