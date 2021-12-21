c509354c7cfde07acfca43e3c3e92b2660d9cf6d
shubhangi013 <choudharyshubhangi13@gmail.com>
2021-12-21 15:06:52 +0530

    fixed the fails

--- web/src/js/components/Modal/Modal.tsx
+++ web/src/js/components/Modal/Modal.tsx
@@ -5,7 +5,7 @@ import { useAppSelector } from "../../ducks";
 
 export default function PureModal() {
     const activeModal = useAppSelector(state => state.ui.modal.activeModal)
-    const ActiveModal:(() => JSX.Element) | undefined = ModalList.find(m => m.name === activeModal )
+    const ActiveModal:React.ComponentClass<any> = ModalList.find(m => m.name === activeModal )
 
     return(
         activeModal ? <ActiveModal/> : <div/>
