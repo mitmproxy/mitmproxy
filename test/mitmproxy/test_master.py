e22f8db5261c4013c7bfb76df20f725b3fb18dda
Maximilian Hils <git@maximilianhils.com>
2022-03-15 19:59:51 +0100

    add asyncio exception handler test

--- test/mitmproxy/test_master.py
+++ test/mitmproxy/test_master.py
@@ -1,1 +1,16 @@
-# TODO: write tests
+import asyncio
+
+from mitmproxy.test.taddons import RecordingMaster
+
+
+async def err():
+    raise RuntimeError
+
+
+async def test_exception_handler():
+    m = RecordingMaster(None)
+    running = asyncio.create_task(m.run())
+    asyncio.create_task(err())
+    await m.await_log("Traceback", level="error")
+    m.shutdown()
+    await running
