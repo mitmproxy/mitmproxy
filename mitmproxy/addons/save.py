e1f58c49df5e713b904caf75c466e09690367c9e
Maximilian Hils <git@maximilianhils.com>
2022-03-16 07:49:34 +0100

    simplify save addon logic

--- mitmproxy/addons/save.py
+++ mitmproxy/addons/save.py
@@ -1,27 +1,43 @@
-from datetime import datetime
-from pathlib import Path
-from sys import exit
 import os.path
+import sys
 import typing
+from datetime import datetime
+from functools import cache
+from pathlib import Path
 
-from mitmproxy import command
-from mitmproxy import exceptions
-from mitmproxy import flowfilter
-from mitmproxy import io
-from mitmproxy import ctx
-from mitmproxy import flow
-from mitmproxy import http
 import mitmproxy.types
+from mitmproxy import command, tcp
+from mitmproxy import ctx
+from mitmproxy import exceptions
+from mitmproxy import flow
+from mitmproxy import flowfilter
+from mitmproxy import http
+from mitmproxy import io
+
+
+@cache
+def _path(path: str) -> str:
+    """Extract the path from a path spec (which may have an extra "+" at the front)"""
+    if path.startswith("+"):
+        path = path[1:]
+    return os.path.expanduser(path)
+
+
+@cache
+def _mode(path: str) -> typing.Literal["ab", "wb"]:
+    """Extract the writing mode (overwrite or append) from a path spec"""
+    if path.startswith("+"):
+        return "ab"
+    else:
+        return "wb"
 
 
 class Save:
-    def __init__(self):
-        self.stream = None
-        self.path = None
-        self.mode = None
-        self.filt = None
+    def __init__(self) -> None:
+        self.stream: typing.Optional[io.FilteredFlowWriter] = None
+        self.filt: typing.Optional[flowfilter.TFilter] = None
         self.active_flows: typing.Set[flow.Flow] = set()
-        self.curpath = None
+        self.current_path: typing.Optional[str] = None
 
     def load(self, loader):
         loader.add_option(
@@ -29,8 +45,8 @@ class Save:
             """
             Stream flows to file as they arrive. Prefix path with + to append.
             The full path can use python strftime() formating, missing
-            directories are created as needed. A new file is opened every the
-            formatted string changes.
+            directories are created as needed. A new file is opened every time
+            the formatted string changes.
             """
         )
         loader.add_option(
@@ -38,65 +54,7 @@ class Save:
             "Filter which flows are written to file."
         )
 
-    def prep_path(self, path):
-        if path.startswith("+"):
-            path = path[1:]
-            mode = "ab"
-        else:
-            mode = "wb"
-        path = os.path.expanduser(path)
-        return path, mode
-
-    def new_path(self):
-        path = datetime.today().strftime(self.path)
-        if self.curpath and self.curpath == path:
-            return False
-        self.curpath = path
-
-        try:
-            parent = Path(self.curpath).parent
-            if not parent.exists():  # pragma: no cover
-                parent.mkdir(parents=True, exist_ok=True)
-        except OSError as v:  # pragma: no cover
-            ctx.log.error(f"Error while creating directories {parent}: {v}")
-        return True
-
-    def save_flow(self, flow: http.HTTPFlow):
-        if self.new_path():
-            if self.stream:
-                self.close_stream()
-            try:
-                f = open(self.curpath, self.mode)
-                self.stream = io.FilteredFlowWriter(f, self.filt)
-            except OSError as v:  # pragma: no cover
-                ctx.log.error(f"Error while opening {self.curpath}: {v}")
-                exit(1)
-        try:
-            self.stream.add(flow)
-        except OSError as v:  # pragma: no cover
-            ctx.log.error(f"Error while writing to {self.curpath}: {v}")
-            exit(1)
-
-    def close_stream(self):
-        try:
-            self.active_flows = set()
-            self.stream.fo.close()
-        except OSError as v:  # pragma: no cover
-            ctx.log.error(f"Error while closing stream file: {v}")
-            exit(1)
-
-    def start_stream_to_path(self, path, flt):
-        try:
-            self.path, self.mode = self.prep_path(path)
-            self.new_path()
-            f = open(self.curpath, self.mode)
-        except OSError as v:
-            raise exceptions.OptionsError(str(v))
-        self.stream = io.FilteredFlowWriter(f, flt)
-        self.active_flows = set()
-
     def configure(self, updated):
-        # We're already streaming - stop the previous stream and restart
         if "save_stream_filter" in updated:
             if ctx.options.save_stream_filter:
                 try:
@@ -106,10 +64,56 @@ class Save:
             else:
                 self.filt = None
         if "save_stream_file" in updated or "save_stream_filter" in updated:
-            if self.stream:
-                self.done()
             if ctx.options.save_stream_file:
-                self.start_stream_to_path(ctx.options.save_stream_file, self.filt)
+                try:
+                    self.maybe_rotate_to_new_file()
+                except OSError as e:
+                    raise exceptions.OptionsError(str(e)) from e
+                self.stream.flt = self.filt
+            else:
+                self.done()
+
+    def maybe_rotate_to_new_file(self) -> None:
+        path = datetime.today().strftime(_path(ctx.options.save_stream_file))
+        if self.current_path == path:
+            return
+
+        if self.stream:
+            self.stream.fo.close()
+            self.stream = None
+
+        new_log_file = Path(path)
+        new_log_file.parent.mkdir(parents=True, exist_ok=True)
+
+        f = new_log_file.open(_mode(ctx.options.save_stream_file))
+        self.stream = io.FilteredFlowWriter(f, self.filt)
+        self.current_path = path
+
+    def save_flow(self, flow: flow.Flow) -> None:
+        """
+        Write the flow to the stream, but first check if we need to rotate to a new file.
+        """
+        try:
+            self.maybe_rotate_to_new_file()
+            self.stream.add(flow)
+        except OSError as e:
+            # If we somehow fail to write flows to a logfile, we really want to crash visibly
+            # instead of letting traffic through unrecorded.
+            # No normal logging here, that would not be triggered anymore.
+            sys.stderr.write(f"Error while writing to {self.current_path}: {e}")
+            sys.exit(1)
+        else:
+            self.active_flows.discard(flow)
+
+    def done(self) -> None:
+        if self.stream:
+            for f in self.active_flows:
+                self.stream.add(f)
+            self.active_flows.clear()
+
+            self.current_path = None
+            self.stream.fo.close()
+            self.stream = None
 
     @command.command("save.file")
     def save(self, flows: typing.Sequence[flow.Flow], path: mitmproxy.types.Path) -> None:
@@ -118,32 +122,28 @@ class Save:
             appended to the file, otherwise it is over-written.
         """
         try:
-            path, mode = self.prep_path(path)
-            f = open(path, mode)
-            stream = io.FlowWriter(f)
-            for i in flows:
-                stream.add(i)
-            f.close()
-        except OSError as v:
-            raise exceptions.CommandError(v) from v
-        ctx.log.alert("Saved %s flows." % len(flows))
+            with open(_path(path), _mode(path)) as f:
+                stream = io.FlowWriter(f)
+                for i in flows:
+                    stream.add(i)
+        except OSError as e:
+            raise exceptions.CommandError(e) from e
+        ctx.log.alert(f"Saved {len(flows)} flows.")
 
-    def tcp_start(self, flow):
+    def tcp_start(self, flow: tcp.TCPFlow):
         if self.stream:
             self.active_flows.add(flow)
 
-    def tcp_end(self, flow):
+    def tcp_end(self, flow: tcp.TCPFlow):
         if self.stream:
             self.save_flow(flow)
-            self.active_flows.discard(flow)
 
-    def tcp_error(self, flow):
+    def tcp_error(self, flow: tcp.TCPFlow):
         self.tcp_end(flow)
 
     def websocket_end(self, flow: http.HTTPFlow):
         if self.stream:
             self.save_flow(flow)
-            self.active_flows.discard(flow)
 
     def request(self, flow: http.HTTPFlow):
         if self.stream:
@@ -154,14 +154,6 @@ class Save:
         # we don't want to persist them here already
         if self.stream and flow.websocket is None:
             self.save_flow(flow)
-            self.active_flows.discard(flow)
 
     def error(self, flow: http.HTTPFlow):
         self.response(flow)
-
-    def done(self):
-        if self.stream:
-            for f in self.active_flows:
-                self.save_flow(f)
-            self.close_stream()
-            self.stream = None
