9243ba4e25cf1e811f11f511a744c514d9fabbcd
Maximilian Hils <git@maximilianhils.com>
2022-03-19 16:35:40 +0100

    fix vt code detection on Windows

--- mitmproxy/addons/termlog.py
+++ mitmproxy/addons/termlog.py
@@ -4,6 +4,7 @@ from typing import IO, Optional
 from mitmproxy import ctx
 from mitmproxy import log
 from mitmproxy.contrib import click as miniclick
+from mitmproxy.utils import vt_codes
 
 LOG_COLORS = {'error': "red", 'warn': "yellow", 'alert': "magenta"}
 
@@ -15,9 +16,9 @@ class TermLog:
         err: Optional[IO[str]] = None,
     ):
         self.out_file: IO[str] = out or sys.stdout
-        self.out_isatty = self.out_file.isatty()
+        self.out_has_vt_codes = vt_codes.ensure_supported(self.out_file)
         self.err_file: IO[str] = err or sys.stderr
-        self.err_isatty = self.err_file.isatty()
+        self.err_has_vt_codes = vt_codes.ensure_supported(self.err_file)
 
     def load(self, loader):
         loader.add_option(
@@ -30,13 +31,13 @@ class TermLog:
         if log.log_tier(ctx.options.termlog_verbosity) >= log.log_tier(e.level):
             if e.level == "error":
                 f = self.err_file
-                isatty = self.err_isatty
+                has_vt_codes = self.err_has_vt_codes
             else:
                 f = self.out_file
-                isatty = self.out_isatty
+                has_vt_codes = self.out_has_vt_codes
 
             msg = e.msg
-            if isatty:
+            if has_vt_codes:
                 msg = miniclick.style(
                     e.msg,
                     fg=LOG_COLORS.get(e.level),
---
+++ mitmproxy/utils/vt_codes.py
@@ -0,0 +1,51 @@
+"""
+This module provides a method to detect if a given file object supports virtual terminal escape codes.
+"""
+import os
+import sys
+from typing import IO
+
+if os.name == "nt":
+    from ctypes import byref, windll  # type: ignore
+    from ctypes.wintypes import BOOL, DWORD, HANDLE, LPDWORD
+
+    ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004
+    STD_OUTPUT_HANDLE = -11
+    STD_ERROR_HANDLE = -12
+
+    # https://docs.microsoft.com/de-de/windows/console/getstdhandle
+    GetStdHandle = windll.kernel32.GetStdHandle
+    GetStdHandle.argtypes = [DWORD]
+    GetStdHandle.restype = HANDLE
+
+    # https://docs.microsoft.com/de-de/windows/console/getconsolemode
+    GetConsoleMode = windll.kernel32.GetConsoleMode
+    GetConsoleMode.argtypes = [HANDLE, LPDWORD]
+    GetConsoleMode.restype = BOOL
+
+    # https://docs.microsoft.com/de-de/windows/console/setconsolemode
+    SetConsoleMode = windll.kernel32.SetConsoleMode
+    SetConsoleMode.argtypes = [HANDLE, DWORD]
+    SetConsoleMode.restype = BOOL
+
+    def ensure_supported(f: IO[str]) -> bool:
+        if not f.isatty():
+            return False
+        if f == sys.stdout:
+            h = STD_OUTPUT_HANDLE
+        elif f == sys.stderr:
+            h = STD_ERROR_HANDLE
+        else:
+            return False
+
+        handle = GetStdHandle(h)
+        console_mode = DWORD()
+        ok = GetConsoleMode(handle, byref(console_mode))
+        if not ok:
+            return False
+
+        ok = SetConsoleMode(handle, console_mode.value | ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+        return ok
+else:
+    def ensure_supported(f: IO[str]) -> bool:
+        return f.isatty()
