e534086053593eca9cedc112ddfa36c3d52fd0aa
Paulo Romeira <paulo@pauloromeira.com>
2022-03-23 09:56:20 -0300

    Add flatpak support to the browser addon (#5200)

--- mitmproxy/addons/browser.py
+++ mitmproxy/addons/browser.py
@@ -23,6 +23,34 @@ def get_chrome_executable() -> typing.Optional[str]:
     ):
         if shutil.which(browser):
             return browser
+
+    return None
+
+
+def get_chrome_flatpak() -> typing.Optional[str]:
+    if shutil.which("flatpak"):
+        for browser in (
+            "com.google.Chrome",
+            "org.chromium.Chromium",
+            "com.github.Eloston.UngoogledChromium",
+            "com.google.ChromeDev",
+        ):
+            if subprocess.run(
+                ["flatpak", "info", browser],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            ).returncode == 0:
+                return browser
+
+    return None
+
+
+def get_browser_cmd() -> typing.Optional[typing.List[str]]:
+    if browser := get_chrome_executable():
+        return [browser]
+    elif browser := get_chrome_flatpak():
+        return ["flatpak", "run", "-p", browser]
+
     return None
 
 
@@ -39,7 +67,7 @@ class Browser:
         if len(self.browser) > 0:
             ctx.log.alert("Starting additional browser")
 
-        cmd = get_chrome_executable()
+        cmd = get_browser_cmd()
         if not cmd:
             ctx.log.alert("Your platform is not supported yet - please submit a patch.")
             return
@@ -48,7 +76,7 @@ class Browser:
         self.tdir.append(tdir)
         self.browser.append(subprocess.Popen(
             [
-                cmd,
+                *cmd,
                 "--user-data-dir=%s" % str(tdir.name),
                 "--proxy-server={}:{}".format(
                     ctx.options.listen_host or "127.0.0.1",
@@ -61,8 +89,8 @@ class Browser:
 
                 "about:blank",
             ],
-            stdout = subprocess.DEVNULL,
-            stderr = subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
         ))
 
     def done(self):
