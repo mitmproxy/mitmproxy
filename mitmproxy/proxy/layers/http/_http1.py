398aa4be3da7e1db6e626a88002d99e8e94639e9
Maximilian Hils <github@maximilianhils.com>
2022-05-12 20:50:59 +0200

    minor code style nits

--- mitmproxy/proxy/layers/http/_http1.py
+++ mitmproxy/proxy/layers/http/_http1.py
@@ -348,11 +348,11 @@ class Http1Client(Http1Connection):
                 if "Host" not in request.headers and request.authority:
                     request.headers.insert(0, "Host", request.authority)
                 request.authority = ""
-                if "Cookie" in request.headers and len(request.headers.get_all("Cookie")) > 1:
-                    # we should merge multiple Cookie headers to one, since HTTP/2 supporting multiple cookie headers but HTTP/1.x is not.
+                if cookie_headers := request.headers.get_all("Cookie") and len(cookie_headers) > 1:
+                    # Only HTTP/2 supports multiple cookie headers, HTTP/1.x does not.
                     # see: https://www.rfc-editor.org/rfc/rfc6265#section-5.4
                     #      https://www.rfc-editor.org/rfc/rfc7540#section-8.1.2.5
-                    request.headers.set_all("Cookie", ["; ".join(request.headers.get_all("Cookie"))])
+                    request.headers["Cookie"] = "; ".join(cookie_headers)
             raw = http1.assemble_request_head(request)
             yield commands.SendData(self.conn, raw)
         elif isinstance(event, RequestData):
