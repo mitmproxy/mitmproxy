3eab9f0856403c53871d258bba289dfcbeb6e0df
PavelICS <45364791+PavelICS@users.noreply.github.com>
2022-06-06 12:27:30 +0300

    Drop pkg_resources dependency used by kaitaistruct version checking (#5401) (#5403)
    Co-authored-by: Pavel <pavel@yarpg.ru>

--- mitmproxy/contrib/kaitaistruct/ico.py
+++ mitmproxy/contrib/kaitaistruct/ico.py
@@ -1,12 +1,10 @@
 # This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
 
-from pkg_resources import parse_version
-from kaitaistruct import __version__ as ks_version, KaitaiStruct, KaitaiStream, BytesIO
+from kaitaistruct import KaitaiStruct, KaitaiStream, BytesIO
 import struct
 
 
-if parse_version(ks_version) < parse_version('0.7'):
-    raise Exception("Incompatible Kaitai Struct Python API: 0.7 or later is required, but you have %s" % (ks_version))
+# manually removed version check, see https://github.com/mitmproxy/mitmproxy/issues/5401
 
 class Ico(KaitaiStruct):
     """Microsoft Windows uses specific file format to store applications
