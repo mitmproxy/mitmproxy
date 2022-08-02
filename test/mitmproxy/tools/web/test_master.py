d1a5924d1c21b26647875fa7aca02a5914b494e7
Manuel Meitinger <github@meitinger.at>
2022-08-02 17:03:27 +0200

    fix reuse test for master

--- test/mitmproxy/tools/web/test_master.py
+++ test/mitmproxy/tools/web/test_master.py
@@ -7,11 +7,11 @@ from mitmproxy.tools.web.master import WebMaster
 
 
 async def test_reuse():
-    server = await asyncio.start_server(MagicMock(), "127.0.0.1", 0)
-    options = Options()
-    options.add_option("web_port", int, server.sockets[0].getsockname()[1], "")
-    options.add_option("web_host", str, "127.0.0.1", "")
-    master = WebMaster(options)
-    with pytest.raises(OSError, match=f"--set web_port={options.web_port + 1}"):
+    server = await asyncio.start_server(MagicMock(), host="127.0.0.1", port=0)
+    port = server.sockets[0].getsockname()[1]
+    master = WebMaster(Options())
+    master.options.web_host = "127.0.0.1"
+    master.options.web_port = port
+    with pytest.raises(OSError, match=f"--set web_port={port + 1}"):
         await master.running()
     server.close()
