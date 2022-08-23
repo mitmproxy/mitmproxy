60252f1cb2c8c1fc7520f20b869fc6336c04fe6d
Joran van Apeldoorn <joran@bitsoffreedom.nl>
2022-08-23 16:44:53 +0200

    Hardcoded hash

--- test/mitmproxy/utils/test_magisk.py
+++ test/mitmproxy/utils/test_magisk.py
@@ -1,10 +1,7 @@
-import pytest
 from mitmproxy.utils import magisk
 from cryptography import x509
 from mitmproxy.test import taddons
 import os
-import subprocess
-import shutil
 
 
 def test_get_ca(tdata):
@@ -14,24 +11,13 @@ def test_get_ca(tdata):
         assert isinstance(ca, x509.Certificate)
 
 
-@pytest.mark.skipif(
-    shutil.which("openssl") is None, reason="openssl not avalible as executable."
-)
 def test_subject_hash_old(tdata):
     # checks if the hash is the same as that comming form openssl
     with taddons.context() as tctx:
         tctx.options.confdir = tdata.path("mitmproxy/data/confdir")
         ca = magisk.get_ca_from_files()
         our_hash = magisk.subject_hash_old(ca)
-
-        confdir = tctx.options.confdir
-        pemfile = os.path.join(confdir, "mitmproxy-ca-cert.pem")
-        openssl_result = subprocess.run(
-            ["openssl", "x509", "-in", pemfile, "-subject_hash_old"],
-            capture_output=True,
-        )
-        openssl_hash = openssl_result.stdout.decode().strip().split("\n")[0]
-        assert openssl_hash == our_hash
+        assert our_hash == "efb15d7d"
 
 
 def test_magisk_write(tdata, tmp_path):
