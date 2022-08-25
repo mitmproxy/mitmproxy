d9b4ef1fee639660185f20bd6a28dad76ae94ca6
Maximilian Hils <git@maximilianhils.com>
2022-08-25 20:41:19 +0200

    refactor `Servers.update` to retain order
    `set()` does not guarantee order, which means the list of servers is not in sync with `options.modes`.
    Also removed modes from signals - we readd them once we have a concrete use case.
    Finally, we only try to start each server once. Retrying should be done by the UI.

--- mitmproxy/addons/proxyserver.py
+++ mitmproxy/addons/proxyserver.py
@@ -7,7 +7,7 @@ import asyncio
 import collections
 import ipaddress
 from contextlib import contextmanager
-from typing import Iterable, Iterator, Optional
+from typing import Iterable, Iterator, Optional, cast
 
 from wsproto.frame_protocol import Opcode
 
@@ -31,9 +31,9 @@ from mitmproxy.utils import human, signals
 
 class Servers:
     def __init__(self, manager: ServerManager):
-        self.updating = signals.AsyncSignal(lambda old_modes, new_modes: None)
+        self.updating = signals.AsyncSignal(lambda: None)
         """"Notified before any instances are started or stopped"""
-        self.updated = signals.AsyncSignal(lambda old_modes, new_modes: None)
+        self.updated = signals.AsyncSignal(lambda: None)
         """"Notified after all instances have been configured."""
         self._instances: dict[mode_specs.ProxyMode, ServerInstance] = dict()
         self._lock = asyncio.Lock()
@@ -48,16 +48,15 @@ class Servers:
 
         async with self._lock:
             # Notify listeners about the pending update.
-            old_modes = set(self._instances.keys())
-            new_modes = set(modes)
+            new_instances = dict.fromkeys(modes)
             if not ctx.options.server:
-                new_modes.clear()
-            await self.updating.send(old_modes=old_modes, new_modes=new_modes)
+                new_instances.clear()
+            await self.updating.send()
 
             # Shutdown modes that have been removed from the list.
             stop_tasks = [
                 s.stop() for spec, s in self._instances.items()
-                if spec not in new_modes
+                if spec not in new_instances
             ]
             for ret in await asyncio.gather(*stop_tasks, return_exceptions=True):
                 if ret:
@@ -65,27 +64,25 @@ class Servers:
                     ctx.log.error(str(ret))
 
             # Create missing modes and keep existing ones.
-            instances: dict[mode_specs.ProxyMode, ServerInstance] = dict()
-            for spec in new_modes:
-                if not (instance := self._instances.get(spec, None)):
+            start_tasks = []
+            for spec in new_instances:
+                if spec in self._instances:
+                    instance = self._instances[spec]
+                else:
                     instance = ServerInstance.make(spec, self._manager)
-                instances[spec] = instance
+                    start_tasks.append(instance.start())
+                new_instances[spec] = instance
 
-            # Start all non-running instances.
-            start_tasks = [
-                s.start() for s in instances.values()
-                if not s.is_running
-            ]
             for ret in await asyncio.gather(*start_tasks, return_exceptions=True):
                 if ret:
                     all_ok = False
                     ctx.log.error(str(ret))
 
             # Set the new instances and...
-            self._instances = instances
+            self._instances = cast(dict[mode_specs.ProxyMode, ServerInstance], new_instances)
 
         # ...notify the listeners outside the lock.
-        await self.updated.send(old_modes=old_modes, new_modes=new_modes)
+        await self.updated.send()
         return all_ok
 
     def __len__(self) -> int:
@@ -264,7 +261,7 @@ class Proxyserver(ServerManager):
                 asyncio.create_task(self.servers.update(modes))
 
     async def setup_servers(self) -> bool:
-        return await self.servers.update(map(mode_specs.ProxyMode.parse, ctx.options.mode))
+        return await self.servers.update([mode_specs.ProxyMode.parse(m) for m in ctx.options.mode])
 
     def listen_addrs(self) -> list[Address]:
         return [
