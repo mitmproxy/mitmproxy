ead157bd4fbcede2e76746ee5296b35431f514c5
Manuel Meitinger <github@meitinger.at>
2022-08-28 19:29:07 +0200

    [modes] fix typo

--- mitmproxy/net/udp.py
+++ mitmproxy/net/udp.py
@@ -54,7 +54,7 @@ class DrainableDatagramProtocol(asyncio.DatagramProtocol):
     def connection_lost(self, exc: Exception | None) -> None:
         self._closed.set()
         if exc:
-            ctx.log.warn(f"Connection lost on {self!r}: {exc!r}")  # pragma: nocover
+            ctx.log.warn(f"Connection lost on {self!r}: {exc!r}")  # pragma: no cover
 
     def pause_writing(self) -> None:
         self._paused = self._paused + 1
@@ -71,7 +71,7 @@ class DrainableDatagramProtocol(asyncio.DatagramProtocol):
         await self._can_write.wait()
 
     def error_received(self, exc: Exception) -> None:
-        ctx.log.warn(f"Send/receive on {self!r} failed: {exc!r}")  # pragma: nocover
+        ctx.log.warn(f"Send/receive on {self!r} failed: {exc!r}")  # pragma: no cover
 
     async def wait_closed(self) -> None:
         await self._closed.wait()
@@ -150,7 +150,7 @@ class DatagramReader:
         else:
             try:
                 return await self._packets.get()
-            except RuntimeError:  # pragma: nocover
+            except RuntimeError:  # pragma: no cover
                 # event loop got closed
                 return b""
 
