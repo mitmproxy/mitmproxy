6a4058d9c11df3dbe00364976fe8150f1380a4ad
requires.io <support@requires.io>
2022-10-09 08:33:43 +1300

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
             "hypothesis>=5.8,<7",
             "parver>=0.1,<2.0",
             "pdoc>=4.0.0",
-            "pyinstaller==5.4.1",
+            "pyinstaller==5.5",
             "pytest-asyncio>=0.17,<0.20",
             "pytest-cov>=4.0,<4.1",
             "pytest-timeout>=1.3.3,<2.2",
