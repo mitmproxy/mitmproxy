3dffdc22615926a64e97f4c9779ecca7e249d8df
Maximilian Hils <git@maximilianhils.com>
2022-10-19 21:03:28 +0200

    fix vScroll calcuation, refs #5658

--- web/src/js/__tests__/components/helpers/VirtualScrollSpec.tsx
+++ web/src/js/__tests__/components/helpers/VirtualScrollSpec.tsx
@@ -18,4 +18,17 @@ describe('VirtualScroll', () => {
             start: 0, end: 4, paddingTop: 0, paddingBottom: 100
         })
     })
+
+    it('should handle the case where lots of existing rows are removed without itemHeights', () => {
+        expect(calcVScroll({itemCount: 10, rowHeight: 32, viewportHeight: 400, viewportTop: 12_000})).toEqual({
+            start: 0, end: 10, paddingTop: 0, paddingBottom: 0
+        })
+    })
+
+    it('should handle the case where lots of existing rows are removed with itemHeights', () => {
+        expect(calcVScroll({itemCount: 4, itemHeights: [100, 100, 100, 100],
+            viewportHeight: 400, viewportTop: 12_000, rowHeight: 32})).toEqual({
+            start: 0, end: 4, paddingTop: 0, paddingBottom: 0
+        })
+    })
 })
