eed3fdbc51be962aa8c9402871a2c63730907147
konneka <inceabdullah@yahoo.com>
2022-10-20 21:53:36 +0300

    remove requestID_to_entry

--- examples/contrib/har_dump.py
+++ examples/contrib/har_dump.py
@@ -16,7 +16,6 @@ from datetime import datetime
 from datetime import timezone
 
 import zlib
-import uuid
 
 import mitmproxy
 from mitmproxy import connection
@@ -26,7 +25,6 @@ from mitmproxy.net.http import cookies
 from mitmproxy.utils import strutils
 
 HAR: dict = {}
-requestID_to_entry: dict = {}
 
 # A list of server seen till now is maintained so we can avoid
 # using 'connect' time for entries that use an existing connection.
@@ -52,32 +50,24 @@ def configure(updated):
         }
     })
 
-def websocket_end(flow: mitmproxy.http.HTTPFlow):
-    assert flow.websocket is not None
-    messages = flow.websocket.messages
-    websocketMessages = []
+def appendWebsocketMessages(flow: mitmproxy.http.HTTPFlow, entry):
+    if flow.websocket:
+        messages = flow.websocket.messages
+        websocketMessages = []
 
-    for message in messages:
-        websocketMessage = {
-        'type': 'send' if message.from_client else 'receive',
-        'time': message.timestamp,
-        'opcode': message.type.value,
-        'data': message.content.decode()
-        }
-        websocketMessages.append(websocketMessage)
+        for message in messages:
+            websocketMessage = {
+            'type': 'send' if message.from_client else 'receive',
+            'time': message.timestamp,
+            'opcode': message.type.value,
+            'data': message.content.decode()
+            }
+            websocketMessages.append(websocketMessage)
 
-    requestID_to_entry[flow.request.id]['_resourceType'] = 'websocket'
-    requestID_to_entry[flow.request.id]['_webSocketMessages'] = websocketMessages
+        entry['_resourceType'] = 'websocket'
+        entry['_webSocketMessages'] = websocketMessages
 
-def request(flow: mitmproxy.http.HTTPFlow):
-    request_uuid = uuid.uuid4()
-    print(f'request.id: {request_uuid}')
-    flow.request.id = request_uuid
-
-def response(flow: mitmproxy.http.HTTPFlow):
-    """
-       Called when a server response has been received.
-    """
+def flow_entry(flow: mitmproxy.http.HTTPFlow):
 
     # -1 indicates that these values do not apply to current request
     ssl_time = -1
@@ -176,9 +166,26 @@ def response(flow: mitmproxy.http.HTTPFlow):
 
     if flow.server_conn.connected:
         entry["serverIPAddress"] = str(flow.server_conn.peername[0])
-    requestID_to_entry[flow.request.id] = entry
+
+    appendWebsocketMessages(flow, entry)
+
     HAR["log"]["entries"].append(entry)
 
+def websocket_end(flow: mitmproxy.http.HTTPFlow):
+    assert flow.websocket is not None
+    flow_entry(flow)
+
+def request(flow: mitmproxy.http.HTTPFlow):
+    request_uuid = uuid.uuid4()
+    print(f'request.id: {request_uuid}')
+    flow.request.id = request_uuid
+
+def response(flow: mitmproxy.http.HTTPFlow):
+    """
+       Called when a server response has been received.
+    """
+    if flow.websocket is None:
+        flow_entry(flow)
 
 def done():
     """
