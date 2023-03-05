5b3ad8285ea73a6be17af2779fbdb5b00a4772af
HereC <ebeland@gmail.com>
2023-03-05 09:16:53 -0500

    Fix schemas for new items

--- mitmproxy/addons/browserup/har/har_schemas.py
+++ mitmproxy/addons/browserup/har/har_schemas.py
@@ -1,71 +1,64 @@
 from marshmallow import Schema, fields
 
-
 class VerifyResultSchema(Schema):
-    result = fields.Boolean(required=True, description="Result True / False")
-    name = fields.Str(required=True, description="Name")
-    type = fields.Str(required=True, description="Type")
-
+    result = fields.Boolean(metadata={"required": True, "description": "Result True / False"})
+    name = fields.Str(metadata={"required":True, "description": "Name"})
+    type = fields.Str(metadata={"required":True, "description": "Type"})
 
 class NameValuePairSchema(Schema):
-    name = fields.Str(optional=True, description="Name to match")
-    value = fields.Str(optional=True, description="Value to match")
-
+    name = fields.Str(metadata={"optional":True, "description": "Name to match"})
+    value = fields.Str(metadata={"optional":True, "description": "Value to match"})
 
 class ErrorSchema(Schema):
-    name = fields.Str(optional=False, description="Name of the Error to add. Stored in har under _errors")
-    details = fields.Str(optional=False, description="Short details of the error")
-
+    name = fields.Str(metadata={"optional":False, "description": "Name of the Error to add. Stored in har under _errors"})
+    details = fields.Str(metadata={"optional":False, "description": "Short details of the error"})
 
 class CounterSchema(Schema):
-    name = fields.Str(required=True, description="Name of Custom Counter value you are adding to the page under _counters")
-    value = fields.Number(required=True, format="double", description="Value for the counter")
+    name = fields.Str(metadata={"required":True, "description": "Name of Custom Counter value you are adding to the page under _counters"})
+    value = fields.Number(metadata={"required":True, "format": "double", "description": "Value for the counter"})
 
 class PageTimingSchema(Schema):
-    onContentLoad = fields.Number(required=True, description="onContentLoad per the browser")
-    onLoad = fields.Number(required=True, description="onLoad per the browser")
-    name = fields.Str(required=False, description="Name of Custom Counter value you are adding to the page under counters")
-    value = fields.Number(required=False, format="double", description="Value for the counter")
-    _firstInputDelay = fields.Number(required=False, description="firstInputDelay from the browser")
-    _firstPaint = fields.Number(required=False, description="firstPaint from the browser")
-    _cumulativeLayoutShift = fields.Number(required=False, description="cumulativeLayoutShift metric from the browser")
-    _largestContentFullPaint = fields.Number(required=False, description="largestContentFullPaint from the browser")
-    _domInteractive = fields.Number(required=False, description="domInteractive from the browser")
-    _firstContentfulPaint = fields.Number(required=False, description="firstContentfulPaint from the browser")
-    _dns = fields.Number(required=False, description="dns lookup time from the browser")
-    _ssl = fields.Number(required=False, description="Ssl connect time from the browser")
-    _ttfb = fields.Number(required=False, description="Time to first byte of the page's first request per the browser")
-    _href =  fields.Str(required=False, description="Top level href, including hashtag, etc per the browser")
-
+    onContentLoad = fields.Number(metadata={"required":True, "description": "onContentLoad per the browser"})
+    onLoad = fields.Number(metadata={"required":True, "description": "onLoad per the browser"})
+    _firstInputDelay = fields.Number(metadata={"required":False, "description": "firstInputDelay from the browser"})
+    _firstPaint = fields.Number(metadata={"required":False, "description": "firstPaint from the browser"})
+    _cumulativeLayoutShift = fields.Number(metadata={"required":False, "description": "cumulativeLayoutShift metric from the browser"})
+    _largestContentfulPaint = fields.Number(metadata={"required":False, "description": "largestContentfulPaint from the browser"})
+    _domInteractive = fields.Number(metadata={"required":False, "description": "domInteractive from the browser"})
+    _firstContentfulPaint = fields.Number(metadata={"required":False, "description": "firstContentfulPaint from the browser"})
+    _dns = fields.Number(metadata={"required":False, "description": "dns lookup time from the browser"})
+    _ssl = fields.Number(metadata={"required":False, "description": "Ssl connect time from the browser"})
+    _ttfb = fields.Number(metadata={"required":False, "description": "Time to first byte of the page's first request per the browser"})
+    _href = fields.Str(metadata={"required":False, "description": "Top level href, including hashtag, etc per the browser"})
 
 class MatchCriteriaSchema(Schema):
-    url = fields.Str(optional=True, description="Request URL regexp to match", externalDocs={
-                     'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    page = fields.Str(optional=True, description="current|all", externalDocs={
-                      'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    status = fields.Str(optional=True, description="HTTP Status code to match.", externalDocs={
-                        'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    content = fields.Str(optional=True, description="Body content regexp content to match", externalDocs={
-                         'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    content_type = fields.Str(optional=True, description="Content type", externalDocs={
-                              'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    websocket_message = fields.Str(optional=True, description="Websocket message text to match", externalDocs={
-                                   'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    request_header = fields.Nested(NameValuePairSchema, optional=True, externalDocs={
-                                   'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    request_cookie = fields.Nested(NameValuePairSchema, optional=True, externalDocs={
-                                   'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    response_header = fields.Nested(NameValuePairSchema, optional=True, externalDocs={
-                                    'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    response_cookie = fields.Nested(NameValuePairSchema, optional=True, externalDocs={
-                                    'description': 'Python Regex', 'url': 'https://docs.python.org/3/howto/regex.html'})
-    json_valid = fields.Boolean(optional=True, description="Is valid JSON")
-    json_path = fields.Str(optional=True, description="Has JSON path")
-    json_schema = fields.Str(optional=True, description="Validates against passed JSON schema")
-    error_if_no_traffic = fields.Boolean(optional=True, default=True, description="If the proxy has NO traffic at all, return error")
+    url = fields.Str(metadata={"optional":True, "description": "Request URL regexp to match", "externalDocs":{
+                     "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    page = fields.Str(metadata={"optional":True, "description": "current|all", "externalDocs":{
+                      "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    status = fields.Str(metadata={"optional":True, "description": "HTTP Status code to match.", "externalDocs":{
+                        "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    content = fields.Str(metadata={"optional":True, "description": "Body content regexp content to match", "externalDocs":{
+                         "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    content_type = fields.Str(metadata={"optional":True, "description": "Content type", "externalDocs":{
+                              "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    websocket_message = fields.Str(metadata={"optional":True, "description": "Websocket message text to match", "externalDocs":{
+                                   "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    request_header = fields.Nested(NameValuePairSchema, metadata={ "optional": True, "externalDocs":{
+                                   "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    request_cookie = fields.Nested(NameValuePairSchema, metadata={"optional": True, "externalDocs":{
+                                   "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    response_header = fields.Nested(NameValuePairSchema, metadata={"optional": True, "externalDocs":{
+                                    "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    response_cookie = fields.Nested(NameValuePairSchema, metadata={"optional": True, "externalDocs":{
+                                    "description": "Python Regex", "url": "https://docs.python.org/3/howto/regex.html"}})
+    json_valid = fields.Boolean(metadata={"optional":True, "description": "Is valid JSON"})
+    json_path = fields.Str(metadata={"optional":True, "description": "Has JSON path"})
+    json_schema = fields.Str(metadata={"optional":True, "description": "Validates against passed JSON schema"})
+    error_if_no_traffic = fields.Boolean(metadata={"optional":True, "default": True, "description": "If the proxy has NO traffic at all, return error"})
 
     class Meta:
         ordered = True
         description = """A set of criteria for filtering HTTP Requests and Responses.
                          Criteria are AND based, and use python regular expressions for string comparison"""
-        externalDocs = {'description': 'Python Regex Doc', 'url': 'https://docs.python.org/3/howto/regex.html'}
+        externalDocs = {"description": "Python Regex Doc", "url": "https://docs.python.org/3/howto/regex.html"}
