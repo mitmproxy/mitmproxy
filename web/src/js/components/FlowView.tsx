ed064018bd293f1f692c3efb2c75655da913b920
RG <rishabhgautam727@gmail.com>
2023-03-11 23:49:11 +0530

    ignore: to make it easy to compare changes

--- web/src/js/components/FlowView.tsx
+++ web/src/js/components/FlowView.tsx
@@ -1,30 +1,24 @@
-import * as React from "react";
-import { FunctionComponent } from "react";
-import { Request, Response } from "./FlowView/HttpMessages";
-import {
-    Request as DnsRequest,
-    Response as DnsResponse,
-} from "./FlowView/DnsMessages";
-import Connection from "./FlowView/Connection";
-import Error from "./FlowView/Error";
-import Timing from "./FlowView/Timing";
-import WebSocket from "./FlowView/WebSocket";
+import * as React from "react"
+import {FunctionComponent} from "react"
+import {Request, Response} from './FlowView/HttpMessages'
+import {Request as DnsRequest, Response as DnsResponse} from './FlowView/DnsMessages'
+import Connection from './FlowView/Connection'
+import Error from "./FlowView/Error"
+import Timing from "./FlowView/Timing"
+import WebSocket from "./FlowView/WebSocket"
 
-import { selectTab } from "../ducks/ui/flow";
-import { useAppDispatch, useAppSelector } from "../ducks";
-import { Flow } from "../flow";
+import {selectTab} from '../ducks/ui/flow'
+import {useAppDispatch, useAppSelector} from "../ducks";
+import {Flow} from "../flow";
 import classnames from "classnames";
 import TcpMessages from "./FlowView/TcpMessages";
 import UdpMessages from "./FlowView/UdpMessages";
-import Metadata from "./FlowView/Metadata";
 
 type TabProps = {
-    flow: Flow;
-};
+    flow: Flow
+}
 
-export const allTabs: {
-    [name: string]: FunctionComponent<TabProps> & { displayName: string };
-} = {
+export const allTabs: { [name: string]: FunctionComponent<TabProps> & { displayName: string } } = {
     request: Request,
     response: Response,
     error: Error,
@@ -35,50 +29,45 @@ export const allTabs: {
     udpmessages: UdpMessages,
     dnsrequest: DnsRequest,
     dnsresponse: DnsResponse,
-    metadata: Metadata,
-};
+}
 
 export function tabsForFlow(flow: Flow): string[] {
-    let tabs: string[];
+    let tabs;
     switch (flow.type) {
         case "http":
-            tabs = ["request", "response", "websocket"].filter((k) => flow[k]);
-            break;
+            tabs = ['request', 'response', 'websocket'].filter(k => flow[k])
+            break
         case "tcp":
-            tabs = ["tcpmessages"];
-            break;
+            tabs = ["tcpmessages"]
+            break
         case "udp":
-            tabs = ["udpmessages"];
-            break;
+            tabs = ["udpmessages"]
+            break
         case "dns":
-            tabs = ["request", "response"]
-                .filter((k) => flow[k])
-                .map((s) => "dns" + s);
-            break;
+            tabs = ['request', 'response'].filter(k => flow[k]).map(s => "dns" + s)
+            break
     }
 
-    if (flow.error) tabs.push("error");
-    tabs.push("connection");
-    tabs.push("timing");
-    if (Object.keys(flow.metadata ?? {}).length !== 0) tabs.push("metadata");
+    if (flow.error)
+        tabs.push("error")
+    tabs.push("connection")
+    tabs.push("timing")
     return tabs;
 }
 
 export default function FlowView() {
     const dispatch = useAppDispatch(),
-        flow = useAppSelector(
-            (state) => state.flows.byId[state.flows.selected[0]]
-        ),
+        flow = useAppSelector(state => state.flows.byId[state.flows.selected[0]]),
         tabs = tabsForFlow(flow);
 
-    let active = useAppSelector((state) => state.ui.flow.tab);
+    let active = useAppSelector(state => state.ui.flow.tab)
     if (tabs.indexOf(active) < 0) {
-        if (active === "response" && flow.error) {
-            active = "error";
-        } else if (active === "error" && "response" in flow) {
-            active = "response";
+        if (active === 'response' && flow.error) {
+            active = 'error'
+        } else if (active === 'error' && "response" in flow) {
+            active = 'response'
         } else {
-            active = tabs[0];
+            active = tabs[0]
         }
     }
     const Tab = allTabs[active];
@@ -86,21 +75,17 @@ export default function FlowView() {
     return (
         <div className="flow-detail">
             <nav className="nav-tabs nav-tabs-sm">
-                {tabs.map((tabId) => (
-                    <a
-                        key={tabId}
-                        href="#"
-                        className={classnames({ active: active === tabId })}
-                        onClick={(event) => {
-                            event.preventDefault();
-                            dispatch(selectTab(tabId));
-                        }}
-                    >
+                {tabs.map(tabId => (
+                    <a key={tabId} href="#" className={classnames({active: active === tabId})}
+                       onClick={event => {
+                           event.preventDefault()
+                           dispatch(selectTab(tabId))
+                       }}>
                         {allTabs[tabId].displayName}
                     </a>
                 ))}
             </nav>
-            <Tab flow={flow} />
+            <Tab flow={flow}/>
         </div>
-    );
+    )
 }
