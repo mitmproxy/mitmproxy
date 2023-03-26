ceb8d3a43e1aca52ae6f4442954db457f9be81ab
RG <rishabhgautam727@gmail.com>
2023-03-26 15:27:43 +0530

    added search filter for tree view

--- web/src/js/components/FlowTreeView.tsx
+++ web/src/js/components/FlowTreeView.tsx
@@ -15,7 +15,7 @@ interface TreeViewFlowWrapper {
 }
 
 function FlowTreeView() {
-    const flows = useAppSelector((state) => state.flows.list);
+    const flows = useAppSelector((state) => state.flows.view);
     const newFlows: Map<string, TreeViewFlowWrapper> = new Map();
     const highlightFilter = useAppSelector((state) => state.flows.highlight);
     const isHighlightedFn = highlightFilter ? Filt.parse(highlightFilter) : () => false;
@@ -70,13 +70,13 @@ function FlowRow({
     text,
 }: {
     active?: boolean;
-    show?: boolean;
     flow: TreeViewFlowWrapper;
     text: string;
 }) {
     const [show, setShow] = React.useState(false);
     const childs = Array.from(flow.child ?? []);
     const dispatch = useDispatch()
+    const selected = useAppSelector((state) => state.flows.selected);
 
     return (
         <>
@@ -85,7 +85,7 @@ function FlowRow({
                     if (childs.length !== 0) setShow(!show)
                     if (flow.flow) dispatch(select(flow.flow.id));
                 }}
-                style={{ backgroundColor: flow.highlight ? "aqua" : "" }}
+                style={{ backgroundColor: active ? "#7bbefc" : (flow.highlight ? "#ffeb99" : "") }}
                 className={classnames(["list-group-item", active ? "active" : ""])}
             >
                 <span style={{}}>
@@ -96,7 +96,7 @@ function FlowRow({
             <div style={{ display: show ? "block" : "none" }}>
                 <pre>
                     {childs.map((el) => (
-                        <FlowRow flow={el[1]} text={el[1].path} />
+                        <FlowRow flow={el[1]} text={el[1].path} active={selected.includes(el[1].flow?.id ?? "")} />
                     ))}
                 </pre>
             </div>
