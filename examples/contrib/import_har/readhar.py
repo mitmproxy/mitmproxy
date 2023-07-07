48da5d4f6c960705a1cbb28f1411bd818c72cd97
Stanley <stanley@goodresearch.com>
2023-07-07 16:31:01 +0200

    tests for read_har

--- examples/contrib/import_har/readhar.py
+++ examples/contrib/import_har/readhar.py
@@ -3,7 +3,7 @@ import asyncio
 import json
 import logging
 import time
-
+from datetime import datetime
 from mitmproxy import command
 from mitmproxy import connection
 from mitmproxy import ctx
@@ -43,6 +43,13 @@ class ReadHar:
         Creates a HTTPFlow object from a given entry in HAR file
         """
 
+        timestamp_started = request_json["startedDateTime"]
+        timestamp_started = datetime.fromisoformat(timestamp_started)  
+
+        # Convert datetime object to UNIX timestamp
+        timestamp_started = timestamp_started.timestamp()
+        
+        timestamp_ended = timestamp_started + request_json["time"]
         request_method = request_json["request"]["method"]
         request_url = request_json["request"]["url"]
         server_address = request_json.get("serverIPAddress", None)
@@ -66,6 +73,7 @@ class ReadHar:
             server_conn = connection.Server(address=None)
 
         new_flow = http.HTTPFlow(client_conn, server_conn)
+
         new_flow.request = http.Request.make(
             request_method, request_url, "", request_headers
         )
@@ -80,7 +88,13 @@ class ReadHar:
         new_flow.response = http.Response.make(
             response_code, response_content, response_headers
         )
-
+        
+        # Change time to match HAR file
+        new_flow.request.timestamp_start = timestamp_started
+        new_flow.request.timestamp_end = timestamp_ended
+        
+        new_flow.response.timestamp_start = timestamp_started
+        new_flow.response.timestamp_end = timestamp_ended
         return new_flow
 
     @command.command("readhar")
---
+++ examples/contrib/import_har/write_to_expected.py
@@ -0,0 +1,33 @@
+import json
+from test_readhar import file_to_flows
+import glob
+
+
+
+def requests_to_flows(file_list):
+
+    for path_name in file_list:
+        print(path_name)
+        pathname = path_name.split("/")[2]
+        expected_json = {'outcome':[]}
+        flows = file_to_flows(path_name)
+        expected_json['outcome'] = flows
+        with open(f'expected/{pathname}','w') as f:
+
+            json.dump(expected_json,f,indent=4)
+    
+
+
+
+if __name__ == '__main__':
+    file_list = glob.glob('./har_files/*.har')
+    print(file_list)
+    requests_to_flows(file_list)
+
+    # nested_tuple = ((1,2),(2,3),(3,4))
+    # nested_list = [[1,1],[2,3],[3,4]]
+    # print(nested_list == nested_tuple)
+   
+    # for i in range(10):
+    #     flow = file_to_flows("./test.json")
+    #     print(flow[0]['response']['contentHash'])
