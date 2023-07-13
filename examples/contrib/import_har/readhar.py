5b322dbc98512c42ae698046b7e15eb1393c4424
Stanley <stanley@goodresearch.com>
2023-07-13 14:38:31 +0200

    added timed out request and updated chrome.har to include postdata. also added http version support

--- examples/contrib/import_har/readhar.py
+++ examples/contrib/import_har/readhar.py
@@ -1,10 +1,8 @@
 """Reads HAR files into flow objects"""
 import asyncio
-import base64
 import json
 import logging
 import time
-from datetime import datetime
 
 from mitmproxy import command
 from mitmproxy import connection
@@ -17,11 +15,14 @@ logger = logging.getLogger(__name__)
 
 
 class ReadHar:
+    def __init__(self):
+        pass
+
     def fix_headers(
         self, request_headers: list[dict[str, str]] | list[tuple[str, str]]
-    ) -> http.Headers:
+    ) -> list[tuple[bytes, bytes]]:
         """Converts provided headers into (b"header-name", b"header-value") tuples"""
-        flow_headers: list[tuple[bytes, bytes]] = []
+        flow_headers = []
         for header in request_headers:
             # Applications that use the {"name":item,"value":item} notation are Brave,Chrome,Edge,Firefox,Charles,Fiddler,Insomnia,Safari
             if isinstance(header, dict):
@@ -35,9 +36,9 @@ class ReadHar:
                     value = header[1]
                 except IndexError as e:
                     raise exceptions.OptionsError(str(e)) from e
-            flow_headers.append((key.encode(), value.encode()))
+            flow_headers.append((bytes(key, "utf-8"), bytes(value, "utf-8")))
 
-        return http.Headers(flow_headers)
+        return flow_headers
 
     # Don't know how to make a type annotation for the request json
     def request_to_flow(self, request_json: dict) -> http.HTTPFlow:
@@ -45,23 +46,20 @@ class ReadHar:
         Creates a HTTPFlow object from a given entry in HAR file
         """
 
-        timestamp_start = datetime.fromisoformat(
-            request_json["startedDateTime"]
-        ).timestamp()
-        timestamp_end = timestamp_start + request_json["time"]
         request_method = request_json["request"]["method"]
         request_url = request_json["request"]["url"]
-        server_address = request_json.get("serverIPAddress", None)
+        server_address = request_json.get("serverIPAddress",None)
         request_headers = self.fix_headers(request_json["request"]["headers"])
+        http_version = request_json["request"]["httpVersion"]
         # List contains all the representations of an http request across different HAR files
-        if request_url.startswith("http://"):
-            port = 80
-        else:
+        if http_version in ["http/2.0","h3","HTTP/1.1","HTTP/3","HTTP/2"]:
             port = 443
+        else:
+            port = 80
 
         client_conn = connection.Client(
-            peername=("127.0.0.1", 0),
-            sockname=("127.0.0.1", 0),
+            peername=("127.0.0.1", 51513),
+            sockname=("127.0.0.1", 8080),
             # TODO Get time info from HAR File
             timestamp_start=time.time(),
         )
@@ -72,8 +70,6 @@ class ReadHar:
             server_conn = connection.Server(address=None)
 
         new_flow = http.HTTPFlow(client_conn, server_conn)
-
-        # FIXME: Handle request body.
         new_flow.request = http.Request.make(
             request_method, request_url, "", request_headers
         )
@@ -81,25 +77,14 @@ class ReadHar:
         response_code = request_json["response"]["status"]
 
         # In Firefox HAR files images don't include response bodies
-        response_content = request_json["response"]["content"].get("text", None)
-        content_encoding = request_json["response"]["content"].get("encoding", None)
-        if content_encoding == "base64":
-            response_content = base64.b64decode(response_content)
-        response_headers = self.fix_headers(request_json["response"]["headers"])
+        response_content = request_json["response"]["content"].get("text","")
 
+        response_headers = self.fix_headers(request_json["response"]["headers"])
+        
         new_flow.response = http.Response.make(
             response_code, response_content, response_headers
         )
 
-        # Change time to match HAR file
-        new_flow.request.timestamp_start = timestamp_start
-        new_flow.request.timestamp_end = timestamp_end
-
-        new_flow.response.timestamp_start = timestamp_start
-        new_flow.response.timestamp_end = timestamp_end
-
-        new_flow.client_conn.timestamp_start = timestamp_start
-        new_flow.client_conn.timestamp_end = timestamp_end
         return new_flow
 
     @command.command("readhar")
@@ -109,15 +94,17 @@ class ReadHar:
     ) -> None:
         """
         Reads a HAR file into mitmproxy. Loads a flow for each entry in given HAR file.
+
+        Args:
+            path (types.Path): Path to HAR file
         """
         flows = []
         with open(path) as fp:
             try:
                 har_file = json.load(fp)
             except Exception:
-                raise exceptions.CommandError(
-                    "Unable to read HAR file. Please provide a valid HAR file"
-                )
+                raise exceptions.CommandError("Unable to read HAR file. Please provide a valid HAR file")
+                
 
         for request_json in har_file["log"]["entries"]:
             flow = self.request_to_flow(request_json)
