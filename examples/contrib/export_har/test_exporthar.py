fb07bdf0e8003ddd2de1b8066b0d32797dd00b71
Stanley <stanley@goodresearch.com>
2023-07-25 21:23:29 +0200

    fixed cookie formatters and added tests for them, added more test cases for export_har

--- examples/contrib/export_har/test_exporthar.py
+++ examples/contrib/export_har/test_exporthar.py
@@ -3,37 +3,113 @@ from pathlib import Path
 
 import pytest
 from exporthar import ExportHar
-
-from mitmproxy import io
-from mitmproxy import types
+from mitmproxy import io, types
+from mitmproxy import exceptions
+from mitmproxy.http import Request, Response, Headers
 
 here = Path(__file__).parent.absolute()
 
 
-@pytest.mark.parametrize(
-    "log_file", [pytest.param(here / "flows/logfile", id="logfile")]
-)
-def test_errors(log_file):
+def test_write_errors():
     e = ExportHar()
-    path = open(log_file, "rb")
-    flows = io.FlowReader(path).stream()
-    pytest.raises(
-        FileNotFoundError,
-        e.export_har,
-        flows,
-        types.Path("unknown_dir/testing_flow.har"),
-    )
+
+    with pytest.raises(FileNotFoundError):
+        e.export_har([], types.Path("unknown_dir/testing_flow.har"))
 
 
 @pytest.mark.parametrize(
-    "log_file", [pytest.param(here / "flows/logfile", id="logfile")]
+    "header, expected",
+    [
+        (Headers([(b"cookie", b"foo=bar")]), [{"name": "foo", "value": "bar"}]),
+        (
+            Headers([(b"cookie", b"foo=bar"), (b"cookie", b"foo=baz")]),
+            [{"name": "foo", "value": "bar"}, {"name": "foo", "value": "baz"}],
+        ),
+    ],
 )
-def test_exporthar(log_file, tmp_path):
+def test_request_cookies(header, expected):
     e = ExportHar()
-    path = open(log_file, "rb")
-    flows = io.FlowReader(path).stream()
+    req = Request.make("GET", "https://example.com", "", header)
+    assert e.format_request_cookies(req) == expected
+
+
+@pytest.mark.parametrize(
+    "header, expected",
+    [
+        (
+            Headers(
+                [
+                    (
+                        b"set-cookie",
+                        b"foo=bar; expires=Wed, 24-Jul-2024 12:58:46 GMT; path=/; domain=.google.com; priority=high",
+                    )
+                ]
+            ),
+            [
+                {
+                    "name": "foo",
+                    "value": "bar",
+                    "path": "/",
+                    "domain": ".google.com",
+                    "expires": "2024-07-24T12:58:46.000Z",
+                    "httpOnly": False,
+                    "secure": False,
+                }
+            ],
+        ),
+        (
+            Headers(
+                [
+                    (
+                        b"set-cookie",
+                        b"foo=bar; expires=Wed, 24-Jul-2024 12:58:46 GMT; path=/; domain=.google.com; Secure; HttpOnly; priority=high",
+                    ),
+                    (
+                        b"set-cookie",
+                        b"fooz=baz; expires=Wed, 24-Jul-2024 12:58:46 GMT; path=/; domain=.google.com; Secure; HttpOnly; priority=high; SameSite=none",
+                    ),
+                ]
+            ),
+            [
+                {
+                    "name": "foo",
+                    "value": "bar",
+                    "path": "/",
+                    "domain": ".google.com",
+                    "expires": "2024-07-24T12:58:46.000Z",
+                    "httpOnly": True,
+                    "secure": True,
+                },
+                {
+                    "name": "fooz",
+                    "value": "baz",
+                    "path": "/",
+                    "domain": ".google.com",
+                    "expires": "2024-07-24T12:58:46.000Z",
+                    "httpOnly": True,
+                    "secure": True,
+                    "sameSite": "none",
+                },
+            ],
+        ),
+    ],
+)
+def test_response_cookies(header, expected):
+    e = ExportHar()
+    resp = Response.make(200, "", header)
+    assert e.format_response_cookies(resp) == expected
+
+
+@pytest.mark.parametrize(
+    "log_file", [pytest.param(x, id=x.stem) for x in here.glob("flows/*.mitm")]
+)
+def test_exporthar(log_file: Path, tmp_path: Path):
+    e = ExportHar()
+
+    flows = io.read_flows_from_paths([log_file])
+
     e.export_har(flows, types.Path(tmp_path / "testing_flow.har"))
-    correct_har = json.load(open("flows/correct_flows.har"))
+    correct_har = json.load(open(f"flows/{log_file.stem}.har"))
     testing_har = json.load(open(tmp_path / "testing_flow.har"))
 
     assert testing_har == correct_har
@@ -41,6 +117,10 @@ def test_exporthar(log_file, tmp_path):
 
 if __name__ == "__main__":
     e = ExportHar()
-    path = open(here / "flows/logfile", "rb")
-    flows = io.FlowReader(path).stream()
-    e.export_har(flows, types.Path(here / "flows/correct_flows.har"))
+
+    for file in here.glob("flows/*"):
+        if not file.suffix == ".har":
+            path = open(file, "rb")
+            flows = io.FlowReader(path).stream()
+
+            e.export_har(flows, types.Path(f"flows/{file.stem}.har"))
