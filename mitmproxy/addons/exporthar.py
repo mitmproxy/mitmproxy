49ea3fe9fbe6894d8b272deebfc4347590194cc7
Maximilian Hils <git@maximilianhils.com>
2023-07-25 23:01:33 +0200

    make mypy happy

--- mitmproxy/addons/exporthar.py
+++ mitmproxy/addons/exporthar.py
@@ -5,33 +5,22 @@ import logging
 from collections.abc import Iterable
 from datetime import datetime
 from datetime import timezone
+from typing import Any
 
 from mitmproxy import command
 from mitmproxy import flow
 from mitmproxy import http
 from mitmproxy import types
 from mitmproxy import version
+from mitmproxy.connection import Server
+from mitmproxy.coretypes.multidict import _MultiDict
+from mitmproxy.exceptions import CommandError
 from mitmproxy.utils import strutils
-
 
 logger = logging.getLogger(__name__)
 
 
 class ExportHar:
-    def __init__(self):
-        # used to store unique server connections
-        self.servers_seen = set()
-
-    def format_request_cookies(self, request: http.Request) -> list[dict[str, str]]:
-        """Formats the request's cookie header to list of cookies"""
-        cookie_list = request.cookies.items(multi=True)
-
-        rv = []
-
-        for key, value in cookie_list:
-            rv.append({"name": key, "value": value})
-        return rv
-
     def format_response_cookies(self, response: http.Response) -> list[dict]:
         """Formats the response's cookie header to list of cookies"""
         cookie_list = response.cookies.items(multi=True)
@@ -42,16 +31,12 @@ class ExportHar:
                 "value": value,
                 "path": attrs["path"],
                 "domain": attrs["domain"],
-                "expires": attrs["expires"],
                 "httpOnly": "httpOnly" in attrs,
                 "secure": "secure" in attrs,
             }
-            if attrs.get("expires", None):
-                # date is given as 'Wed, 24-Jul-2024 12:58:46 GMT' format but need to be '2024-07-24T12:58:46.000Z' format
-                output_date_string = datetime.strptime(
-                    attrs["expires"], "%a, %d-%b-%Y %H:%M:%S %Z"
-                ).strftime("%Y-%m-%dT%H:%M:%S.000Z")
-                cookie["expires"] = output_date_string
+            # TODO: handle expires attribute here.
+            # This is not quite trivial because we need to parse random date formats.
+            # For now, we just ignore the attribute.
 
             if "sameSite" in attrs:
                 cookie["sameSite"] = attrs["sameSite"]
@@ -59,76 +44,58 @@ class ExportHar:
             rv.append(cookie)
         return rv
 
-    def name_value(self, obj: http.Headers) -> list[dict]:
-        return [{"name": k, "value": v} for k, v in obj.items()]
+    def format_multidict(self, obj: _MultiDict[str, str]) -> list[dict]:
+        return [
+            {"name": k, "value": v}
+            for k, v in obj.items(multi=True)
+        ]
 
-    def flow_entry(self, flow: flow.Flow) -> dict:
+    def flow_entry(self, flow: http.HTTPFlow, servers_seen: set[Server]) -> dict:
         """Creates HAR entry from flow"""
-        ssl_time = -1
-        connect_time = -1
 
-        if flow.server_conn not in self.servers_seen:
-            connect_time = (flow.server_conn.timestamp_tcp_setup or 0) - (
-                flow.server_conn.timestamp_start or 0
-            )
-            if (
-                flow.server_conn.timestamp_tls_setup
-                and flow.server_conn.timestamp_tcp_setup
-            ):
-                ssl_time = (
+        if flow.server_conn in servers_seen:
+            connect_time = -1.0
+            ssl_time = -1.0
+        elif flow.server_conn.timestamp_tcp_setup:
+            assert flow.server_conn.timestamp_start
+            connect_time = 1000 * (flow.server_conn.timestamp_tcp_setup - flow.server_conn.timestamp_start)
+
+            if flow.server_conn.timestamp_tls_setup:
+                ssl_time = 1000 * (
                     flow.server_conn.timestamp_tls_setup
                     - flow.server_conn.timestamp_tcp_setup
                 )
-            self.servers_seen.add(flow.server_conn)
+            else:
+                ssl_time = None
+            servers_seen.add(flow.server_conn)
+        else:
+            connect_time = None
+            ssl_time = None
+
+        if flow.request.timestamp_end:
+            send = 1000 * (flow.request.timestamp_end - flow.request.timestamp_start)
+        else:
+            send = 0
+
+        if flow.response and flow.request.timestamp_end:
+            wait = 1000 * (flow.response.timestamp_start - flow.request.timestamp_end)
+        else:
+            wait = 0
+
+        if flow.response and flow.response.timestamp_end:
+            receive = 1000 * (flow.response.timestamp_end - flow.response.timestamp_start)
+        else:
+            receive = 0
+
+        timings: dict[str, float | None] = {
+            "connect": connect_time,
+            "ssl": ssl_time,
+            "send": send,
+            "receive": receive,
+            "wait": wait,
+        }
 
         if flow.response:
-            timings_raw = {
-                "send": flow.request.timestamp_end - flow.request.timestamp_start,
-                "receive": flow.response.timestamp_end - flow.response.timestamp_start,
-                "wait": flow.response.timestamp_start - flow.request.timestamp_end,
-                "connect": connect_time,
-                "ssl": ssl_time,
-            }
-        elif flow.error:
-            timings_raw = {
-                "send": 0,
-                "receive": 0,
-                "wait": 0,
-                "connect": -1,
-                "ssl": -1,
-                "blocked": flow.request.timestamp_end - flow.request.timestamp_start,
-            }
-        else:
-            timings_raw = {
-                "send": flow.request.timestamp_end - flow.request.timestamp_start,
-                "receive": 0,
-                "wait": 0,
-                "connect": connect_time,
-                "ssl": ssl_time,
-            }
-
-        timings = {k: int(1000 * v) if v != -1 else -1 for k, v in timings_raw.items()}
-        full_time = sum(v for v in timings.values() if v > -1)
-        started_date_time = datetime.fromtimestamp(
-            flow.request.timestamp_start, timezone.utc
-        ).isoformat()
-
-        if not flow.response:
-            response = {
-                "status": 0,
-                "statusText": "",
-                "httpVersion": "",
-                "headers": [],
-                "cookies": [],
-                "redirectURL": "",
-                "headersSize": -1,
-                "bodySize": -1,
-                "_transferSize": 0,
-                "_error": None,
-            }
-            if flow.error:
-                response["_error"] = flow.error.msg
-        else:
             response_body_size = (
                 len(flow.response.raw_content) if flow.response.raw_content else 0
             )
@@ -141,7 +108,7 @@ class ExportHar:
                 "statusText": flow.response.reason,
                 "httpVersion": flow.response.http_version,
                 "cookies": self.format_response_cookies(flow.response),
-                "headers": self.name_value(flow.response.headers),
+                "headers": self.format_multidict(flow.response.headers),
                 "content": {
                     "size": response_body_size,
                     "compression": response_body_compression,
@@ -151,49 +118,55 @@ class ExportHar:
                 "headersSize": len(str(flow.response.headers)),
                 "bodySize": response_body_size,
             }
+            if flow.response.content and strutils.is_mostly_bin(flow.response.content):
+                response["content"]["text"] = base64.b64encode(flow.response.content).decode()
+                response["content"]["encoding"] = "base64"
+            else:
+                response["content"]["text"] = flow.response.get_text(strict=False)
+        else:
+            response = {
+                "status": 0,
+                "statusText": "",
+                "httpVersion": "",
+                "headers": [],
+                "cookies": [],
+                "content": {},
+                "redirectURL": "",
+                "headersSize": -1,
+                "bodySize": -1,
+                "_transferSize": 0,
+                "_error": None,
+            }
+            if flow.error:
+                response["_error"] = flow.error.msg
 
-        entry = {
-            "startedDateTime": started_date_time,
-            "time": full_time,
+        entry: dict[str, Any] = {
+            "startedDateTime": datetime.fromtimestamp(flow.request.timestamp_start, timezone.utc).isoformat(),
+            "time": sum(v for v in timings.values() if v is not None and v >= 0),
             "request": {
                 "method": flow.request.method,
                 "url": flow.request.pretty_url,
                 "httpVersion": flow.request.http_version,
-                "cookies": self.format_request_cookies(flow.request),
-                "headers": self.name_value(flow.request.headers),
-                "queryString": self.name_value(flow.request.query),
+                "cookies": self.format_multidict(flow.request.cookies),
+                "headers": self.format_multidict(flow.request.headers),
+                "queryString": self.format_multidict(flow.request.query),
                 "headersSize": len(str(flow.request.headers)),
-                "bodySize": len(flow.request.content),
+                "bodySize": len(flow.request.content) if flow.request.content else 0,
             },
             "response": response,
             "cache": {},
             "timings": timings,
         }
 
-        if not flow.response:
-            entry["response"]["content"] = []
-        elif strutils.is_mostly_bin(flow.response.content):
-            entry["response"]["content"] = {
-                "text": base64.b64encode(flow.response.content).decode(),
-                "encoding": "base64",
-            }
-        else:
-            entry["response"]["content"] = {
-                "text": flow.response.get_text(strict=False)
-            }
-
         if flow.request.method in ["POST", "PUT", "PATCH"]:
-            params = [
-                {"name": a, "value": b}
-                for a, b in flow.request.urlencoded_form.items(multi=True)
-            ]
+            params = self.format_multidict(flow.request.urlencoded_form)
             entry["request"]["postData"] = {
                 "mimeType": flow.request.headers.get("Content-Type", ""),
                 "text": flow.request.get_text(strict=False),
                 "params": params,
             }
 
-        if flow.server_conn.connected:
+        if flow.server_conn.peername:
             entry["serverIPAddress"] = str(flow.server_conn.peername[0])
 
         return entry
@@ -201,16 +174,28 @@ class ExportHar:
     @command.command("exporthar")
     def export_har(self, flows: Iterable[flow.Flow], path: types.Path) -> None:
         """Writes provided flows into a HAR file at a given path"""
+        entries = []
+
+        # A list of server seen till now is maintained so we can avoid
+        # using 'connect' time for entries that use an existing connection.
+        servers_seen: set[Server] = set()
+
+        for f in flows:
+            if isinstance(f, http.HTTPFlow):
+                entries.append(self.flow_entry(f, servers_seen))
+            else:
+                raise CommandError(f"Cannot export {type(f).__name__} flows to HAR.")
+
         har = {
             "log": {
                 "version": "1.2",
                 "creator": {
                     "name": "mitmproxy exporthar",
                     "version": "0.1",
-                    "comment": "mitmproxy version %s" % version.MITMPROXY,
+                    "comment": f"mitmproxy version {version.MITMPROXY}",
                 },
                 "pages": [],
-                "entries": [self.flow_entry(flow) for flow in flows],
+                "entries": entries,
             }
         }
         with open(path, "w") as fp:
