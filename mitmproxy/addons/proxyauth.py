896f42972fc5ebaed478e0a7e557a0f77eb61688
jincong.bai <jincong.bai@zatech.com>
2023-10-31 10:49:06 +0800

    fix # 6426 Optimize LDAP Proxy Auth

--- mitmproxy/addons/proxyauth.py
+++ mitmproxy/addons/proxyauth.py
@@ -40,7 +40,7 @@ class ProxyAuth:
             "username:pass",
             "any" to accept any user/pass combination,
             "@path" to use an Apache htpasswd file,
-            or "ldap[s]:url_server_ldap[:port]:dn_auth:password:dn_subtree" for LDAP authentication.
+            or "ldap[s]:url_server_ldap[:port]:dn_auth:password:dn_subtree[?search_filter_key=...]" for LDAP authentication.
             """,
         )
 
@@ -213,6 +213,7 @@ class Ldap(Validator):
     conn: ldap3.Connection
     server: ldap3.Server
     dn_subtree: str
+    filter_key: str
 
     def __init__(self, proxyauth: str):
         (
@@ -222,6 +223,7 @@ class Ldap(Validator):
             ldap_user,
             ldap_pass,
             self.dn_subtree,
+            self.filter_key,
         ) = self.parse_spec(proxyauth)
         server = ldap3.Server(url, port=port, use_ssl=use_ssl)
         conn = ldap3.Connection(server, ldap_user, ldap_pass, auto_bind=True)
@@ -229,7 +231,7 @@ class Ldap(Validator):
         self.server = server
 
     @staticmethod
-    def parse_spec(spec: str) -> tuple[bool, str, int | None, str, str, str]:
+    def parse_spec(spec: str) -> tuple[bool, str, int | None, str, str, str, str]:
         try:
             if spec.count(":") > 4:
                 (
@@ -245,6 +247,16 @@ class Ldap(Validator):
                 security, url, ldap_user, ldap_pass, dn_subtree = spec.split(":")
                 port = None
 
+            if '?' in dn_subtree:
+                dn_subtree, search_str = dn_subtree.split('?')
+                key, value = search_str.split('=')
+                if key == 'search_filter_key':
+                    search_filter_key = value
+                else:
+                    raise ValueError
+            else:
+                search_filter_key = 'cn'
+
             if security == "ldaps":
                 use_ssl = True
             elif security == "ldap":
@@ -252,14 +264,22 @@ class Ldap(Validator):
             else:
                 raise ValueError
 
-            return use_ssl, url, port, ldap_user, ldap_pass, dn_subtree
+            return (
+                use_ssl,
+                url,
+                port,
+                ldap_user,
+                ldap_pass,
+                dn_subtree,
+                search_filter_key,
+            )
         except ValueError:
             raise exceptions.OptionsError(f"Invalid LDAP specification: {spec}")
 
     def __call__(self, username: str, password: str) -> bool:
         if not username or not password:
             return False
-        self.conn.search(self.dn_subtree, f"(cn={username})")
+        self.conn.search(self.dn_subtree, f"({self.filter_key}={username})")
         if self.conn.response:
             c = ldap3.Connection(
                 self.server, self.conn.response[0]["dn"], password, auto_bind=True
