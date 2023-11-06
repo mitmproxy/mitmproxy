506bcf1b48893fac85c965ac223ab78dc8b0d876
DKarandikar <daniel.karandikar@zoho.com>
2023-11-06 16:44:06 +0000

    Add 404-extra to statusbar and cmdline group

--- test/mitmproxy/tools/console/test_statusbar.py
+++ test/mitmproxy/tools/console/test_statusbar.py
@@ -19,6 +19,7 @@ async def test_statusbar(console, monkeypatch):
         showhost=True,
         server_replay_refresh=False,
         server_replay_kill_extra=True,
+        server_replay_404_extra=True,
         upstream_cert=False,
         stream_large_bodies="3m",
         mode=["transparent"],
