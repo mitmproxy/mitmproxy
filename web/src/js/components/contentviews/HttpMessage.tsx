c83f5f5ef27878889c6e7c688fc5283207b2f1c6
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2023-12-10 21:25:53 +0530

    added svg-preview-support

--- web/src/js/components/contentviews/HttpMessage.tsx
+++ web/src/js/components/contentviews/HttpMessage.tsx
@@ -141,37 +141,17 @@ type ViewImageProps = {
     flow: HTTPFlow;
     message: HTTPMessage;
 };
-// **
-//  * ViewImage Component
-//  *
-//  * Displays an image from the provided content URL with support for various image formats.
-//  *
-//  * @component
-//  * @param {Object} props - The component props.
-//  * @param {Object} props.flow - The flow object containing image information.
-//  * @param {Object} props.message - The message object containing image details.
-//  * @returns {JSX.Element} - The rendered ViewImage component.
-//  */
 export function ViewImage({ flow, message }: ViewImageProps) {
-    // /**
-    //  * Get the content URL for the image.
-    //  *
-    //  * @type {string}
-    //  */
     const imageContentURL: string = MessageUtils.getContentURL(flow, message);
 
     return (
         <div className="flowview-image">
-            {/* Object element to dynamically handle various image formats */}
             <object
                 data={imageContentURL}
                 type="image/*"
                 width="100%"
                 height="100%"
-            >
-                {/* Fallback content in case the browser doesn't support the object tag */}
-                <p>Image not supported</p>
-            </object>
+            ></object>
         </div>
     );
 }
