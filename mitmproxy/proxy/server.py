1166db6ce9b119e87b748f64c0c0d0b6529b477e
haanhvu <haanh6594@gmail.com>
2024-02-03 12:44:21 +0700

    Rename and add test

--- mitmproxy/proxy/server.py
+++ mitmproxy/proxy/server.py
@@ -196,7 +196,7 @@ class ConnectionHandler(metaclass=abc.ABCMeta):
             self.log(
                 f"server connection to {human.format_address(command.connection.address)} killed before connect: {err}"
             )
-            await self.handle_hook(server_hooks.ServerConnectionErrorHook(hook_data))
+            await self.handle_hook(server_hooks.ServerConnectErrorHook(hook_data))
             self.server_event(
                 events.OpenConnectionCompleted(command, f"Connection killed: {err}")
             )
@@ -225,7 +225,7 @@ class ConnectionHandler(metaclass=abc.ABCMeta):
                     err = "connection cancelled"
                 self.log(f"error establishing server connection: {err}")
                 command.connection.error = err
-                await self.handle_hook(server_hooks.ServerConnectionErrorHook(hook_data))
+                await self.handle_hook(server_hooks.ServerConnectErrorHook(hook_data))
                 self.server_event(events.OpenConnectionCompleted(command, err))
                 if isinstance(e, asyncio.CancelledError):
                     # From https://docs.python.org/3/library/asyncio-exceptions.html#asyncio.CancelledError:
---
+++ test/mitmproxy/proxy/test_server.py
@@ -0,0 +1,42 @@
+import asyncio
+import collections
+
+from collections.abc import Callable
+
+from mitmproxy.connection import Client
+from mitmproxy.connection import Server
+
+from mitmproxy.proxy import commands
+from mitmproxy.proxy import server
+from mitmproxy.proxy import server_hooks
+
+def test_server_connect_error(monkeypatch):
+    monkeypatch.setattr(server.SimpleConnectionHandler, "__init__", mock_simple_connection_handler_init)
+    monkeypatch.setattr(Client, "__init__", mock_client_init)
+    s = server.SimpleConnectionHandler()
+
+    monkeypatch.setattr(asyncio, "open_connection", mock_OSError)
+    asyncio.run(s.open_connection(initialize_command()))
+
+    assert server_hooks.ServerConnectionErrorHook in server_hooks.all_hooks.values()
+
+def mock_simple_connection_handler_init(self):
+    self.client = Client()
+    self.hook_handlers = {"server_connect_error": commands.StartHook.args}
+    self.max_conns = collections.defaultdict(lambda: asyncio.Semaphore(5))
+
+def mock_client_init(self):
+    pass
+
+def mock_OSError(**kwds):
+    raise OSError
+
+def initialize_command() -> commands.OpenConnection:
+    c = commands.OpenConnection(
+        connection = Server(
+            address = "test address",
+            transport_protocol = "tcp",
+            sockname = "test sockname"
+        )
+    )
+    return c
