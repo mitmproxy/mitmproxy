mitmproxy/mitmproxy

commit 0b5e310881f0116c9e8e2ca5b9da98e258de0a11

simplify cert loading with new cryptography APIs (#6550)
/cc @driuba - does that look good?

---------

Co-authored-by: Andrius Andrikonis <andrius.andrikonis@toughlex.com>
Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

mitmproxy/certs.py
@@ -2,7 +2,6 @@ import contextlib
 import datetime
 import ipaddress
 import os
-import re
 import sys
 from dataclasses import dataclass
 from pathlib import Path
@@ -328,14 +327,7 @@ class CertStore:
         self.default_ca = default_ca
         self.default_chain_file = default_chain_file
         self.default_chain_certs = (
-            [
-                Cert.from_pem(chunk)
-                for chunk in re.split(
-                    rb"(?=-----BEGIN( [A-Z]+)+-----)",
-                    self.default_chain_file.read_bytes(),
-                )
-                if chunk.startswith(b"-----BEGIN CERTIFICATE-----")
-            ]
+            x509.load_pem_x509_certificates(self.default_chain_file.read_bytes())
             if self.default_chain_file
             else [default_ca]
         )
@@ -395,9 +387,9 @@ class CertStore:
         raw = ca_file.read_bytes()
         key = load_pem_private_key(raw, passphrase)
         dh = cls.load_dhparam(dhparam_file)
-        certs = re.split(rb"(?=-----BEGIN CERTIFICATE-----)", raw)
-        ca = Cert.from_pem(certs[1])
-        if len(certs) > 2:
+        certs = x509.load_pem_x509_certificates(raw)
+        ca = Cert(certs[-1])
+        if len(certs) > 1:
             chain_file: Path | None = ca_file
         else:
             chain_file = None

commit b1469e07c6417d6c6a036a769a536ea9515173c6

Fix ca_file containing multiple CAs
Unfortunately 0b5e310881f0116c9e8e2ca5b9da98e258de0a11 broke mitmproxy's ability to issue leaf
certificates if `ca_file` contains multiple CAs. This PR restores that capability.

The issue lies in `mitmproxy/certs.py` - specifically, in the `from_files` method of the `CertStore`
class. Before 0b5e310881f0116c9e8e2ca5b9da98e258de0a11, the issuing CA was identified like this:

``` python
raw = ca_file.read_bytes()
key = load_pem_private_key(raw, passphrase)
…
certs = re.split(rb"(?=-----BEGIN CERTIFICATE-----)", raw)
ca = Cert.from_pem(certs[1])
```

This worked even when `ca_file` contained multiple CAs. For example, consider this example:

```
-----BEGIN PRIVATE KEY-----
REDACTED
-----END PRIVATE KEY-----
-----BEGIN CERTIFICATE-----
MIICmTCCAfqgAwIBAgIRAIqaBJ6aU5wXT+5JAn8Uj1IwCgYIKoZIzj0EAwQwMDEu
MCwGA1UEAxMlbWFuc2VsbWkgSW50ZXJtZWRpYXRlIENBICgyMDIzLTA0LTExKTAe
Fw0yNDAyMTQxNzEzMDBaFw0yNDAyMTUxNzE0MDBaMB8xHTAbBgNVBAMTFG1pdG1w
cm94eSBJc3N1aW5nIENBMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEeKTLpy6KeiWN
89S9UJoTM9w6HyrEiVByf/ie2wUX3vR/3nHVMTi5XZQD6/8h3A9yUWwtRtVDj55D
STbPKjN6G8t/Qh1dGz2vanNjmWNlceM3DOIlT30vmR5+GrUOwj+7o4HoMIHlMA4G
A1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcDAgYIKwYBBQUHAwEwEgYD
VR0TAQH/BAgwBgEB/wIBADAdBgNVHQ4EFgQUkf9kgx5DbP0B66Ir6oLUDV/CIU4w
HwYDVR0jBBgwFoAUqBYQLb8rkIidwq5dYuiDFdKhY48wYAYMKwYBBAGCpGTGKEAB
BFAwTgIBAQQcbWlrZUBtYW5zZWxtaS5jb206aXNzdWluZy1jYQQrYmZhODBJaGMz
VXNUcU0yQlp6ZG1rYW1OLW1XNGtpWTJrM1g5X19kd3Q0bzAKBggqhkjOPQQDBAOB
jAAwgYgCQgF/QLZ0cThJKeoeIk4dY4+Z2MdIey7kvvy1jwlWrsOqOY+UXUWHXRlj
oXZN92imAKqgLU5T859MMqWrjA1LjN7w6QJCAY+2xN7ovry3WSMqwnu86NuPL8vh
fjftZeIW5GWuLzOsEFt7SfWMhkmFc/jcFpZ/qlolPhvr6E170zNhHUSMKvjb
-----END CERTIFICATE-----
-----BEGIN CERTIFICATE-----
MIICcjCCAdSgAwIBAgIQeK+VyMjx85py1kqGdjn5nTAKBggqhkjOPQQDBDAoMSYw
JAYDVQQDEx1tYW5zZWxtaSBSb290IENBICgyMDIzLTA0LTExKTAeFw0yMzA0MTEw
MjIzMzdaFw0yODA0MDkwMjIzMzdaMDAxLjAsBgNVBAMTJW1hbnNlbG1pIEludGVy
bWVkaWF0ZSBDQSAoMjAyMy0wNC0xMSkwgZswEAYHKoZIzj0CAQYFK4EEACMDgYYA

mitmproxy/certs.py
@@ -415,7 +415,7 @@ class CertStore:
         key = load_pem_private_key(raw, passphrase)
         dh = cls.load_dhparam(dhparam_file)
         certs = x509.load_pem_x509_certificates(raw)
-        ca = Cert(certs[-1])
+        ca = Cert(certs[0])
         if len(certs) > 1:
             chain_file: Path | None = ca_file
         else: