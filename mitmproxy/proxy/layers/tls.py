5a19d4400c7737c7d1c9d6eb7fa10f480baba909
Maximilian Hils <git@maximilianhils.com>
2024-02-15 22:39:14 +0100

    make tests pass on older OpenSSL
    This re-adds some log messages adjusted in 2a82674fdc7aa1b7dde398565f6524f6dbab7828.
    We shouldn't fail tests for this.

--- mitmproxy/proxy/layers/tls.py
+++ mitmproxy/proxy/layers/tls.py
@@ -318,9 +318,11 @@ class TLSLayer(tunnel.TunnelLayer):
                 err = f"Certificate verify failed: {error}"
             elif last_err in [
                 ("SSL routines", "ssl3_read_bytes", "tlsv1 alert unknown ca"),
-                ("SSL routines", "ssl3_read_bytes", "ssl/tls alert bad certificate"),
+                ("SSL routines", "ssl3_read_bytes", "sslv3 alert bad certificate"),
+                ("SSL routines", "ssl3_read_bytes", "ssl/tls alert bad certificate"),  # OpenSSL 3.2+
                 ("SSL routines", "", "tlsv1 alert unknown ca"),  # OpenSSL 3+
-                ("SSL routines", "", "ssl/tls alert bad certificate"),  # OpenSSL 3+
+                ("SSL routines", "", "sslv3 alert bad certificate"),  # OpenSSL 3+
+                ("SSL routines", "", "ssl/tls alert bad certificate"),  # OpenSSL 3.2+
             ]:
                 assert isinstance(last_err, tuple)
                 err = last_err[2]
