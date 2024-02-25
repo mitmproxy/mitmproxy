603fdf3ae1588248e8d3ee804c4a804fd51d23c6
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-02-25 09:09:26 +0100

    improve tree design

--- web/src/js/components/FlowTreeView_2.tsx
+++ web/src/js/components/FlowTreeView_2.tsx
@@ -1,29 +1,35 @@
 import * as React from "react";
 import { Flow } from "../flow";
 import { RequestUtils } from "../flow/utils";
+import classnames from "classnames";
 
 interface TreeElement {
     address: string;
     flow: Flow | null;
     children: TreeElement[];
-    highlight?: boolean; //TODO
+    hidden?: boolean;
 }
 
-interface FlowTreeView_2Props {
+interface FlowTreeViewProps {
     flows: Flow[];
 }
 
-function FlowTreeView_2({ flows }: FlowTreeView_2Props) {
+interface FlowRowProps {
+    treeElement: TreeElement;
+    toggleNode: (treeElement: TreeElement) => void;
+}
+
+function FlowTreeView({ flows }: FlowTreeViewProps) {
+    const [treeView, setTreeView] = React.useState<TreeElement[]>([]);
+
     function buildTree(flows: Flow[]): TreeElement[] {
         const flowsTree: TreeElement = {
-            //sort of dummy node, then we return the children of it
             address: "",
             flow: null,
             children: [],
         };
 
         flows.forEach((flow) => {
-            console.log(flow.id);
             if (flow.server_conn?.address && flow.type === "http") {
                 const url = new URL(RequestUtils.pretty_url(flow.request));
                 const address = url.href;
@@ -45,6 +51,7 @@ function FlowTreeView_2({ flows }: FlowTreeView_2Props) {
                             address: currPath,
                             flow: index === path.length - 1 ? flow : null,
                             children: [],
+                            hidden: true,
                         });
                         current = current.children[current.children.length - 1];
                     }
@@ -57,10 +64,92 @@ function FlowTreeView_2({ flows }: FlowTreeView_2Props) {
 
     React.useEffect(() => {
         const tree = buildTree(flows);
-        console.log(tree);
+        setTreeView(tree);
     }, [flows]);
 
-    return <div>ciao</div>;
+    const toggleNode = (treeElement: TreeElement) => {
+        treeElement.children.forEach((child) => {
+            child.hidden = !child.hidden;
+            child.children.forEach((c) => {
+                c.hidden = true;
+            });
+        });
+        setTreeView([...treeView]);
+    };
+
+    return (
+        <div
+            style={{
+                width: "100%",
+                maxHeight: "90vh",
+            }}
+        >
+            <ul
+                className="list-group w-100 overflow-auto"
+                style={{ width: "100%", height: "100%" }}
+            >
+                {treeView.map((element, index) => (
+                    <FlowRow
+                        key={index}
+                        treeElement={element}
+                        toggleNode={toggleNode}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default FlowTreeView_2;
+function rotateSymbol(symbol: HTMLElement | null) {
+    if (symbol) {
+        if (symbol.style.transform === "rotate(180deg)") {
+            symbol.style.transform = "rotate(90deg)";
+        } else {
+            symbol.style.transform = "rotate(180deg)";
+        }
+    }
+}
+
+function FlowRow({ treeElement, toggleNode }: FlowRowProps) {
+    const rotateSymbolRef = React.useRef<HTMLElement>(null);
+
+    const handleToggle = () => {
+        toggleNode(treeElement);
+        rotateSymbol(rotateSymbolRef.current);
+    };
+
+    return (
+        <>
+            <li
+                onClick={handleToggle}
+                className={classnames("list-group-item", {
+                    "has-children": treeElement.children.length > 0,
+                })}
+                style={{
+                    cursor: "pointer",
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: 15,
+                    marginBottom: 10,
+                }}
+            >
+                <span ref={rotateSymbolRef} className="rotate-symbol">
+                    {treeElement.children.length > 0 ? "^" : ""}
+                </span>
+                <span>{treeElement.address}</span>
+            </li>
+            {treeElement.children.map((element, index) => (
+                <div key={index} style={{ paddingLeft: 30 }}>
+                    {!element.hidden && (
+                        <FlowRow
+                            treeElement={element}
+                            toggleNode={toggleNode}
+                        />
+                    )}
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default FlowTreeView;
