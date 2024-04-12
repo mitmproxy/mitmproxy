71a2107f97cfb9afb1c759cd3b3f2c6515377747
spacewasp <spacewasp1982@gmail.com>
2024-04-12 17:12:27 +0100

    fixed test_server.py

--- test/mitmproxy/proxy/test_server.py
+++ test/mitmproxy/proxy/test_server.py
@@ -7,38 +7,35 @@ from mitmproxy.proxy import commands
 from mitmproxy.proxy import server
 from mitmproxy.proxy import server_hooks
 
-
-def test_server_connect_error(monkeypatch):
-    monkeypatch.setattr(
-        server.SimpleConnectionHandler, "__init__", mock_simple_connection_handler_init
-    )
-    monkeypatch.setattr(Client, "__init__", mock_client_init)
-    s = server.SimpleConnectionHandler()
-
-    monkeypatch.setattr(asyncio, "open_connection", mock_OSError)
-    asyncio.run(s.open_connection(initialize_command()))
-
-    assert server_hooks.ServerConnectErrorHook in server_hooks.all_hooks.values()
-
-
-def mock_simple_connection_handler_init(self):
-    self.client = Client()
-    self.hook_handlers = {"server_connect_error": commands.StartHook.args}
-    self.max_conns = collections.defaultdict(lambda: asyncio.Semaphore(5))
-
-
-def mock_client_init(self):
-    pass
-
-
-def mock_OSError():
-    raise OSError
-
-
 def initialize_command() -> commands.OpenConnection:
     c = commands.OpenConnection(
         connection=Server(
-            address="test address", transport_protocol="tcp", sockname="test sockname"
+            address=("server", 5678), transport_protocol="tcp", sockname="test sockname"
         )
     )
     return c
+
+def server_connect_error_cb(hook_data: server_hooks.ServerConnectionHookData):
+    global hook_triggered
+    hook_triggered = True
+
+def mock_simple_connection_handler_init(self):
+    self.client = Client(peername=("client", 1234), sockname=("server", 5678))
+    self.hook_handlers = {
+        server_hooks.ServerConnectErrorHook.name: server_connect_error_cb
+        }
+    self.max_conns = collections.defaultdict(lambda: asyncio.Semaphore(5))
+    self.timeout_watchdog = server.TimeoutWatchdog(0, lambda x: None)
+
+def test_server_connect_error(monkeypatch):
+    global hook_triggered
+    hook_triggered = False
+
+    monkeypatch.setattr(
+        server.SimpleConnectionHandler, "__init__", mock_simple_connection_handler_init
+    )
+    
+    s = server.SimpleConnectionHandler()
+    asyncio.run(s.open_connection(initialize_command()))
+    
+    assert hook_triggered
