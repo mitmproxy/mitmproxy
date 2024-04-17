1fd17ee9c79787037e44c10b6407ce7286178707
Maximilian Hils <github@maximilianhils.com>
2024-04-17 11:35:20 +0200

    test++, docs++

--- test/mitmproxy/proxy/test_server.py
+++ test/mitmproxy/proxy/test_server.py
@@ -1,45 +1,54 @@
 import asyncio
 import collections
+from unittest import mock
 
-from mitmproxy.connection import Client
+import pytest
+
+from mitmproxy import options
 from mitmproxy.connection import Server
 from mitmproxy.proxy import commands
 from mitmproxy.proxy import server
 from mitmproxy.proxy import server_hooks
+from mitmproxy.proxy.mode_specs import ProxyMode
 
 
-def initialize_command() -> commands.OpenConnection:
-    c = commands.OpenConnection(
-        connection=Server(
-            address=("server", 5678), transport_protocol="tcp", sockname="test sockname"
+class MockConnectionHandler(server.SimpleConnectionHandler):
+    hook_handlers: dict[str, mock.Mock]
+    def __init__(self):
+        super().__init__(
+            reader=mock.Mock(),
+            writer=mock.Mock(),
+            options=options.Options(),
+            mode=ProxyMode.parse("regular"),
+            hooks=collections.defaultdict(lambda: mock.Mock())
         )
+
+
+@pytest.mark.parametrize("result", ("success", "killed", "failed"))
+async def test_open_connection(result, monkeypatch):
+
+    handler = MockConnectionHandler()
+    server_connect = handler.hook_handlers["server_connect"]
+    server_connected = handler.hook_handlers["server_connected"]
+    server_connect_error = handler.hook_handlers["server_connect_error"]
+
+    match result:
+        case "success":
+            monkeypatch.setattr(asyncio, "open_connection", mock.AsyncMock(return_value=(mock.MagicMock(),mock.MagicMock())))
+        case "failed":
+            monkeypatch.setattr(asyncio, "open_connection", mock.AsyncMock(side_effect=OSError))
+        case "killed":
+            def _kill(d: server_hooks.ServerConnectionHookData) -> None:
+                d.server.error = "do not connect"
+            server_connect.side_effect = _kill
+
+    await handler.open_connection(
+        commands.OpenConnection(connection=Server(
+            address=("server", 1234)
+        ))
     )
-    return c
 
+    assert server_connect.call_args[0][0].server.address == ("server", 1234)
 
-def server_connect_error_cb(hook_data: server_hooks.ServerConnectionHookData):
-    global hook_triggered
-    hook_triggered = True
-
-
-def mock_simple_connection_handler_init(self):
-    self.client = Client(peername=("client", 1234), sockname=("server", 5678))
-    self.hook_handlers = {
-        server_hooks.ServerConnectErrorHook.name: server_connect_error_cb
-    }
-    self.max_conns = collections.defaultdict(lambda: asyncio.Semaphore(5))
-    self.timeout_watchdog = server.TimeoutWatchdog(0, lambda x: None)
-
-
-def test_server_connect_error(monkeypatch):
-    global hook_triggered
-    hook_triggered = False
-
-    monkeypatch.setattr(
-        server.SimpleConnectionHandler, "__init__", mock_simple_connection_handler_init
-    )
-
-    s = server.SimpleConnectionHandler()
-    asyncio.run(s.open_connection(initialize_command()))
-
-    assert hook_triggered
+    assert server_connected.called == (result == "success")
+    assert server_connect_error.called == (result != "success")
