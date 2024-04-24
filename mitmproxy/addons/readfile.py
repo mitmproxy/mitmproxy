b5574fb2987a5c2984f50711df56dba7098b976e
Maximilian Hils <git@maximilianhils.com>
2024-04-24 14:14:54 +0200

    Fix program exit when `n` is passed (#6819)
    * fix program exit when `n` is passed
    
    fix #6818
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- mitmproxy/addons/readfile.py
+++ mitmproxy/addons/readfile.py
@@ -71,8 +71,6 @@ class ReadFile:
             await self.load_flows_from_path(rfile)
         except exceptions.FlowReadException as e:
             logger.exception(f"Failed to read {ctx.options.rfile}: {e}")
-        finally:
-            self._read_task = None
 
     def running(self):
         if ctx.options.rfile:
@@ -80,7 +78,7 @@ class ReadFile:
 
     @command.command("readfile.reading")
     def reading(self) -> bool:
-        return bool(self._read_task)
+        return bool(self._read_task and not self._read_task.done())
 
 
 class ReadFileStdin(ReadFile):
