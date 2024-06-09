fa8b8998d7131e270e0778e820dc37c9b52323a8
Gaurav Jain <64748057+errorxyz@users.noreply.github.com>
2024-06-10 01:56:26 +0530

    Update strip_ech addon to use the new DNS HTTPS records API (#6905)
    * Update and rename strip_ech addon to use new DNS HTTPS records API
    
    * Update CHANGELOG.md
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,8 @@
   ([#6876](https://github.com/mitmproxy/mitmproxy/pull/6876), @errorxyz)
 * Allow parsing of HTTPS records from DNS RDATA
   ([#6884](https://github.com/mitmproxy/mitmproxy/pull/6884), @errorxyz)
+* Update strip_ech addon to use the new DNS HTTPS records API
+  ([#6905](https://github.com/mitmproxy/mitmproxy/pull/6905), @errorxyz)
 
 ## 17 April 2024: mitmproxy 10.3.0
 
--- mitmproxy/addons/strip_ech.py
+++ mitmproxy/addons/strip_ech.py
@@ -3,20 +3,18 @@ from mitmproxy import dns
 from mitmproxy.net.dns import types
 
 
-class BlockECH:
+class StripECH:
     def load(self, loader):
         loader.add_option(
-            "block_ech",
+            "strip_ech",
             bool,
             True,
             "Strip DNS HTTPS records to prevent clients from sending Encrypted ClientHello (ECH) messages",
         )
 
     def dns_response(self, flow: dns.DNSFlow):
-        # TODO: parse HTTPS records and remove ech value alone. For now,
-        # if HTTPS record is part of response, remove that record.
         assert flow.response
-        if ctx.options.block_ech:
-            flow.response.answers = [
-                answer for answer in flow.response.answers if answer.type != types.HTTPS
-            ]
+        if ctx.options.strip_ech:
+            for answer in flow.response.answers:
+                if answer.type == types.HTTPS:
+                    answer.https_ech = None
