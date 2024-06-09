b375cc42c52ee4733d5138140b37f2439acbe79c
Gaurav Jain <gjain1563@gmail.com>
2024-06-10 02:23:48 +0530

    Fix connect request triggering response hook

--- mitmproxy/proxy/layers/http/__init__.py
+++ mitmproxy/proxy/layers/http/__init__.py
@@ -756,6 +756,12 @@ class HttpStream(layer.Layer):
                 self.context.client,
             )
             yield SendHttp(ResponseEndOfMessage(self.stream_id), self.context.client)
+        elif self.flow.response.status_code == 502:
+            self.flow.error = flow.Error("CONNECT failed")
+            yield HttpErrorHook(self.flow)
+            yield SendHttp(ResponseProtocolError(self.stream_id, "CONNECT failed"), self.context.client)
+            self.client_state = self.state_errored
+            self.flow.live = False
         else:
             yield from self.send_response()
 
