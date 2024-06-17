95bf441bc3a1dfcac2cb313cac8e72d3716a4bed
Gaurav Jain <64748057+errorxyz@users.noreply.github.com>
2024-06-17 13:03:25 +0530

    make DNS mode listen for both UDP and TCP (#6912)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- test/mitmproxy/proxy/test_mode_servers.py
+++ test/mitmproxy/proxy/test_mode_servers.py
@@ -276,10 +276,12 @@ async def test_udp_start_error():
     manager = MagicMock()
 
     with taddons.context():
-        inst = ServerInstance.make("dns@127.0.0.1:0", manager)
+        inst = ServerInstance.make("reverse:udp://127.0.0.1:1234@127.0.0.1:0", manager)
         await inst.start()
         port = inst.listen_addrs[0][1]
-        inst2 = ServerInstance.make(f"dns@127.0.0.1:{port}", manager)
+        inst2 = ServerInstance.make(
+            f"reverse:udp://127.0.0.1:1234@127.0.0.1:{port}", manager
+        )
         with pytest.raises(
             Exception, match=f"Failed to bind UDP socket to 127.0.0.1:{port}"
         ):
