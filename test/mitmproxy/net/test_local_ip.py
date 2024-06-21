59931cb056e255f1526b0656c2b1d89c17389e48
Walt Chen <godsarmycy@gmail.com>
2024-06-21 11:11:37 -0400

    Fix test on an ipv6 disabled host

--- test/mitmproxy/net/test_local_ip.py
+++ test/mitmproxy/net/test_local_ip.py
@@ -1,4 +1,5 @@
 from mitmproxy.net import local_ip
+from ...conftest import skip_no_ipv6
 
 
 def test_get_local_ip():
@@ -9,6 +10,7 @@ def test_get_local_ip():
     local_ip.get_local_ip("invalid!")
 
 
+@skip_no_ipv6
 def test_get_local_ip6():
     # should never error, but may return None depending on the host OS configuration.
     local_ip.get_local_ip6()
