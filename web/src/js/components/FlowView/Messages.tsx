96382ea4464232e09d56955cf0080e550caf2cce
Alexander Prinzhorn <alexander@prinzhorn.it>
2024-06-25 08:40:16 +0200

    Fix #6951

--- web/src/js/components/FlowView/Messages.tsx
+++ web/src/js/components/FlowView/Messages.tsx
@@ -1,13 +1,13 @@
-import { Flow, MessagesMeta } from "../../flow";
-import { useAppDispatch, useAppSelector } from "../../ducks";
-import * as React from "react";
-import { useCallback, useMemo, useState } from "react";
-import { ContentViewData, useContent } from "../contentviews/useContent";
-import { MessageUtils } from "../../flow/utils";
-import ViewSelector from "../contentviews/ViewSelector";
-import { setContentViewFor } from "../../ducks/ui/flow";
-import { formatTimeStamp } from "../../utils";
-import LineRenderer from "../contentviews/LineRenderer";
+import { Flow, MessagesMeta } from '../../flow';
+import { useAppDispatch, useAppSelector } from '../../ducks';
+import * as React from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { ContentViewData, useContent } from '../contentviews/useContent';
+import { MessageUtils } from '../../flow/utils';
+import ViewSelector from '../contentviews/ViewSelector';
+import { setContentViewFor } from '../../ducks/ui/flow';
+import { formatTimeStamp } from '../../utils';
+import LineRenderer from '../contentviews/LineRenderer';
 
 type MessagesPropTypes = {
     flow: Flow;
@@ -17,18 +17,13 @@ type MessagesPropTypes = {
 export default function Messages({ flow, messages_meta }: MessagesPropTypes) {
     const dispatch = useAppDispatch();
 
-    const contentView = useAppSelector(
-        (state) => state.ui.flow.contentViewFor[flow.id + "messages"] || "Auto",
-    );
-    let [maxLines, setMaxLines] = useState<number>(
+    const contentView = useAppSelector((state) => state.ui.flow.contentViewFor[flow.id + 'messages'] || 'Auto');
+    const [maxLines, setMaxLines] = useState<number>(
         useAppSelector((state) => state.options.content_view_lines_cutoff),
     );
-    const showMore = useCallback(
-        () => setMaxLines(Math.max(1024, maxLines * 2)),
-        [maxLines],
-    );
+    const showMore = useCallback(() => setMaxLines(Math.max(1024, maxLines * 2)), [maxLines]);
     const content = useContent(
-        MessageUtils.getContentURL(flow, "messages", contentView, maxLines + 1),
+        MessageUtils.getContentURL(flow, 'messages', contentView, maxLines + 1),
         flow.id + messages_meta.count,
     );
     const messages =
@@ -39,8 +34,8 @@ export default function Messages({ flow, messages_meta }: MessagesPropTypes) {
                 } catch (e) {
                     const err: ContentViewData[] = [
                         {
-                            description: "Network Error",
-                            lines: [[["error", `${content}`]]],
+                            description: 'Network Error',
+                            lines: [[['error', `${content}`]]],
                         },
                     ];
                     return err;
@@ -48,37 +43,29 @@ export default function Messages({ flow, messages_meta }: MessagesPropTypes) {
             }
         }, [content]) || [];
 
+    let remainingLines = maxLines;
+
     return (
         <div className="contentview">
             <div className="controls">
                 <h5>{messages_meta.count} Messages</h5>
                 <ViewSelector
                     value={contentView}
-                    onChange={(cv) =>
-                        dispatch(setContentViewFor(flow.id + "messages", cv))
-                    }
+                    onChange={(cv) => dispatch(setContentViewFor(flow.id + 'messages', cv))}
                 />
             </div>
             {messages.map((d: ContentViewData, i) => {
-                const className = `fa fa-fw fa-arrow-${
-                    d.from_client ? "right text-primary" : "left text-danger"
-                }`;
+                const className = `fa fa-fw fa-arrow-${d.from_client ? 'right text-primary' : 'left text-danger'}`;
                 const renderer = (
                     <div key={i}>
                         <small>
                             <i className={className} />
-                            <span className="pull-right">
-                                {d.timestamp && formatTimeStamp(d.timestamp)}
-                            </span>
+                            <span className="pull-right">{d.timestamp && formatTimeStamp(d.timestamp)}</span>
                         </small>
-                        <LineRenderer
-                            lines={d.lines}
-                            maxLines={maxLines}
-                            showMore={showMore}
-                        />
+                        <LineRenderer lines={d.lines} maxLines={remainingLines} showMore={showMore} />
                     </div>
                 );
-                maxLines -= d.lines.length;
+                remainingLines -= d.lines.length;
                 return renderer;
             })}
         </div>
