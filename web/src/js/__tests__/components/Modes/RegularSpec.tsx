750b8a437279e4ef00f388717601c795beb33e79
Maximilian Hils <git@maximilianhils.com>
2024-07-04 15:10:54 +0200

    add snapshot tests for regular mode ui
    we want more of those once we have changed the structure of backendState.servers

---
+++ web/src/js/__tests__/components/Modes/RegularSpec.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { render } from "../../test-utils";
+import { TStore } from "../../ducks/tutils";
+import Regular from "../../../components/Modes/Regular";
+import { mockUpdate } from "../../../ducks/backendState";
+
+test("RegularSpec", async () => {
+    const store = TStore(),
+        { asFragment } = render(<Regular />, { store });
+
+        expect(asFragment()).toMatchSnapshot();
+
+        store.dispatch(mockUpdate({
+            servers: [
+                {
+                    description: "Regular Mode",
+                    full_spec: "regular",
+                    is_running: false,
+                    last_exception: "port already in use",
+                    listen_addrs: [],
+                    type: "regular",
+                }
+            ] 
+        }))
+
+        expect(asFragment()).toMatchSnapshot();
+});
