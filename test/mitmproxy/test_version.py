3a411bd8cb88a460fb516fd956d1cf76d4b0b641
Maximilian Hils <git@maximilianhils.com>
2024-07-11 11:49:13 +0200

    nits

--- test/mitmproxy/test_version.py
+++ test/mitmproxy/test_version.py
@@ -26,11 +26,11 @@ def test_get_version(monkeypatch):
         assert version.get_dev_version() == "3.0.0rc2"
 
         sys.frozen = True
-        assert version.get_dev_version() == f"3.0.0rc2 binary"
+        assert version.get_dev_version() == "3.0.0rc2 binary"
         sys.frozen = False
 
         m.return_value = b"tag-2-cafecafe"
-        assert version.get_dev_version() == f"3.0.0rc2 (+2, commit cafecaf)"
+        assert version.get_dev_version() == "3.0.0rc2 (+2, commit cafecaf)"
 
         m.side_effect = subprocess.CalledProcessError(-1, "git describe --tags --long")
         assert version.get_dev_version() == "3.0.0rc2"
