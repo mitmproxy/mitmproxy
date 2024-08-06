64e63bd0af103377cb4d1316bba2859ef660cde5
Maximilian Hils <git@maximilianhils.com>
2024-08-06 17:57:14 +0200

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -15,7 +15,6 @@ skip_not_windows = pytest.mark.skipif(
     os.name != "nt", reason="Skipping due to not Windows"
 )
 
-
 try:
     s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     s.bind(("::1", 0))
@@ -24,7 +23,6 @@ except OSError:
     no_ipv6 = True
 else:
     no_ipv6 = False
-
 
 skip_no_ipv6 = pytest.mark.skipif(no_ipv6, reason="Host has no IPv6 support")
 
