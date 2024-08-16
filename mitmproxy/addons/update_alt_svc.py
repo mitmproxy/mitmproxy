a88e8f1f09a8685f21052bf8873bfd65fe8ea333
errorxyz <gjain1563@gmail.com>
2024-08-16 23:56:36 +0530

    Add tests

--- mitmproxy/addons/update_alt_svc.py
+++ mitmproxy/addons/update_alt_svc.py
@@ -18,14 +18,14 @@ class UpdateAltSvc:
             "keep_alt_svc_header",
             bool,
             False,
-            "Keep the original alt-svc header instead of updating it to the reverse proxy target.",
+            "Keep the original alt-svc header instead of updating as per the reverse proxy target.",
         )
 
     def responseheaders(self, flow: HTTPFlow):
         if not ctx.options.keep_alt_svc_header and isinstance(
             flow.client_conn.proxy_mode, mode_specs.ReverseMode
         ):
-            listen_port = flow.client_conn.sockname[1]
+            (_, listen_port) = flow.client_conn.sockname
             headers = flow.response.headers
             if ALT_SVC in headers:
                 headers[ALT_SVC] = update_alt_svc_header(headers[ALT_SVC], listen_port)
