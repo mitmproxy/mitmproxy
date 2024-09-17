d69d78eb83cadddbd7a190375b963d539469c61b
Maximilian Hils <git@maximilianhils.com>
2024-09-17 13:02:39 +0200

    strip HTTPS records ALPN if http3 is disabled

--- test/mitmproxy/addons/test_strip_dns_https_records.py
+++ test/mitmproxy/addons/test_strip_dns_https_records.py
@@ -1,5 +1,5 @@
 from mitmproxy import dns
-from mitmproxy.addons import strip_ech
+from mitmproxy.addons import strip_dns_https_records
 from mitmproxy.net.dns import https_records
 from mitmproxy.net.dns import types
 from mitmproxy.net.dns.https_records import SVCParamKeys
@@ -9,8 +9,8 @@ from mitmproxy.test import tutils
 
 
 class TestStripECH:
-    def test_simple(self):
-        se = strip_ech.StripECH()
+    def test_strip_ech(self):
+        se = strip_dns_https_records.StripDnsHttpsRecords()
         with taddons.context(se) as tctx:
             params1 = {
                 SVCParamKeys.PORT.value: b"\x01\xbb",
@@ -51,3 +51,35 @@ class TestStripECH:
                 for answer in f.response.answers
                 if answer.type == types.HTTPS
             )
+
+    def test_strip_alpn(self):
+        se = strip_dns_https_records.StripDnsHttpsRecords()
+        with taddons.context(se) as tctx:
+            record2 = https_records.HTTPSRecord(
+                1,
+                "example.com",
+                {
+                    SVCParamKeys.ALPN.value: b"\x02h2\x02h3",
+                },
+            )
+            answers = [
+                dns.ResourceRecord(
+                    "dns.google",
+                    dns.types.HTTPS,
+                    dns.classes.IN,
+                    32,
+                    https_records.pack(record2),
+                )
+            ]
+            f = tflow.tdnsflow(resp=tutils.tdnsresp(answers=answers))
+
+            se.dns_response(f)
+            assert f.response.answers[0].https_alpn == (b"h2", b"h3")
+
+            tctx.configure(se, http3=False)
+            se.dns_response(f)
+            assert f.response.answers[0].https_alpn == (b"h2",)
+
+            f.response.answers[0].https_alpn = [b"h3"]
+            se.dns_response(f)
+            assert f.response.answers[0].https_alpn is None
