2d68a5246a2112531556611f84f0b297c8f7438b
Sujal Singh <email.sujalsingh@gmail.com>
2024-11-18 15:24:14 +0530

    remove hard coded default window size for HTTP/2 connections. (#7333)

--- mitmproxy/proxy/layers/http/_http_h2.py
+++ mitmproxy/proxy/layers/http/_http_h2.py
@@ -61,7 +61,9 @@ class BufferedH2Connection(h2.connection.H2Connection):
         super().initiate_connection()
         # We increase the flow-control window for new streams with a setting,
         # but we need to increase the overall connection flow-control window as well.
-        self.increment_flow_control_window(2**31 - 1 - 65535)  # maximum - default
+        self.increment_flow_control_window(
+            2**31 - 1 - self.inbound_flow_control_window
+        )  # maximum - default
 
     def send_data(
         self,
