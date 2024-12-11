e2fe98931d3a7892a9a20a055a45ac2b434bb909
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-12-11 09:50:19 +0100

    style

--- web/src/js/components/Modes.tsx
+++ web/src/js/components/Modes.tsx
@@ -30,7 +30,10 @@ export default function Modes() {
                     ) : (
                         <MissingMode
                             title="Local Redirect Mode"
-                            description={localModeUnavailable ?? "This mode is only supported on Windows and MacOS."}
+                            description={
+                                localModeUnavailable ??
+                                "This mode is only supported on Windows and MacOS."
+                            }
                         />
                     )}
                     <Wireguard />
