e0434726f38394bcb336baf57672f275d4feec15
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-12-11 16:57:19 +0100

    Exposing 'localModeUnavailable' to the frontend (#7390)
    * expose localModeUnavailable
    
    * adapt frontend
    
    * update gen file
    
    * [autofix.ci] apply automated fixes
    
    * add default description
    
    * style
    
    * use 'localModeUnavailable' to check
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- web/gen/state_js.py
+++ web/gen/state_js.py
@@ -50,6 +50,7 @@ async def make() -> str:
     data["contentViews"] = ["Auto", "Raw"]
     data["version"] = "1.2.3"
     data["platform"] = "darwin"
+    data["localModeUnavailable"] = None
 
     # language=TypeScript
     content = (
