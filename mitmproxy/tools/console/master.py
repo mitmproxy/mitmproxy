9b7430e7f3a646790a0370af73792161122fb573
errorxyz <gjain1563@gmail.com>
2025-01-09 13:09:33 +0530

    Prompt user before clearing screen

--- mitmproxy/tools/console/master.py
+++ mitmproxy/tools/console/master.py
@@ -67,15 +67,18 @@ class ConsoleMaster(master.Master):
     def options_error(self, exc) -> None:
         signals.status_message.send(message=str(exc), expire=1)
 
-    def prompt_for_exit(self) -> None:
+    def prompt_for_user_choice(self, prompt, callback) -> None:
         signals.status_prompt_onekey.send(
-            prompt="Quit",
+            prompt=prompt,
             keys=[
                 ("yes", "y"),
                 ("no", "n"),
             ],
-            callback=self.quit,
+            callback=callback,
         )
+
+    def prompt_for_exit(self) -> None:
+        self.prompt_for_user_choice("Quit", self.quit)
 
     def sig_add_log(self, entry: log.LogEntry):
         if log.log_tier(self.options.console_eventlog_verbosity) < log.log_tier(
