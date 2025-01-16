ffb40ae2124457071609b7be261599f06ceb6305
Manuel Meitinger <github@meitinger.at>
2025-01-16 14:51:05 +0100

    fix run.ps1 being in %PATH% (#7481)

--- release/installbuilder/run.ps1
+++ release/installbuilder/run.ps1
@@ -1,6 +1,6 @@
 $tool = $args[0]
 if (Get-Command wt -ErrorAction SilentlyContinue) {
-	Start-Process wt -ArgumentList "powershell.exe","-Command","& '$PSScriptRoot\$tool.exe'"
+	Start-Process wt -ArgumentList "powershell.exe","-Command","& '$PSScriptRoot\bin\$tool.exe'"
 } else { 
-	Start-Process powershell -ArgumentList "-Command","& '$PSScriptRoot\$tool.exe'"
+	Start-Process powershell -ArgumentList "-Command","& '$PSScriptRoot\bin\$tool.exe'"
 }
