dde9a9a640bf985fd256088c4a6db1cd60e40a23
Sujal Singh <email.sujalsingh@gmail.com>
2025-01-28 20:55:27 +0530

    update binary data test

--- test/mitmproxy/addons/test_export.py
+++ test/mitmproxy/addons/test_export.py
@@ -298,26 +298,38 @@ class TestExportPythonRequestsCommand:
 
         assert export_python_requests(post_request) == result
 
-    def test_success_with_binary_data(self, post_request):
-        # yeah, we support binary data in python
+    def test_success_with_binary_data(self, export_python_requests, post_request):
         post_request.request.headers["Content-Type"] = "application/json; charset=utf-8"
-        result = (
-            "import requests\n"
-            "\n"
-            'url = "http://address:22/path"\n'
-            "cookies = {\n"
-            "}\n"
-            "headers = {\n"
-            '    "Content-Type": "application/json; charset=utf-8",\n'
-            "}\n"
-            "body = "
-            '"""\\x00\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t\\n\\x0b\\x0c\\r\\x0e\\x0f\\x10\\x11\\x12\\x13\\x14\\x15\\x16\\x17\\x18\\x19\\x1a\\x1b\\x1c\\x1d\\x1e\\x1f '
-            '!"#$%&\\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\x7f\\x80\\x81\\x82\\x83\\x84\\x85\\x86\\x87\\x88\\x89\\x8a\\x8b\\x8c\\x8d\\x8e\\x8f\\x90\\x91\\x92\\x93\\x94\\x95\\x96\\x97\\x98\\x99\\x9a\\x9b\\x9c\\x9d\\x9e\\x9f\\xa0\\xa1\\xa2\\xa3\\xa4\\xa5\\xa6\\xa7\\xa8\\xa9\\xaa\\xab\\xac\\xad\\xae\\xaf\\xb0\\xb1\\xb2\\xb3\\xb4\\xb5\\xb6\\xb7\\xb8\\xb9\\xba\\xbb\\xbc\\xbd\\xbe\\xbf\\xc0\\xc1\\xc2\\xc3\\xc4\\xc5\\xc6\\xc7\\xc8\\xc9\\xca\\xcb\\xcc\\xcd\\xce\\xcf\\xd0\\xd1\\xd2\\xd3\\xd4\\xd5\\xd6\\xd7\\xd8\\xd9\\xda\\xdb\\xdc\\xdd\\xde\\xdf\\xe0\\xe1\\xe2\\xe3\\xe4\\xe5\\xe6\\xe7\\xe8\\xe9\\xea\\xeb\\xec\\xed\\xee\\xef\\xf0\\xf1\\xf2\\xf3\\xf4\\xf5\\xf6\\xf7\\xf8\\xf9\\xfa\\xfb\\xfc\\xfd\\xfe\\xff"""\n'
-            'res = requests.request(method="POST", url=url, headers=headers, '
-            "cookies=cookies, data=body)\n"
-            "print(res.text)\n"
-        )
-        assert export.python_requests_command(post_request) == result
+
+        result = textwrap.dedent("""
+        import requests
+
+        url = 'http://address:22/path'
+        cookies = {}
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        body = (b'\\x00\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t\\n\\x0b\\x0c\\r\\x0e\\x0f\\x10\\x11\\x12\\x13'
+         b'\\x14\\x15\\x16\\x17\\x18\\x19\\x1a\\x1b\\x1c\\x1d\\x1e\\x1f !"#$%&\\\'()*+,-./01234567\'
+         b'89:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\x7f'
+         b'\\x80\\x81\\x82\\x83\\x84\\x85\\x86\\x87\\x88\\x89\\x8a\\x8b\\x8c\\x8d\\x8e\\x8f'
+         b'\\x90\\x91\\x92\\x93\\x94\\x95\\x96\\x97\\x98\\x99\\x9a\\x9b\\x9c\\x9d\\x9e\\x9f'
+         b'\\xa0\\xa1\\xa2\\xa3\\xa4\\xa5\\xa6\\xa7\\xa8\\xa9\\xaa\\xab\\xac\\xad\\xae\\xaf'
+         b'\\xb0\\xb1\\xb2\\xb3\\xb4\\xb5\\xb6\\xb7\\xb8\\xb9\\xba\\xbb\\xbc\\xbd\\xbe\\xbf'
+         b'\\xc0\\xc1\\xc2\\xc3\\xc4\\xc5\\xc6\\xc7\\xc8\\xc9\\xca\\xcb\\xcc\\xcd\\xce\\xcf'
+         b'\\xd0\\xd1\\xd2\\xd3\\xd4\\xd5\\xd6\\xd7\\xd8\\xd9\\xda\\xdb\\xdc\\xdd\\xde\\xdf'
+         b'\\xe0\\xe1\\xe2\\xe3\\xe4\\xe5\\xe6\\xe7\\xe8\\xe9\\xea\\xeb\\xec\\xed\\xee\\xef'
+         b'\\xf0\\xf1\\xf2\\xf3\\xf4\\xf5\\xf6\\xf7\\xf8\\xf9\\xfa\\xfb\\xfc\\xfd\\xfe\\xff')
+
+
+        def main():
+            with requests.request(
+                method='POST', url=url, cookies=cookies, headers=headers, data=body
+            ) as response:
+                print(response.text)
+
+
+        main()
+        """).lstrip()
+        assert export_python_requests(post_request) == result
 
     def test_patch(self, export_curl, patch_request):
         result = (
