22f88b0c7ef2e45265188b153a3ae151232eab79
Mark Kiami <mark.kiami@gmail.com>
2025-04-07 14:26:31 +0000

    Update docstrings for store_streamed_bodies and stream_large_bodies.

--- mitmproxy/addons/proxyserver.py
+++ mitmproxy/addons/proxyserver.py
@@ -148,7 +148,8 @@ class Proxyserver(ServerManager):
             "store_streamed_bodies",
             bool,
             False,
-            "Store HTTP request and response bodies when streamed.",
+            "Store HTTP request and response bodies when streamed (see `stream_large_bodies`). "
+            "This increases memory consumption, but makes it possible to inspect streamed bodies.",
         )
         loader.add_option(
             "connection_strategy",
@@ -165,10 +166,10 @@ class Proxyserver(ServerManager):
             Optional[str],
             None,
             """
-            Stream data to the client if response body exceeds the given
+            Stream data to the client if request or response body exceeds the given
             threshold. If streamed, the body will not be stored in any way,
             and such responses cannot be modified. Understands k/m/g
-            suffixes, i.e. 3m for 3 megabytes.
+            suffixes, i.e. 3m for 3 megabytes. To store streamed bodies, see `store_streamed_bodies`.
             """,
         )
         loader.add_option(
