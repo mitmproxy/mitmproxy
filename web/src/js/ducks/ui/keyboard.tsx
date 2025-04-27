87472e4e15971493ff22744624e6ba834784526b
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2025-04-27 20:16:51 +0200

    remove if

--- web/src/js/ducks/ui/keyboard.tsx
+++ web/src/js/ducks/ui/keyboard.tsx
@@ -107,9 +107,7 @@ export function onKeyDown(e: KeyboardEvent) {
             }
 
             case "r": {
-                if (selectedFlows.length === 1) {
-                    dispatch(flowsActions.replay(selectedFlows));
-                }
+                dispatch(flowsActions.replay(selectedFlows));
                 break;
             }
 
