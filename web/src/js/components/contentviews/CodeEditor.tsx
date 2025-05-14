76f407d7091be46658c4cdcc6f6085d3159f599b
Maximilian Hils <git@maximilianhils.com>
2025-05-14 17:30:11 +0200

    web: Upgrade to CodeMirror 6 (#7709)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- web/src/js/components/contentviews/CodeEditor.tsx
+++ web/src/js/components/contentviews/CodeEditor.tsx
@@ -1,31 +1,50 @@
 import * as React from "react";
-import { Component } from "react";
-import CodeMirror from "../../contrib/CodeMirror";
+import { useCallback, useMemo } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { yaml } from "@codemirror/lang-yaml";
+import { css } from "@codemirror/lang-css";
+import { html } from "@codemirror/lang-html";
 
 type CodeEditorProps = {
     initialContent: string;
+    onChange: (content: string) => void;
+    readonly?: boolean;
+    language?: "javascript" | "yaml" | "css" | "html";
 };
 
-export default class CodeEditor extends Component<CodeEditorProps> {
-    private editor = React.createRef<CodeMirror>();
-
-    getContent = (): string => {
-        return this.editor.current?.codeMirror.getValue();
-    };
-
-    render = () => {
-        const options = {
-            lineNumbers: true,
-        };
-        return (
-            <div className="codeeditor" onKeyDown={(e) => e.stopPropagation()}>
-                <CodeMirror
-                    ref={this.editor}
-                    value={this.props.initialContent}
-                    onChange={() => 0}
-                    options={options}
-                />
-            </div>
-        );
-    };
+export default function CodeEditor({
+    initialContent,
+    onChange,
+    language,
+    readonly = false,
+}: CodeEditorProps) {
+    const stopPropagation = useCallback(
+        (e: React.KeyboardEvent<HTMLDivElement>) => e.stopPropagation(),
+        [],
+    );
+    const extensions = useMemo(() => {
+        switch (language) {
+            case "javascript":
+                return [javascript()];
+            case "yaml":
+                return [yaml()];
+            case "css":
+                return [css()];
+            case "html":
+                return [html()];
+            default:
+                return [];
+        }
+    }, [language]);
+    return (
+        <div className="codeeditor" onKeyDown={stopPropagation}>
+            <CodeMirror
+                value={initialContent}
+                onChange={onChange}
+                readOnly={readonly}
+                extensions={extensions}
+            />
+        </div>
+    );
 }
