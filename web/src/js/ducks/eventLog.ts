2493e8d3cecad9d891f828b22bb5a0b837182c59
Maximilian Hils <git@maximilianhils.com>
2025-05-15 16:12:48 +0200

    web: move eventlog reducer off immer

--- web/src/js/ducks/eventLog.ts
+++ web/src/js/ducks/eventLog.ts
@@ -1,7 +1,9 @@
-import { createAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAction, UnknownAction } from "@reduxjs/toolkit";
 
 export const EVENTS_ADD = createAction<EventLogItem>("EVENTS_ADD");
 export const EVENTS_RECEIVE = createAction<EventLogItem[]>("EVENTS_RECEIVE");
+export const toggleVisibility = createAction("events/toggleVisibility");
+export const toggleFilter = createAction<LogLevel>("events/toggleFilter");
 
 export enum LogLevel {
     debug = "debug",
@@ -31,37 +33,44 @@ export const defaultState: EventLogState = {
     view: [],
 };
 
-const eventLogSlice = createSlice({
-    name: "eventLog",
-    initialState: defaultState,
-    reducers: {
-        toggleVisibility: (state) => {
-            state.visible = !state.visible;
-        },
-        toggleFilter: (state, action: PayloadAction<LogLevel>) => {
-            state.filters[action.payload] = !state.filters[action.payload];
-            state.view = state.list.filter((log) => state.filters[log.level]);
-        },
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(EVENTS_ADD, (state, { payload: logItem }) => {
-                state.list.push(logItem);
-                if (state.filters[logItem.level]) {
-                    state.view.push(logItem);
-                }
-            })
-            .addCase(EVENTS_RECEIVE, (state, action) => {
-                state.list = action.payload;
-                state.view = state.list.filter(
-                    (log) => state.filters[log.level],
-                );
-            });
-    },
-});
-
-export const { toggleVisibility, toggleFilter } = eventLogSlice.actions;
-export default eventLogSlice.reducer;
+export default function eventLogReducer(
+    state = defaultState,
+    action: UnknownAction,
+): EventLogState {
+    if (EVENTS_ADD.match(action)) {
+        const logItem = action.payload;
+        return {
+            ...state,
+            list: [...state.list, logItem],
+            view: state.filters[logItem.level]
+                ? [...state.view, logItem]
+                : state.view,
+        };
+    } else if (EVENTS_RECEIVE.match(action)) {
+        return {
+            ...state,
+            list: action.payload,
+            view: action.payload.filter((log) => state.filters[log.level]),
+        };
+    } else if (toggleVisibility.match(action)) {
+        return {
+            ...state,
+            visible: !state.visible,
+        };
+    } else if (toggleFilter.match(action)) {
+        const filters = {
+            ...state.filters,
+            [action.payload]: !state.filters[action.payload],
+        };
+        return {
+            ...state,
+            filters,
+            view: state.list.filter((log) => filters[log.level]),
+        };
+    } else {
+        return state;
+    }
+}
 
 export function add(message: string, level: LogLevel = LogLevel.web) {
     return EVENTS_ADD({
