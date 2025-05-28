fde0616b73cd8987738b6490bf45392880eb024d
Sleeyax <yourd3veloper@gmail.com>
2025-05-28 15:55:19 +0200

    chore(web2): set up tailwind and shadcn components

--- web2/vite.config.ts
+++ web2/vite.config.ts
@@ -1,7 +1,14 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }  
 })
