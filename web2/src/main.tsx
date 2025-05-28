ae1aa800c1ab0d192726987d44c020e8bdfe4287
Sleeyax <yourd3veloper@gmail.com>
2025-05-28 18:15:07 +0200

    refactor(web2): reorganize styles and components, remove unused files

--- web2/src/main.tsx
+++ web2/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './globals.css'
+import App from './app.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
