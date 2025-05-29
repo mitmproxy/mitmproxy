81d8c49cb3a4cd5d3d3f6f58c4125f3e3efdecd8
Sleeyax <yourd3veloper@gmail.com>
2025-05-29 15:23:51 +0200

    chore(web2): add react eslint plugins

--- web2/eslint.config.js
+++ web2/eslint.config.js
@@ -3,19 +3,27 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
       prettier,
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
