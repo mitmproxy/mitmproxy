b17be67f019b0683bc7b0689f0208ee0a5b15d9b
Sleeyax <yourd3veloper@gmail.com>
2025-05-30 19:22:41 +0200

    fix(webnext): update no-unused-vars rule configuration

--- webnext/eslint.config.js
+++ webnext/eslint.config.js
@@ -33,6 +33,18 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
 );
