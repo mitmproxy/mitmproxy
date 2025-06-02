7ae2824412cf538d4669c0b0e8ef9fe4f028b7fb
Sleeyax <yourd3veloper@gmail.com>
2025-06-02 21:06:15 +0200

    refactor(webnext): move refined property selection to within `useAppSelector`

--- webnext/src/components/flow-view/http-message/headers.tsx
+++ webnext/src/components/flow-view/http-message/headers.tsx
@@ -3,15 +3,17 @@ import type { HTTPFlow } from "web/flow";
 import { KvpTable } from "@/components/kvp-table";
 
 export function RequestHeadersTable() {
-  const headers = useAppSelector((state) => state.flows.selected[0] as HTTPFlow)
-    .request.headers;
+  const headers = useAppSelector(
+    (state) => (state.flows.selected[0] as HTTPFlow).request.headers,
+  );
 
   return <KvpTable pairs={headers} />;
 }
 
 export function ResponseHeadersTable() {
-  const headers = useAppSelector((state) => state.flows.selected[0] as HTTPFlow)
-    .response?.headers;
+  const headers = useAppSelector(
+    (state) => (state.flows.selected[0] as HTTPFlow).response?.headers,
+  );
 
   // TODO: add some loading / empty state instead?
   return headers ? <KvpTable pairs={headers} /> : null;
