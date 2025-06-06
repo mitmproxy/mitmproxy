2467266ae4c6f2ee48cb5efa49e8d9037128de15
Sleeyax <yourd3veloper@gmail.com>
2025-06-06 11:23:14 +0200

    feat(webnext): change tablist background color

--- webnext/src/components/ui/tabs.tsx
+++ webnext/src/components/ui/tabs.tsx
@@ -24,7 +24,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+        "text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
         className,
       )}
       {...props}
