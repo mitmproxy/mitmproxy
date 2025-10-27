73dd139282b2f9e98e30d6f8ad836eb246260a73
Cheng Xu <git@xuc.me>
2025-10-27 13:47:58 -0700

    Fix various issues in infer_content_encoding
    - Per spec, the byte order mark (BOM) always has the highest priority to
      determine various UTF related encodings. Ref:
        - https://www.w3.org/International/questions/qa-html-encoding-declarations#bom
        - https://html.spec.whatwg.org/multipage/semantics.html#charset
        - https://drafts.csswg.org/css-syntax/#css-decode-bytes
    - For xml traffics such as `application/xml`, `image/svg+xml` etc, a XML
      declaration may contain the encoding information. Ref:
        - https://www.w3.org/TR/xml/#charencoding
        - https://html.spec.whatwg.org/multipage/semantics.html#charset
    - For CSS, the default fallback encoding should be UTF-8. Ref:
        - https://drafts.csswg.org/css-syntax/#determine-the-fallback-encoding

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@
 
 ## Unreleased: mitmproxy next
 
+- Fix various issues in infer_content_encoding
+  ([#7928](https://github.com/mitmproxy/mitmproxy/pull/7928), @xu-cheng)
 
 ## 15 October 2025: mitmproxy 12.2.0
 
--- mitmproxy/net/http/headers.py
+++ mitmproxy/net/http/headers.py
@@ -40,9 +40,23 @@ def infer_content_encoding(content_type: str, content: bytes = b"") -> str:
     """
     Infer the encoding of content from the content-type header.
     """
-    # Use the charset from the header if possible
-    parsed_content_type = parse_content_type(content_type)
-    enc = parsed_content_type[2].get("charset") if parsed_content_type else None
+    enc = None
+
+    # BOM has the highest priority
+    if content.startswith(b"\x00\x00\xfe\xff"):
+        enc = "utf-32be"
+    elif content.startswith(b"\xff\xfe\x00\x00"):
+        enc = "utf-32le"
+    elif content.startswith(b"\xfe\xff"):
+        enc = "utf-16be"
+    elif content.startswith(b"\xff\xfe"):
+        enc = "utf-16le"
+    elif content.startswith(b"\xef\xbb\xbf"):
+        # 'utf-8-sig' will strip the BOM on decode
+        enc = "utf-8-sig"
+    elif parsed_content_type := parse_content_type(content_type):
+        # Use the charset from the header if possible
+        enc = parsed_content_type[2].get("charset")
 
     # Otherwise, infer the encoding
     if not enc and "json" in content_type:
@@ -55,11 +69,22 @@ def infer_content_encoding(content_type: str, content: bytes = b"") -> str:
         if meta_charset:
             enc = meta_charset.group(1).decode("ascii", "ignore")
 
+    if not enc and "xml" in content_type:
+        xml_encoding = re.search(
+            rb"""<\?xml[^\?>]+encoding=['"]([^'"\?>]+)""", content, re.IGNORECASE
+        )
+        if xml_encoding:
+            enc = xml_encoding.group(1).decode("ascii", "ignore")
+
     if not enc and "text/css" in content_type:
         # @charset rule must be the very first thing.
         css_charset = re.match(rb"""@charset "([^"]+)";""", content, re.IGNORECASE)
         if css_charset:
             enc = css_charset.group(1).decode("ascii", "ignore")
+        else:
+            # Fallback to utf8 for css
+            # Ref: https://drafts.csswg.org/css-syntax/#determine-the-fallback-encoding
+            enc = "utf8"
 
     # Fallback to latin-1
     if not enc:
