cf952bc1fc58d04c2f430421fee9de6df2904019
Cheng Xu <3105373+xu-cheng@users.noreply.github.com>
2025-10-27 13:55:36 -0700

    Fix various issues in infer_content_encoding (#7928)
    * Fix various issues in infer_content_encoding
    
    - Per spec, the byte order mark (BOM) always has the highest priority to
      determine various UTF related encodings. Ref:
        - https://www.w3.org/International/questions/qa-html-encoding-declarations#bom
        - https://html.spec.whatwg.org/multipage/semantics.html#charset
        - https://drafts.csswg.org/css-syntax/#css-decode-bytes
    - For xml traffics such as `application/xml`, `image/svg+xml` etc, a XML
      declaration may contain the encoding information. Ref:
        - https://www.w3.org/TR/xml/#charencoding
        - https://html.spec.whatwg.org/multipage/semantics.html#charset
    - For CSS, the default fallback encoding should be UTF-8. Ref:
        - https://drafts.csswg.org/css-syntax/#determine-the-fallback-encoding
    
    * nits
    
    ---------
    
    Co-authored-by: Maximilian Hils <git@maximilianhils.com>

--- test/mitmproxy/net/http/test_headers.py
+++ test/mitmproxy/net/http/test_headers.py
@@ -37,22 +37,51 @@ def test_assemble_content_type():
         ("", b"foo", "latin-1"),
         ("", b"\xfc", "latin-1"),
         ("", b"\xf0\xe2", "latin-1"),
+        # bom
+        ("", b"\xef\xbb\xbffoo", "utf-8-sig"),
+        ("", b"\xff\xfef\x00o\x00o\x00", "utf-16le"),
+        ("", b"\xfe\xff\x00f\x00o\x00o", "utf-16be"),
+        ("", b"\xff\xfe\x00\x00f\x00\x00\x00o\x00\x00\x00o\x00\x00\x00", "utf-32le"),
+        ("", b"\x00\x00\xfe\xff\x00\x00\x00f\x00\x00\x00o\x00\x00\x00o", "utf-32be"),
+        # content-type charset
         ("text/html; charset=latin1", b"\xc3\xbc", "latin1"),
         ("text/html; charset=utf8", b"\xc3\xbc", "utf8"),
         # json
         ("application/json", b'"\xc3\xbc"', "utf8"),
-        # meta charset
+        # html meta charset
+        (
+            "text/html",
+            b'<meta charset="gb2312">\xe6\x98\x8e\xe4\xbc\xaf',
+            "gb18030",
+        ),
         (
             "text/html",
             b'<meta http-equiv="content-type" '
             b'content="text/html;charset=gb2312">\xe6\x98\x8e\xe4\xbc\xaf',
             "gb18030",
         ),
+        # xml declaration encoding
+        (
+            "application/xml",
+            b'<?xml version="1.0" encoding="gb2312"?>'
+            b"<root>\xe6\x98\x8e\xe4\xbc\xaf</root>",
+            "gb18030",
+        ),
         # css charset
+        (
+            "text/css",
+            b'\xef\xbb\xbf@charset "UTF-8";.\xe5\xb9\xb3\xe5\x92\x8c,#div2 {color: green;}',
+            "utf-8-sig",
+        ),
         (
             "text/css",
             b'@charset "gb2312";#foo::before {content: "\xe6\x98\x8e\xe4\xbc\xaf"}',
             "gb18030",
+        ),
+        (
+            "text/css",
+            b"h1 {}",
+            "utf8",
         ),
     ],
 )
