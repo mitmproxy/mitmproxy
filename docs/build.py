864dfe4dacb644ddcbee0ffb24eb361662495f6d
vitaly <optinsoft@gmail.com>
2026-04-17 12:35:32 +0500

    Use sys.executable instead of python3 in docs build script

--- docs/build.py
+++ docs/build.py
@@ -2,12 +2,13 @@
 import shutil
 import subprocess
 from pathlib import Path
+import sys
 
 here = Path(__file__).parent
 
 for script in sorted((here / "scripts").glob("*.py")):
     print(f"Generating output for {script.name}...")
-    out = subprocess.check_output(["python3", script.absolute()], cwd=here, text=True)
+    out = subprocess.check_output([sys.executable, script.absolute()], cwd=here, text=True)
     if out:
         (here / "src" / "generated" / f"{script.stem}.html").write_text(
             out, encoding="utf8"
