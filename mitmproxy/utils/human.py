7f7b606c81a707e70ca147c43321353471e016f7
yyw <yyweiss@gmail.com>
2026-07-08 21:18:14 +0300

    uds recv: fix human readable address

--- mitmproxy/utils/human.py
+++ mitmproxy/utils/human.py
@@ -78,13 +78,15 @@ def format_timestamp_with_milli(s):
 
 
 @functools.lru_cache
-def format_address(address: tuple | None) -> str:
+def format_address(address: tuple | str | None) -> str:
     """
     This function accepts IPv4/IPv6 tuples and
     returns the formatted address string with port number
     """
     if address is None:
         return "<no address>"
+    if isinstance(address, str):
+        return address
     try:
         host = ipaddress.ip_address(address[0])
         if host.is_unspecified:
