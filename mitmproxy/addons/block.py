233c006f03a612de47b5d65309bd5e7d9233e73d
yyw <yyweiss@gmail.com>
2026-07-08 21:22:02 +0300

    uds recv: skip addons/block

--- mitmproxy/addons/block.py
+++ mitmproxy/addons/block.py
@@ -27,6 +27,8 @@ class Block:
         )
 
     def client_connected(self, client):
+        if isinstance(client.peername, str):
+            return
         parts = client.peername[0].rsplit("%", 1)
         address = ipaddress.ip_address(parts[0])
         if isinstance(address, ipaddress.IPv6Address):
