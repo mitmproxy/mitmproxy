b099ec03f03434b02d4d4346a4c88ccb6a6ba574
Eliahu Frusin <elifrusin@gmail.com>
2026-08-12 17:24:16 +0300

    test: add regression test for addon.configure() not being called

--- test/mitmproxy/test_taddons.py
+++ test/mitmproxy/test_taddons.py
@@ -5,3 +5,18 @@ def test_load_script(tdata):
     with taddons.context() as tctx:
         s = tctx.script(tdata.path("mitmproxy/data/addonscripts/recorder/recorder.py"))
         assert s
+
+
+def test_configure_calls_addon_configure():
+    class TestAddon:
+        def load(self, loader):
+            loader.add_option("foo", str, "", "test option")
+
+        def configure(self, updated):
+            self.configure_was_called = True
+
+    addon = TestAddon()
+    with taddons.context(addon) as tctx:
+        tctx.configure(addon, foo="bar")
+        assert tctx.options.foo == "bar"
+        assert addon.configure_was_called
